811438609ea22f46abee2294b56c4424ca5a96fd
Japheth Obala <japhethobalak@gmail.com>
2019-03-01 14:22:43 +0300

    Fix tests

--- src/tests/services/reducers/query-runner-reducers.spec.tsx
+++ src/tests/services/reducers/query-runner-reducers.spec.tsx
@@ -1,11 +1,11 @@
 import { QUERY_GRAPH_SUCCESS } from '../../../app/services/constants';
-import { queryRunner } from '../../../app/services/reducers/query-runner-reducers';
+import { queryRunnerReducer } from '../../../app/services/reducers/query-runner-reducers';
 
 describe('Query Runner Reducer', () => {
   it('should return initial state', () => {
     const initialState = {};
     const dummyAction = { type: 'Dummy', response: 'response'};
-    const newState = queryRunner(initialState, dummyAction);
+    const newState = queryRunnerReducer(initialState, dummyAction);
 
     expect(newState).toEqual(initialState);
   });
@@ -13,7 +13,7 @@ describe('Query Runner Reducer', () => {
   it('should handle QUERY_GRAPH_SUCCESS', () => {
     const initialState = {};
     const queryAction = { type: QUERY_GRAPH_SUCCESS, response: 'response' };
-    const newState = queryRunner(initialState, queryAction);
+    const newState = queryRunnerReducer(initialState, queryAction);
 
     expect(newState).toContain('response');
   });
