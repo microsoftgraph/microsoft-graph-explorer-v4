7a362a42d62eee830b78ca13abe70c656293d096
Japheth Obala <japhethobalak@gmail.com>
2019-03-12 13:26:02 +0300

    1. Adds locale files 2. Detects the user's locale

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { addLocaleData, IntlProvider } from 'react-intl';
-import enLocaleData from 'react-intl/locale-data/en';
+import br from 'react-intl/locale-data/br';
+import de from 'react-intl/locale-data/de';
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import jp from 'react-intl/locale-data/ja';
+import ru from 'react-intl/locale-data/ru';
+import zh from 'react-intl/locale-data/zh';
+
 import { Provider } from 'react-redux';
 import App from './app/views/App';
 
@@ -13,14 +20,23 @@ import './styles/index.scss';
 initializeIcons();
 const appState = store({});
 
-const locale = 'en-US';
+const supportedLocales = ['de-DE', 'en-US', 'es-ES', 'fr-FR', 'ja-JP', 'pt-BR', 'ru-RU', 'zh-CN'];
 
-addLocaleData(enLocaleData);
+const locale = supportedLocales.indexOf(navigator.language) !== -1 ? navigator.language : 'en-US';
+
+addLocaleData([
+  ...br,
+  ...de,
+  ...en,
+  ...fr,
+  ...jp,
+  ...ru,
+  ...zh,
+]);
 
 ReactDOM.render(
-
   <Provider store={appState}>
-    <IntlProvider locale={locale} messages={messages[locale]}>
+    <IntlProvider locale={locale} messages={(messages as any)[locale]}>
       <App />
     </IntlProvider>
   </Provider>,
