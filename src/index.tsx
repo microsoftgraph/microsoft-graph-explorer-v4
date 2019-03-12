83ca7d80f4f9e2144a3fcb499e44b6098b9ee0a3
Japheth Obala <japhethobalak@gmail.com>
2019-03-12 12:56:55 +0300

    Adds localization for en-US

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
 import { Provider } from 'react-redux';
-
 import App from './app/views/App';
+
+import messages from './messages';
 import { store } from './store';
 import './styles/index.scss';
 
 initializeIcons();
 const appState = store({});
 
+const locale = 'en-US';
+
+addLocaleData(enLocaleData);
+
 ReactDOM.render(
+
   <Provider store={appState}>
-    <App />
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <App />
+    </IntlProvider>
   </Provider>,
   document.getElementById('root'));
