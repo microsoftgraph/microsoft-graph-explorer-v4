55940bf7745507b612f3b15f495c962eb790dae7
jobala <japheth.obala@andela.com>
2019-03-18 12:20:22 +0300

    Changes locale to user's

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(process.env.PORT || 9000);
--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ addLocaleData([
 
 ReactDOM.render(
   <Provider store={appState}>
-    <IntlProvider locale={locale} messages={(messages as any)['fr-FR']}>
+    <IntlProvider locale={locale} messages={(messages as any)[locale]}>
       <App />
     </IntlProvider>
   </Provider>,
