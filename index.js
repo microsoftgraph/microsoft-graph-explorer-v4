68adcccb30d72dda59736de0a0285c59367ac310
jobala <japheth.obala@andela.com>
2019-03-18 16:37:23 +0300

    Sets X-Frame-Options header

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
+  res.set('X-Frame-Options', 'allow-from  https://review.docs.microsoft.com/en-us/graph/try-it?branch=task/tryit-concept');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
