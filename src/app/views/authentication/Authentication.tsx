cddcb72b29de6266f69fd8e45ebd0c2c0fd973ab
Charles Wahome <thewahome.cw@gmail.com>
2019-03-20 15:30:59 +0300

    enable log in

--- src/app/views/authentication/Authentication.tsx
+++ src/app/views/authentication/Authentication.tsx
@@ -2,69 +2,93 @@ import hello from 'hellojs';
 import { PrimaryButton } from 'office-ui-fabric-react';
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import { IAuthenticationProps, IAuthenticationState } from '../../../types/authentication';
+import * as authActionCreators from '../../services/actions/auth-action-creators';
 import './authentication.scss';
 
-export class Authentication extends Component {
+export class Authentication extends Component<IAuthenticationProps,  IAuthenticationState> {
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: {},
+    };
   }
 
-  public signIn() {
+  public signIn = () => {
     hello.init({
-        msft: {
-          oauth: {
-            version: 2,
-            auth: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-            grant: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-          },
-          scope_delim: ' ',
-          form: false,
+      msft: {
+        oauth: {
+          version: 2,
+          auth:
+            'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+          grant: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
         },
-        msft_admin_consent: {
-          oauth: {
-            version: 2,
-            auth: 'https://login.microsoftonline.com/common/adminconsent',
-            grant: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-          },
-          scope_delim: ' ',
-          form: false,
+        scope_delim: ' ',
+        form: false,
+      },
+      msft_admin_consent: {
+        oauth: {
+          version: 2,
+          auth: 'https://login.microsoftonline.com/common/adminconsent',
+          grant: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
         },
-      } as any);
-    hello.init({
+        scope_delim: ' ',
+        form: false,
+      },
+    } as any);
+    hello.init(
+      {
         msft: 'cb2d7367-7429-41c6-ab18-6ecb336139a6',
         msft_admin_consent: 'cb2d7367-7429-41c6-ab18-6ecb336139a6',
-        scope: 'openid',
-      }, {
-          redirect_uri: window.location.pathname,
-        });
-
+      },
+      {
+        redirect_uri: window.location.pathname,
+      },
+    );
     hello('msft').login({
-            response_type: 'token',
-            scope: 'openid profile User.ReadWrite User.ReadBasic.All Sites.ReadWrite.All Contacts.ReadWrite ' +
-            'People.Read Notes.ReadWrite.All Tasks.ReadWrite Mail.ReadWrite Files.ReadWrite.All Calendars.ReadWrite',
-            display: 'popup',
-        }).then((response) => {
-            // tslint:disable-next-line:no-console
-            console.log(response);
-            }, (e) => {
-                // tslint:disable-next-line:no-console
-                console.log(e);
-        });
-
-  }
+        response_type: 'token',
+        scope:
+          'openid profile User.ReadWrite User.ReadBasic.All Sites.ReadWrite.All Contacts.ReadWrite ' +
+          'People.Read Notes.ReadWrite.All Tasks.ReadWrite Mail.ReadWrite Files.ReadWrite.All Calendars.ReadWrite',
+        display: 'popup',
+      });
+    hello.on('auth.login', async (auth) => {
+        let accessToken;
+        if (auth.network === 'msft') {
+          const authResponse = hello('msft').getAuthResponse();
+          accessToken = authResponse.access_token;
+        }
+        if (accessToken) {
+          console.log(accessToken);
+        }
+      });
+  };
 
   public render() {
     return (
       <div className='authentication-container'>
-        <PrimaryButton onClick={this.signIn} className='signIn-button'>Sign In</PrimaryButton>
+        <PrimaryButton onClick={this.signIn} className='signIn-button'>
+          Sign In
+        </PrimaryButton>
         <div className='authentication-details'>
-            <span className ='user-name'>Megan Bowen</span><br/>
-            <span className='user-email'>MeganB@M365x214355.onmicrosoft.com</span>
+          <span className='user-name'>Megan Bowen</span>
+          <br />
+          <span className='user-email'>MeganB@M365x214355.onmicrosoft.com</span>
         </div>
       </div>
     );
   }
 }
 
-export default Authentication;
+function mapDispatchToProps(dispatch: Dispatch): object {
+  return {
+    actions: bindActionCreators(authActionCreators, dispatch),
+  };
+}
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Authentication);
