724ba85c2ee4a45f92b7ed607a65a5ca39623072
Charles Wahome <thewahome.cw@gmail.com>
2019-04-01 14:13:35 +0300

    adds view tests

---
+++ src/tests/views/authentication.spec.tsx
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import { Authentication } from '../../app/views/authentication';
+
+describe('Authentication Component', () => {
+  it('renders without crashing', () => {
+    shallow(<Authentication/>);
+  });
+});
