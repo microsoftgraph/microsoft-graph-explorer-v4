d9cb659f5c5f3e5ebcca6e03ed7bd72f878cd974
Japheth Obala <japhethobalak@gmail.com>
2019-04-17 09:40:53 +0300

    Adds themes

---
+++ src/themes/dark.ts
@@ -0,0 +1,27 @@
+// Theme generated from https://developer.microsoft.com/en-us/fabric#/styles/themegenerator
+export const dark = {
+  palette: {
+    themePrimary: '#0078d4',
+    themeLighterAlt: '#eff6fc',
+    themeLighter: '#deecf9',
+    themeLight: '#c7e0f4',
+    themeTertiary: '#71afe5',
+    themeSecondary: '#2b88d8',
+    themeDarkAlt: '#106ebe',
+    themeDark: '#005a9e',
+    themeDarker: '#004578',
+    neutralLighterAlt: '#0b0b0b',
+    neutralLighter: '#151515',
+    neutralLight: '#252525',
+    neutralQuaternaryAlt: '#2f2f2f',
+    neutralQuaternary: '#373737',
+    neutralTertiaryAlt: '#595959',
+    neutralTertiary: '#fcf7f7',
+    neutralSecondary: '#fcf8f8',
+    neutralPrimaryAlt: '#fdf9f9',
+    neutralPrimary: '#f9f2f2',
+    neutralDark: '#fefcfc',
+    black: '#fefdfd',
+    white: '#000000',
+  },
+};
