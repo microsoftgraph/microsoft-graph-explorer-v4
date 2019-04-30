c8543c42577363ece011e788b77bc6c798b876e5
Japheth Obala <japhethobalak@gmail.com>
2019-04-30 11:36:03 +0300

    Extracts logic for making anonymous requests into it's own function.

--- src/app/services/actions/query-action-creators.ts
+++ src/app/services/actions/query-action-creators.ts
@@ -19,65 +19,78 @@ export function queryResponseError(response: object): IAction {
 }
 
 export function runQuery(query: IQuery): Function {
-  let authToken = '{token:https://graph.microsoft.com/}';
-  let graphUrl = `https://proxy.apisandbox.msdn.microsoft.com/svc?url=${query.sampleUrl}`;
-  const respHeaders: any = {};
-
   return (dispatch: Function, getState: Function) => {
-    const state = getState().authResponse;
-    if (state) {
-      const authenticatedUser = state.authenticatedUser;
-      if (authenticatedUser && authenticatedUser.token) {
-        authToken = authenticatedUser.token;
-        graphUrl = query.sampleUrl;
+    const authState = getState().authResponse;
 
-        GraphClient.getInstance()
+    if (authState) {
+      const isAuthenticated = authState.authenticatedUser;
+
+      if (isAuthenticated) {
+        const graphUrl = query.sampleUrl;
+
+        return GraphClient.getInstance()
           .api('/me')
           .get();
       }
+
+      return anonymousRequest(dispatch, query);
     }
-
-    const headers = {
-      'Authorization': `Bearer ${authToken}`,
-      'Content-Type': 'application/json',
-    };
-
-    let options: IRequestOptions = {};
-    options = { method: query.selectedVerb, headers};
-
-    const hasBody = !!query.sampleBody;
-    const isGET = query.selectedVerb;
-    if (hasBody && !isGET) {
-      const body = JSON.stringify(query.sampleBody);
-      options = {...options,  body};
-    }
-
-    return fetch(graphUrl, options)
-      .then((resp) => {
-        if (resp.ok) {
-          resp.headers.forEach((val, key) => {
-            respHeaders[key] = val;
-          });
-
-          const contentType = getContentType(resp.headers);
-          if (contentType && isImageResponse(contentType)) {
-            return resp;
-          } else {
-            return resp.json();
-          }
-        }
-        throw new Error('The request was not completed');
-      })
-      .then((json) =>
-        dispatch(
-          queryResponse({
-            body: json,
-            headers: respHeaders,
-          }),
-        ),
-      )
-      .catch((error) => dispatch(queryResponseError(error)));
+    //
+    // const hasBody = !!query.sampleBody;
+    // const isGET = query.selectedVerb;
+    // if (hasBody && !isGET) {
+    //   const body = JSON.stringify(query.sampleBody);
+    //   options = {...options,  body};
+    // }
+    //
   };
+}
+
+function anonymousRequest(dispatch: Function, query: IQuery) {
+  const authToken = '{token:https://graph.microsoft.com/}';
+  const graphUrl = `https://proxy.apisandbox.msdn.microsoft.com/svc?url=${query.sampleUrl}`;
+  const respHeaders: any = {};
+
+
+  const headers = {
+    'Authorization': `Bearer ${authToken}`,
+    'Content-Type': 'application/json',
+  };
+
+
+  let options: IRequestOptions = {};
+  options = { method: query.selectedVerb, headers};
+
+  return fetch(graphUrl, options)
+    .then((resp) => {
+      if (resp.ok) {
+        resp.headers.forEach((val, key) => {
+          respHeaders[key] = val;
+        });
+
+        const contentType = getContentType(resp.headers);
+        if (contentType && isImageResponse(contentType)) {
+          return resp;
+        } else {
+          return resp.json();
+        }
+      }
+      throw new Error('The request was not completed');
+    })
+    .then((json) =>
+
+      dispatch(
+        queryResponse({
+          body: json,
+          headers: respHeaders,
+        }),
+      ),
+    )
+    .catch((error) => dispatch(queryResponseError(error)));
+}
+
+function authenticatedRequest() {
+
 }
 
 export function isImageResponse(contentType: string) {
