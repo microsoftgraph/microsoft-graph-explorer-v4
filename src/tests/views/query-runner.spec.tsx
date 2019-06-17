3772c841dddf0b6d22c9dac8ece1aaee0521c202
Charles Wahome <thewahome.cw@gmail.com>
2019-06-17 15:35:04 +0300

    fixes failing tests

--- src/tests/views/query-runner.spec.tsx
+++ src/tests/views/query-runner.spec.tsx
@@ -1,18 +1,19 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import { QueryInputControl } from '../../app/views/query-runner/QueryInput';
 import { QueryRunner } from '../../app/views/query-runner/QueryRunner';
+
+const queryProps = {
+  isLoadingData: false,
+  headers: [],
+  sampleQuery: {
+    selectedVerb: 'GET',
+    sampleUrl: 'https://graph.microsoft.com/v1.0/me/',
+  }
+};
 
 describe('Query Runner Component', () => {
   it('renders without crashing', () => {
-    shallow(<QueryRunner/>);
-  });
-
-  it('has default sample url', () => {
-    const wrapper = shallow(<QueryRunner/>);
-    const props = wrapper.children().find(QueryInputControl).props();
-
-    expect(props.sampleUrl).toBe('https://graph.microsoft.com/v1.0/me/');
+    shallow(<QueryRunner {...queryProps}/>);
   });
 });
