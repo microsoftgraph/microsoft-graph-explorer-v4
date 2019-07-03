2fdc5a2e0bb0a062e0fbdf7eacb486bd7063df6f
Japheth Obala <japhethobalak@gmail.com>
2019-07-03 19:36:01 +0300

    Direct webpack output to the public folder

--- config/paths.js
+++ config/paths.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const path = require('path');
 const fs = require('fs');
@@ -69,7 +69,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('public'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
