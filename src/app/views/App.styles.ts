a6c0db55e1a76ac1265a076c2f860d35a5479c04
Japheth Obala <japhethobalak@gmail.com>
2019-07-10 10:40:24 +0300

    Hides Sign In button when GE is in TryIt mode

--- src/app/views/App.styles.ts
+++ src/app/views/App.styles.ts
@@ -3,10 +3,11 @@ import { ITheme } from '@uifabric/styling';
 export const appStyles = (theme: ITheme) => {
   return {
     app: {
-        background: theme.semanticColors.bodyBackground,
-        color: theme.semanticColors.bodyText,
-        width: '100%',
-        height: '100vh',
-      },
+      background: theme.semanticColors.bodyBackground,
+      color: theme.semanticColors.bodyText,
+      paddingTop: theme.spacing.s1,
+      width: '100%',
+      height: '100vh',
+    },
   };
 };
