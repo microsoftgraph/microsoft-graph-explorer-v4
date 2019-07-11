446e3fa54e3f54fdee8ee71f322e0a825da7cc6a
kachulio1 <jngugi88@gmail.com>
2019-07-11 23:42:21 +0300

    add runQueryResponse

--- src/app/services/actions/query-action-creators.ts
+++ src/app/services/actions/query-action-creators.ts
@@ -1,6 +1,15 @@
 import { IQuery } from '../../../types/query-runner';
-import { anonymousRequest, authenticatedRequest } from './query-action-creator-util';
+import { QUERY_RUN_RESPONSE } from '../redux-constants';
+import {
+  anonymousRequest,
+  authenticatedRequest
+} from './query-action-creator-util';
 
+export function runQueryResponse () {
+  return (dispatch: Function)  => {
+    dispatch({type: QUERY_RUN_RESPONSE})
+  }
+}
 export function runQuery(query: IQuery): Function {
   return (dispatch: Function, getState: Function) => {
     const tokenPresent = getState().authToken;
