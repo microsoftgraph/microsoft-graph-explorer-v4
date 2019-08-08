9bedf3f39a49de0983d6b3ec062bcddebb0560e7
Charles Wahome <thewahome@outlook.com>
2019-08-08 20:52:26 +0300

    creates query input action creator and reducer tests

---
+++ src/tests/services/actions/query-input-action-creators.spec.tsx
@@ -0,0 +1,36 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { setSampleQuery } from '../../../app/services/actions/query-input-action-creators';
+import { SET_SAMPLE_QUERY_SUCCESS } from '../../../app/services/redux-constants';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('actions', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  it('creates SET_SAMPLE_QUERY_SUCCESS when setSampleQuery is called', () => {
+    const expectedActions = [
+      {
+        type: SET_SAMPLE_QUERY_SUCCESS,
+        response: {
+          selectedVerb: 'GET',
+          sampleUrl: 'https://graph.microsoft.com/v1.0/me/'
+        }
+      },
+    ];
+
+    const store = mockStore({ sampleQuery: '' });
+
+    // @ts-ignore
+    store.dispatch(setSampleQuery({
+      selectedVerb: 'GET',
+      sampleUrl: 'https://graph.microsoft.com/v1.0/me/'
+    }));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+});
