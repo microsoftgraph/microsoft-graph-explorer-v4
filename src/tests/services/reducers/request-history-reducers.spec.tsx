8c2b17af0e325f6314ee3313f5f642c5d73f018a
Charles Wahome <thewahome.cw@gmail.com>
2019-08-20 08:44:58 +0300

    adds action creator / reducer tests

---
+++ src/tests/services/reducers/request-history-reducers.spec.tsx
@@ -0,0 +1,27 @@
+import { requestHistory } from '../../../app/services/reducers/request-history-reducers';
+import { ADD_HISTORY_ITEM_SUCCESS } from '../../../app/services/redux-constants';
+
+
+describe('Request History Reducer', () => {
+  it('should return initial state', () => {
+    const initialState: any = [];
+    const dummyHistoryItem: any = [{ name: 'Key', value: 'Value' }];
+    const newState = requestHistory(initialState, dummyHistoryItem);
+
+    expect(newState).toEqual(initialState);
+  });
+
+  it('should handle ADD_HISTORY_ITEM_SUCCESS', () => {
+    const initialState: any = [];
+    const dummy = { query: 'query', runTime: new Date().toISOString() };
+    const queryAction = {
+      type: ADD_HISTORY_ITEM_SUCCESS,
+      response: dummy
+    };
+
+    const newState = requestHistory(initialState, queryAction);
+
+    expect(newState).toEqual([dummy]);
+  });
+
+});
