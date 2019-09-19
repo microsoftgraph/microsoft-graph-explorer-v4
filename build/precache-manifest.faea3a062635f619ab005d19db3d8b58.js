a59db2b1f07aec0ed07596c46acbfb62856b6b8b
jobala <japhethobalak@gmail.com>
2019-09-19 10:14:50 +0300

    Adds build directory.

---
+++ build/precache-manifest.faea3a062635f619ab005d19db3d8b58.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "82bb51deb6beb11120fd",
+    "url": "/static/css/main.1bf4d554.chunk.css"
+  },
+  {
+    "revision": "82bb51deb6beb11120fd",
+    "url": "/static/js/main.82bb51de.chunk.js"
+  },
+  {
+    "revision": "5e362f3d24f00b7040a6",
+    "url": "/static/js/runtime~main.5e362f3d.js"
+  },
+  {
+    "revision": "cc793266a18467600031",
+    "url": "/static/css/2.b742d8a0.chunk.css"
+  },
+  {
+    "revision": "cc793266a18467600031",
+    "url": "/static/js/2.cc793266.chunk.js"
+  },
+  {
+    "revision": "b334c27555b67390648c",
+    "url": "/static/js/3.b334c275.chunk.js"
+  },
+  {
+    "revision": "95cfdc2d6330f5c3afc3",
+    "url": "/static/js/4.95cfdc2d.chunk.js"
+  },
+  {
+    "revision": "a3e227f915b10d0f2b09",
+    "url": "/static/js/5.a3e227f9.chunk.js"
+  },
+  {
+    "revision": "5005d8b63222fec5ebeb",
+    "url": "/static/js/6.5005d8b6.chunk.js"
+  },
+  {
+    "revision": "103984e37ea3eb639e75fa35417ef233",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "442befe685b9f1af06c866e73c65b5ca",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "18a8f72a58babc64bc34dfe4d39f07b7",
+    "url": "/index.html"
+  }
+];
