01c49debbd404c657735a783e6a3110fd99d1dc9
Irvine Sunday <v-irsund@microsoft.com>
2019-10-03 14:22:45 +0300

    Adding action creators

---
+++ src/app/services/actions/terms-of-use-action-creator.ts
@@ -0,0 +1,10 @@
+import { Dispatch } from 'redux';
+import { CLEAR_TERMS_OF_USE } from '../redux-constants';
+
+export function clearTermsOfUse(): Function {
+    return (dispatch: Dispatch) => {
+      dispatch({
+        type: CLEAR_TERMS_OF_USE
+      });
+    };
+  }
