9316cc25fe27ad5effa6fc93e26cd01a73c96971
Irvine Sunday <v-irsund@microsoft.com>
2019-10-07 11:01:52 +0300

    Removing unnecessary state

--- src/tests/services/reducers/terms-of-use-reducer.spec.ts
+++ src/tests/services/reducers/terms-of-use-reducer.spec.ts
@@ -4,14 +4,13 @@ import { CLEAR_TERMS_OF_USE } from '../../../app/services/redux-constants';
 describe('Terms of Use Action Creators', () => {
     it('should return initial state', () => {
       const initialState = true;
-      const termsOfUseInfo = false;
 
       const dummyAction: any = {
-        type: CLEAR_TERMS_OF_USE, response: termsOfUseInfo
+        type: CLEAR_TERMS_OF_USE, response: false
       };
       const newState = termsOfUse(initialState, dummyAction);
 
-      expect(newState).toEqual(termsOfUseInfo);
+      expect(newState).toEqual(false);
 
     });
   });
