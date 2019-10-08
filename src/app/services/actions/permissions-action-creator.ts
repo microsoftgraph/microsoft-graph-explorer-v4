da234a2a40bd39b3e5320863158fb9e9d897e615
Irvine Sunday <v-irsund@microsoft.com>
2019-10-08 14:52:03 +0300

    Adding the permission action creators

---
+++ src/app/services/actions/permissions-action-creator.ts
@@ -0,0 +1,52 @@
+import { IAction } from '../../../types/action';
+import { IRequestOptions } from '../../../types/request';
+
+import { SCOPES_FETCH_ERROR,  SCOPES_FETCH_PENDING,  SCOPES_FETCH_SUCCESS } from '../redux-constants';
+
+export function fetchScopesSuccess(response: object): IAction {
+    return {
+      type: SCOPES_FETCH_SUCCESS,
+      response,
+    };
+  }
+
+  export function fetchScopesError(response: object): IAction {
+    return {
+      type: SCOPES_FETCH_ERROR,
+      response,
+    };
+  }
+
+  export function fetchScopesPending(): any {
+    return {
+      type: SCOPES_FETCH_PENDING
+    };
+  }
+
+  export function fetchScopes(): Function {
+    return async (dispatch: Function) => {
+      const permissionsUrl = 'https://graphexplorerapi.azurewebsites.net/api/GraphExplorerPermissions';
+
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+
+      const options: IRequestOptions = { headers };
+
+      dispatch(fetchScopesPending());
+
+      return fetch(permissionsUrl, options)
+        .then(res => res.json())
+        .then(res => {
+          if (res.error) {
+            throw (res.error);
+          }
+          dispatch(fetchScopesSuccess(res));
+          return res.products;
+        })
+        .catch(error => {
+          dispatch(fetchScopesError(error));
+        });
+
+    };
+  }
