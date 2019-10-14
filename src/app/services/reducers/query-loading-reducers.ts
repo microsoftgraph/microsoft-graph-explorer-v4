aedf9fbc97f318f261e7e3808edfed9bc9859a59
Irvine Sunday <v-irsund@microsoft.com>
2019-10-14 18:27:52 +0300

    Adding case for consent error action to allow query loading to stop

--- src/app/services/reducers/query-loading-reducers.ts
+++ src/app/services/reducers/query-loading-reducers.ts
@@ -1,11 +1,13 @@
 import { IAction } from '../../../types/action';
 import {
-  PROFILE_REQUEST_ERROR, PROFILE_REQUEST_SUCCESS,
-  QUERY_GRAPH_ERROR, QUERY_GRAPH_RUNNING, QUERY_GRAPH_SUCCESS, SCOPES_FETCH_ERROR
+  GET_CONSENT_ERROR, PROFILE_REQUEST_ERROR,
+  PROFILE_REQUEST_SUCCESS, QUERY_GRAPH_ERROR, QUERY_GRAPH_RUNNING, QUERY_GRAPH_SUCCESS, SCOPES_FETCH_ERROR
 } from '../redux-constants';
 
 export function isLoadingData(state = {}, action: IAction): any {
   switch (action.type) {
+    case GET_CONSENT_ERROR:
+      return false;
     case QUERY_GRAPH_RUNNING:
       if (action.response) {
         return action.response;
