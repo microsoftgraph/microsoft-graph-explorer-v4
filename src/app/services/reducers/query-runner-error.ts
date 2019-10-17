8b25d6a6b6ef8ecea7de998a7d29c22cbb669d68
Irvine Sunday <v-irsund@microsoft.com>
2019-10-17 14:29:40 +0300

    Re-arranging the imports alphabetically

--- src/app/services/reducers/query-runner-error.ts
+++ src/app/services/reducers/query-runner-error.ts
@@ -1,6 +1,6 @@
 import { IAction } from '../../../types/action';
-import { CLEAR_QUERY_ERROR, GET_CONSENT_ERROR,
-  QUERY_GRAPH_ERROR, QUERY_GRAPH_SUCCESS, FETCH_SCOPES_ERROR } from '../redux-constants';
+import { CLEAR_QUERY_ERROR, FETCH_SCOPES_ERROR,
+  GET_CONSENT_ERROR, QUERY_GRAPH_ERROR, QUERY_GRAPH_SUCCESS } from '../redux-constants';
 
 export function queryRunnerError(state = {}, action: IAction): any {
   switch (action.type) {
