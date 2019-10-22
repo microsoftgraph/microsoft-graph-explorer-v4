4f65db299633ac3485de594c8afae04892c07b47
Irvine Sunday <v-irsund@microsoft.com>
2019-10-22 18:37:54 +0300

    Error checks

--- src/app/views/query-runner/request/permissions/Permission.tsx
+++ src/app/views/query-runner/request/permissions/Permission.tsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
+import { Monaco } from '../../../common';
 import { fetchScopes } from './util';
 
 export function Permission() {
   const sample = useSelector((state: any) => state.sampleQuery, shallowEqual);
   const [permissions, setPermissions ] = useState([]);
   const [ loading, setLoading ] = useState(false);
+  const [sampleError, setError, ] = useState(false);
+  let errorMessage;
 
   useEffect(() => {
     setLoading(true);
+    setPermissions([]);
+    setError(false);
 
     fetchScopes(sample)
-      .then(res => { setLoading(false); setPermissions(res); });
+      .then(res => { setLoading(false); setPermissions(res); })
+      // .then(res => { if (res.error) {throw(res.error); }})
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+        setPermissions([]);
+        errorMessage = error;
+      });
   }, [sample.sampleUrl]);
 
 
   return (
     <div>
-      {loading && <h1>Fetching permissions...</h1>}
+      {sampleError && <Monaco body = {errorMessage} />}
+      {loading && <Monaco body = {'Fetching permissions...'}/>}
       {permissions &&
         <ul>
           {permissions.map((permission: string) => {
