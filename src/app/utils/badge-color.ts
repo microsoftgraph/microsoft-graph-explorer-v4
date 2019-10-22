6a8d2e433dd80a270ee2cdabfa21b360eca1ba59
Charles Wahome <thewahome.cw@gmail.com>
2019-10-22 16:43:04 +0300

    abstracts to a reusable function

---
+++ src/app/utils/badge-color.ts
@@ -0,0 +1,26 @@
+import { getTheme } from 'office-ui-fabric-react';
+
+export function getMethodStyle (method: string) {
+  const currentTheme = getTheme();
+
+  method = method.toString().toUpperCase();
+  switch (method) {
+    case 'GET':
+      return currentTheme.palette.green;
+
+    case 'POST':
+      return currentTheme.palette.blue;
+
+    case 'PUT':
+      return currentTheme.palette.magentaDark;
+
+    case 'PATCH':
+      return currentTheme.palette.yellowDark;
+
+    case 'DELETE':
+      return currentTheme.palette.redDark;
+
+    default:
+      return currentTheme.palette.green;
+  }
+}
