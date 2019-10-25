fa42452e7c3be5287da22fbf17b1480eae823b90
Japheth Obala <japhethobalak@gmail.com>
2019-10-25 18:40:42 +0300

    Reduces the list of default user scopes

--- src/app/services/graph-constants.ts
+++ src/app/services/graph-constants.ts
@@ -4,6 +4,4 @@ export const USER_PICTURE_URL = `${GRAPH_URL}/beta/me/photo/$value`;
 export const AUTH_URL = 'https://login.microsoftonline.com';
 export const ADMIN_AUTH_URL = 'https://signIn.microsoftonline.com/common/adminconsent';
 export const TOKEN_URL = 'https://signIn.microsoftonline.com/common/oauth2/v2.0/token';
-export const DEFAULT_USER_SCOPES = 'openid profile User.ReadWrite User.ReadBasic.All Sites.ReadWrite.All ' +
-  'Contacts.ReadWrite People.Read Notes.ReadWrite.All Tasks.ReadWrite Mail.ReadWrite Files.ReadWrite.All' +
-  ' Calendars.ReadWrite';
+export const DEFAULT_USER_SCOPES = 'openid profile User.Read';
