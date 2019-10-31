f5b7fcbc23380e189a85929e022606a977ea6753
Japheth Obala <japhethobalak@gmail.com>
2019-10-31 16:11:42 +0300

    Sets localStorage as cache location

--- src/app/services/graph-client/msal-service.ts
+++ src/app/services/graph-client/msal-service.ts
@@ -3,8 +3,6 @@ import { DEFAULT_USER_SCOPES } from '../graph-constants';
 import { msalApplication } from './msal-agent';
 
 const defaultUserScopes = DEFAULT_USER_SCOPES.split(' ');
-const loginType = getLoginType();
-msalApplication.handleRedirectCallback(authCallback);
 
 export async function logIn(): Promise<any> {
   const loginRequest: AuthenticationParameters = {
@@ -12,10 +10,6 @@ export async function logIn(): Promise<any> {
   };
 
   return msalApplication.loginRedirect(loginRequest);
-}
-
-function authCallback(error: any, response: any) {
-  return;
 }
 
 export function logOut() {
@@ -33,23 +27,6 @@ export async function getTokenSilent(userAgentApp: UserAgentApplication,
  *  @returns {Promise.<any>}
  */
 export async function acquireNewAccessToken(userAgentApp: UserAgentApplication, scopes: string[] = []): Promise<any> {
-  const loginRequest: AuthenticationParameters = {
-    scopes,
-  };
-
-  // getTokenSilent(userAgentApp, scopes)).catch ((error) => {
-  //   if (requiresInteraction(error.errorCode)) {
-  //     if (loginType === 'POPUP') {
-  //       try {
-  //         return userAgentApp.acquireTokenPopup({ scopes: generateUserScopes(listOfScopes) });
-  //       } catch (error) {
-  //         throw error;
-  //       }
-  //     } else if (loginType === 'REDIRECT') {
-  //       userAgentApp.acquireTokenRedirect({ scopes: generateUserScopes(listOfScopes) });
-  //     }
-  //   }
-  // }; )
   getTokenSilent(userAgentApp, scopes)
     .catch((error: any) => {
       if (requiresInteraction(error.errorCode)) {
@@ -74,16 +51,3 @@ function requiresInteraction(error: any): boolean {
     errorCode === 'token_renewal_error';
 }
 
-/**
- * Returns whether to load the POPUP/REDIRECT interaction
- * @returns string
- */
-export function getLoginType() {
-  const userAgent = window.navigator.userAgent;
-  const msie = userAgent.indexOf('MSIE ');
-  const msie11 = userAgent.indexOf('Trident/');
-  const msedge = userAgent.indexOf('Edge/');
-  const isIE = msie > 0 || msie11 > 0;
-  const isEdge = msedge > 0;
-  return isIE || isEdge ? 'REDIRECT' : 'POPUP';
-}
