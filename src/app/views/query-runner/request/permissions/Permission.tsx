77863a0bea576096ea5c2920c67c0febecc51b98
Irvine Sunday <v-irsund@microsoft.com>
2019-10-31 16:17:36 +0300

    Removing unnecessary variable

--- src/app/views/query-runner/request/permissions/Permission.tsx
+++ src/app/views/query-runner/request/permissions/Permission.tsx
@@ -24,7 +24,6 @@ export function Permission({}) {
   const consentedScopes: string[] = useSelector((state: any) => state.consentedScopes);
   const [permissions, setPermissions ] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [sampleError, setError] = useState(false);
   const columns = [
     { key: 'value', name: 'Name', fieldName: 'value', minWidth: 100, maxWidth: 150 },
     { key: 'consentDisplayName', name: 'Function', fieldName: 'consentDisplayName', isResizable: true,
