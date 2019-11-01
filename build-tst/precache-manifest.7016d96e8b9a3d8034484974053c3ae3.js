9f66023a1142a1f2e94e7e837bf4cd4e7ab9c26d
Japheth Obala <japhethobalak@gmail.com>
2019-11-01 18:55:08 +0300

    Adds build

---
+++ build-tst/precache-manifest.7016d96e8b9a3d8034484974053c3ae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "241c221ef12501fe6c05",
+    "url": "/static/js/graph-explorer-v2.js"
+  },
+  {
+    "revision": "241c221ef12501fe6c05",
+    "url": "/static/css/graph-explorer-v2.css"
+  },
+  {
+    "revision": "082278a782d2c7a2386295eb564c686f",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "d9d9758c7a6e048ffe961fe269dcaef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28e6c7afa9f611b429f0b41718e489a",
+    "url": "/editor.worker.js"
+  }
+];
--- build-tst/service-worker.js
+++ build-tst/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c908c275a2685d5ccf9a2bab21fa755.js"
+  "/precache-manifest.7016d96e8b9a3d8034484974053c3ae3.js"
 );
 
 workbox.clientsClaim();
