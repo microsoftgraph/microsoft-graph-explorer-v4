50cd99871a177f4ca57596f8d9ba08b2c463bc12
Japheth Obala <japhethobalak@gmail.com>
2019-11-26 10:34:15 +0300

    Deduplicates copy functionality

--- src/app/views/App.tsx
+++ src/app/views/App.tsx
@@ -127,7 +127,13 @@ class App extends Component<IAppProps, IAppState> {
   }
 
   private hashDecode(requestBody: string): string {
-    return requestBody ? atob(requestBody) : '';
+    const decodedBody = atob(requestBody);
+
+    if (!decodedBody) {
+      return '';
+    }
+
+    return decodedBody;
   }
 
   public componentWillUnmount(): void {
