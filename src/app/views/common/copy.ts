5d88595f6120b0c47da0d0e5ee598affb3534587
Japheth Obala <japhethobalak@gmail.com>
2019-11-27 10:53:20 +0300

    Extends the copy method with genericCopy

--- src/app/views/common/copy.ts
+++ src/app/views/common/copy.ts
@@ -1,12 +1,32 @@
-export const copy = (id: string): Promise<any> => {
-  const shareQueryParams: any = document.getElementById(id);
-  shareQueryParams.focus();
-  shareQueryParams.select();
+export const copy = (id: string, text?: string): Promise<any> => {
+  if (text) {
+    return genericCopy(text);
+  }
+  
+  return copyTextArea(id);
+};
+
+function genericCopy(text: string) {
+  const element = document.createElement('textarea');
+  element.value = text;
+  document.body.appendChild(element);
+  element.select();
+
+  document.execCommand('copy');
+  document.body.removeChild(element);
+  
+  return Promise.resolve('copied');
+}
+
+function copyTextArea(id: string) {
+  const textArea: any = document.getElementById(id);
+  textArea.focus();
+  textArea.select();
 
   document.execCommand('copy');
   document.execCommand('unselect');
 
-  shareQueryParams.blur();
+  textArea.blur();
 
   return Promise.resolve('copied');
-};
+}
