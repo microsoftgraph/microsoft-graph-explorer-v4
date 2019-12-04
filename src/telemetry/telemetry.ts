db8e01c0675ff6292b461c27d6c35a9566cbc343
Japheth Obala <japhethobalak@gmail.com>
2019-12-04 10:00:00 +0300

    Adds telemetry.ts and telemetry-event.ts

---
+++ src/telemetry/telemetry.ts
@@ -0,0 +1,37 @@
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+
+class Telemetry {
+  private appInsights: ApplicationInsights;
+
+  constructor() {
+    this.appInsights = new ApplicationInsights({
+      config: {
+        instrumentationKey: 'a800ae98-f89e-4f96-b491-cf1b8a989bff',
+      }
+    });
+  }
+
+  public startCollectingData() {
+    this.appInsights.loadAppInsights();
+  }
+
+  public collect(eventName: string, payload: any) {
+    if (!this.valid(eventName)) {
+      throw new Error('Invalid telemetry event name');
+    }
+
+    // @ts-ignore
+    this.appInsights.trackEvent(eventName, payload);
+  }
+
+  // A valid event name ends with the word EVENT
+  private valid(eventName: string) {
+    const listOfWords = eventName.split('_');
+    const lastIndex = listOfWords.length - 1;
+    const lastWord = listOfWords[lastIndex];
+    return lastWord === 'EVENT';
+  }
+}
+
+export default new Telemetry();
