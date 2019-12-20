49a8dff7aa92cb1551e7937dec26406a0a3cb027
Charles Wahome <thewahome.cw@gmail.com>
2019-12-20 09:48:47 +0300

    creates a default theme variable

--- src/store/theme-cache.ts
+++ src/store/theme-cache.ts
@@ -1,10 +1,11 @@
 const key = 'CURRENT_THEME';
+const defaultTheme = 'light';
 
 export function saveTheme(theme: string) {
   localStorage.setItem(key, theme);
 }
 
 export function readTheme() {
-  const theme = localStorage.getItem(key) || 'light';
+  const theme = localStorage.getItem(key) || defaultTheme;
   return theme;
 }
