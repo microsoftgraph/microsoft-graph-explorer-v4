2ae6cc2d23e83f48ea6c147737edab716455e5f2
jobala <japhethobalak@gmail.com>
2020-02-06 18:20:48 -0800

    Uncomment test setup

--- src/tests/accessibility/accessibility.spec.ts
+++ src/tests/accessibility/accessibility.spec.ts
@@ -10,39 +10,27 @@ const TEST_TIMEOUT_MS = 30000;
 describe('Graph Explorer', () => {
   let driver: ThenableWebDriver;
 
-  // beforeAll(async () => {
-  //   chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
-
-  //   driver = new webdriver
-  //   .Builder()
-  //   .withCapabilities(webdriver.Capabilities.chrome())
-  //   .build();
-  // }, TEST_TIMEOUT_MS);
-
-  // afterAll(async() => {
-  //   return driver && driver.quit();
-  // }, TEST_TIMEOUT_MS);
-
-  // beforeEach(async() => {
-  //   await driver.get('https://developer.microsoft.com/en-us/graph/graph-explorer/preview');
-  //   await driver.wait(until.elementLocated(By.css('h1')));
-  // }, TEST_TIMEOUT_MS);
-
-  it('checks for accessibility violations', async() => {
+  beforeAll(async () => {
     chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
 
     driver = new webdriver
     .Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
+  }, TEST_TIMEOUT_MS);
 
+  afterAll(async() => {
+    return driver && driver.quit();
+  }, TEST_TIMEOUT_MS);
+
+  beforeEach(async() => {
     await driver.get('https://developer.microsoft.com/en-us/graph/graph-explorer/preview');
     await driver.wait(until.elementLocated(By.css('h1')));
+  }, TEST_TIMEOUT_MS);
 
+  it('checks for accessibility violations', async() => {
     const accessibilityScanResults = await AxeBuilder(driver)
       .analyze();
     expect(accessibilityScanResults.violations).toStrictEqual([]);
-
-    return driver && driver.quit();
   });
 });
