5970c176749784381167c77a90527733789b4cf2
Charles Wahome <thewahome.cw@gmail.com>
2020-03-05 15:14:53 +0300

    sets the production url as the share location

--- src/app/views/common/share.ts
+++ src/app/views/common/share.ts
@@ -1,17 +1,17 @@
 import { IQuery } from '../../../types/query-runner';
 
 export const createShareLink = (sampleQuery: IQuery): string => {
-  const { origin, pathname } = window.location;
   const { sampleUrl, sampleBody, selectedVerb, selectedVersion } = sampleQuery;
   const url = new URL(sampleUrl);
   const graphUrl = url.origin;
+  const appUrl = 'https://developer.microsoft.com/graph/graph-explorer/preview';
   /**
    * To ensure backward compatibility the version is removed from the pathname.
    * V3 expects the request query param to not have the version number.
    */
   const graphUrlRequest = url.pathname.substr(6) + url.search;
   const requestBody = hashEncode(JSON.stringify(sampleBody));
-  return origin + pathname
+  return appUrl
     + '?request=' + graphUrlRequest
     + '&method=' + selectedVerb
     + '&version=' + selectedVersion
