872baeb996aa9f8f0b210d28f22eda605aa53a86
Charles Wahome <thewahome.cw@gmail.com>
2020-03-13 11:21:15 +0300

    adds checks to url generation steps

--- src/app/utils/sample-url-generation.ts
+++ src/app/utils/sample-url-generation.ts
@@ -7,20 +7,34 @@ export function parseSampleUrl(url: string, version?: string) {
   let search = '';
 
   if (url !== '') {
-    const urlObject: URL = new URL(url);
-    requestUrl = urlObject.pathname.substr(6).replace(/\/$/, '');
-    queryVersion = (version) ? version : urlObject.pathname.substring(1, 5);
-
-    const searchParameters = urlObject.search;
-    if (searchParameters) {
-      try {
-        search = decodeURI(searchParameters);
-      } catch (error) {
-        search = searchParameters;
-      }
+    try {
+      const urlObject: URL = new URL(url);
+      requestUrl = urlObject.pathname.substr(6).replace(/\/$/, '');
+      queryVersion = (version) ? version : urlObject.pathname.substring(1, 5);
+      search = generateSearchParameters(urlObject, search);
+      sampleUrl = `${GRAPH_URL}/${queryVersion}/${requestUrl + search}`;
+    } catch (error) {
+      return {
+        queryVersion, requestUrl, sampleUrl, search
+      };
     }
-
-    sampleUrl = `${GRAPH_URL}/${queryVersion}/${requestUrl + search}`;
   }
-  return { queryVersion, requestUrl, sampleUrl, search };
+
+  return {
+    queryVersion, requestUrl, sampleUrl, search
+  };
 }
+
+function generateSearchParameters(urlObject: URL, search: string) {
+  const searchParameters = urlObject.search;
+  if (searchParameters) {
+    try {
+      search = decodeURI(searchParameters);
+    }
+    catch (error) {
+      search = searchParameters;
+    }
+  }
+  return search;
+}
+
