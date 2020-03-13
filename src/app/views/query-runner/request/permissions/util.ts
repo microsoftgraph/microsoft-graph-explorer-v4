d187025717d758200bdb2022b5f069bfb9782dc2
Charles Wahome <thewahome.cw@gmail.com>
2020-03-13 13:30:02 +0300

    prevents fetching of scopes if url is invalid

--- src/app/views/query-runner/request/permissions/util.ts
+++ src/app/views/query-runner/request/permissions/util.ts
@@ -1,20 +1,22 @@
 import { IQuery } from '../../../../../types/query-runner';
 import { IRequestOptions } from '../../../../../types/request';
+import { parseSampleUrl } from '../../../../utils/sample-url-generation';
 
 export function fetchScopes(sample: IQuery): Promise<any> {
-    const { sampleUrl, selectedVerb } = sample;
-    const urlObject: URL = new URL(sampleUrl);
-    // remove the prefix i.e. beta or v1.0 and any possible extra '/' character at the end
-    const requestUrl = urlObject.pathname.substr(5).replace(/\/$/, '');
-    const permissionsUrl = 'https://graphexplorerapi.azurewebsites.net/api/GraphExplorerPermissions?requesturl=' +
-      requestUrl + '&method=' + selectedVerb;
+  const { requestUrl, sampleUrl } = parseSampleUrl(sample.sampleUrl);
+  if (!sampleUrl) {
+    Promise.reject('url is invalid');
+  }
 
-    const headers = {
-      'Content-Type': 'application/json',
-    };
+  const permissionsUrl = 'https://graphexplorerapi.azurewebsites.net/api/GraphExplorerPermissions?requesturl=' +
+    requestUrl + '&method=' + sample.selectedVerb;
 
-    const options: IRequestOptions = { headers };
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
-    return fetch(permissionsUrl, options)
-      .then(res => res.json());
+  const options: IRequestOptions = { headers };
+
+  return fetch(permissionsUrl, options)
+    .then(res => res.json());
 }
