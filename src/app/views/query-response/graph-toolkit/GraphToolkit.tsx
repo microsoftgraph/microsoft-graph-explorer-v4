cd8ba9fdaca18eca42db59dce4beb017fb15a64c
Charles Wahome <thewahome.cw@gmail.com>
2020-03-23 16:59:13 +0300

    move implementation to json file and use succinct code

--- src/app/views/query-response/graph-toolkit/GraphToolkit.tsx
+++ src/app/views/query-response/graph-toolkit/GraphToolkit.tsx
@@ -5,12 +5,13 @@ import { connect } from 'react-redux';
 import { Label } from 'office-ui-fabric-react';
 import { IQuery } from '../../../../types/query-runner';
 import { parseSampleUrl } from '../../../utils/sample-url-generation';
+import templates from './toolkit';
 
-const templateMap: any = {
-  '/me/planner/tasks' : 'https://mgt.dev/iframe.html?id=components-mgt-tasks--tasks',
-  '/me': 'https://mgt.dev/iframe.html?id=components-mgt-person-card--person-card-hover',
-  '/me/people' : 'https://mgt.dev/iframe.html?id=components-mgt-people--people',
-};
+// const templateMap: any = {
+//   '/me/planner/tasks' : 'https://mgt.dev/iframe.html?id=components-mgt-tasks--tasks',
+//   '/me': 'https://mgt.dev/iframe.html?id=components-mgt-person-card--person-card-hover',
+//   '/me/people' : 'https://mgt.dev/iframe.html?id=components-mgt-people--people',
+// };
 
 class GraphToolkit extends Component<any> {
   constructor(props: any) {
@@ -20,16 +21,10 @@ class GraphToolkit extends Component<any> {
   public lookupToolkitUrl = (sampleQuery: IQuery): any => {
     if (sampleQuery) {
       const { requestUrl, search } = parseSampleUrl(sampleQuery.sampleUrl);
-      const query = requestUrl + search;
-      // find if the url of the request has a template mapped to it
-      for (const templateMapKey in templateMap) {
-        if (templateMap.hasOwnProperty(templateMapKey)) {
-          // check if the template matches a specific pattern while ignoring case
-          const isMatch = new RegExp(templateMapKey + '$', 'i').test('/' + query);
-          if (isMatch) {
-            return templateMap[templateMapKey];
-          }
-        }
+      const query = '/' + requestUrl + search;
+      const url: string = (templates as any)[query];
+      if (url) {
+        return url;
       }
     }
     return null;
@@ -44,6 +39,7 @@ class GraphToolkit extends Component<any> {
         <iframe width='100%' height='470px' src={toolkitUrl} />
       );
     }
+
     return (
       <Label style={{
         display: 'flex',
