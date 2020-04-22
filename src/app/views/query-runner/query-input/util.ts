26b19ce097d67e820b5d3fe254cc60c04f9e86bc
Charles Wahome <thewahome.cw@gmail.com>
2020-04-22 16:43:21 +0300

    enable search as you type filtering

---
+++ src/app/views/query-runner/query-input/util.ts
@@ -0,0 +1,24 @@
+export function cleanUpSelectedSuggestion(compare: string, userInput: string, selected: string) {
+    let finalSelectedSuggestion = `${userInput + selected}`;
+    if (compare) {
+    /**
+     * Removes the characters used to filter the suggestions
+     * and replaces them with the provided suggestion
+     * together with the sign that was in the query string before
+     * autosuggestion
+     */
+      const availableSignPrefixes = ['?', ',', '='];
+      let entryToRemove = '';
+      let activeSign = '?';
+      availableSignPrefixes.forEach(sign => {
+        const compareStringWithSign = `${sign + compare}`;
+        if (userInput.includes(compareStringWithSign)) {
+          entryToRemove = compareStringWithSign;
+          activeSign = sign;
+        }
+      });
+      finalSelectedSuggestion = userInput.replace(entryToRemove, activeSign);
+      return finalSelectedSuggestion + selected;
+    }
+    return finalSelectedSuggestion;
+  }
