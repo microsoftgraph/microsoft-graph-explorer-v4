c0edf8be41d4cc8d2c1cbc4d6e693bdc9b074703
Charles Wahome <thewahome.cw@gmail.com>
2020-04-23 18:38:08 +0300

    improve styling to resemble dropdown

--- src/app/views/query-runner/query-input/QueryInput.styles.ts
+++ src/app/views/query-runner/query-input/QueryInput.styles.ts
@@ -1,7 +1,7 @@
 import { ITheme } from '@uifabric/styling';
 
 export const queryInputStyles = (theme: ITheme) => {
-  const controlWidth = '300px';
+  const controlWidth = '350px';
     return {
       autoComplete: {
         input: {
@@ -12,7 +12,7 @@ export const queryInputStyles = (theme: ITheme) => {
           padding: 10
         },
         suggestions: {
-          maxHeight: '143px',
+          maxHeight: '250px',
           overflowY: 'auto',
           paddingLeft: 0,
           position: 'absolute',
@@ -27,16 +27,69 @@ export const queryInputStyles = (theme: ITheme) => {
             ':hover': {
               background: theme.palette.neutralLight,
             },
-            padding: 10,
-            cursor: 'pointer',
-            backgroundColor: theme.palette.neutralLighter,
-          },
-          suggestionActive: {
-            padding: 10,
-            display: 'block',
-            backgroundColor: theme.palette.neutralLight,
-            cursor: 'pointer',
+          padding: 10,
+          cursor: 'pointer',
+          backgroundColor: theme.palette.white,
+          boxShadow: 'none',
+          marginTop: '0px',
+          marginRight: '0px',
+          marginBottom: '0px',
+          marginLeft: '0px',
+          paddingTop: '0px',
+          paddingRight: '32px',
+          paddingBottom: '0px',
+          paddingLeft: '12px',
+          boxSizing: 'border-box',
+          height: '32px',
+          lineHeight: '30px',
+          position: 'relative',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+          borderWidth: '1px',
+          borderStyle: 'solid',
+          borderColor: theme.palette.neutralLight,
+          overflow: 'hidden',
         },
+        suggestionActive: {
+          display: 'block',
+          ':hover': {
+            background: theme.palette.neutralLight,
+          },
+          padding: 10,
+          cursor: 'pointer',
+          boxShadow: 'none',
+          marginTop: '0px',
+          marginRight: '0px',
+          marginBottom: '0px',
+          marginLeft: '0px',
+          paddingTop: '0px',
+          paddingRight: '32px',
+          paddingBottom: '0px',
+          paddingLeft: '12px',
+          boxSizing: 'border-box',
+          height: '32px',
+          lineHeight: '30px',
+          position: 'relative',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+          borderWidth: '1px',
+          borderStyle: 'solid',
+          overflow: 'hidden',
+          backgroundColor: theme.palette.neutralLight,
+          borderColor: theme.palette.neutralLight,
+          // borderBottom:
+          borderTopColor: theme.palette.blue,
+          borderBottomColor: theme.palette.blue,
+          // verticalAlign: 'baseline'
+        },
+        suggestionTitle: {
+          // paddingBottom: 5,
+          display: 'flex',
+          height: '100%',
+          flexWrap: 'nowrap',
+          justifyContent: 'flex-start',
+          alignItems: 'center'
+        }
       }
     };
   };
