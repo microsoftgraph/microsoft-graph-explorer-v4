dd23c2abcde083427c7343d84be6a46918e75b7e
Charles Wahome <thewahome.cw@gmail.com>
2020-04-27 16:06:40 +0300

    enhance open api parser to return verbs and values

--- src/app/utils/open-api-parser.ts
+++ src/app/utils/open-api-parser.ts
@@ -1,29 +1,37 @@
 export function parseOpenApiResponse(params: any) {
-    const { options: { paths }, url, verb } = params;
+    const { options: { paths }, url } = params;
 
     try {
       const parameters: any[] = [];
-      let rootPath = url;
-      if (url.includes('me/drive')) {
-        rootPath = url.substring(3);
-      }
+      const requestUrl = Object.keys(paths)[0];
+      const verbs = Object.keys(paths[`${requestUrl}`]);
+      const pathValues: any = Object.values(paths)[0];
 
-      const root = paths[`/${rootPath}`];
-      const verbContent = root[`${verb}`];
-      const queryParams = verbContent.parameters;
-      if (queryParams.length > 0) {
-        queryParams.forEach((param: any) => {
-          if (param.name) {
-            const parameter = {
-              name: param.name,
-              items: param.items.enum || null
-            };
-            parameters.push(parameter);
-          }
+      verbs.forEach((verb: string) => {
+        parameters.push({
+          verb,
+          values: getVerbParameterValues(pathValues[`${verb}`])
         });
-      }
-      return { url, parameters, verb };
+      });
+
+      return { url, parameters };
     } catch (error) {
       return { error };
     }
+  }
+
+  function getVerbParameterValues(values: any) {
+    const parameterValues: any[] = [];
+      const queryParameters = values.parameters;
+      if (queryParameters.length > 0) {
+        queryParameters.forEach((parameter: any) => {
+          if (parameter.name) {
+            parameterValues.push({
+              name: parameter.name,
+              items: (parameter.items && parameter.items.enum) ? parameter.items.enum : null
+            });
+          }
+        });
+      }
+    return parameterValues;
   }
