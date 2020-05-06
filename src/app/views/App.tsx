1b7a8eb2f13f22970b367da0f283e2f0e92e0087
Charles Wahome <thewahome.cw@gmail.com>
2020-05-06 10:43:50 +0300

    add try it check when displaying authentication

--- src/app/views/App.tsx
+++ src/app/views/App.tsx
@@ -393,7 +393,7 @@ class App extends Component<IAppProps, IAppState> {
             <div className={layout}>
               {graphExplorerMode.TryIt && headerMessaging(loginType, classes, query)}
 
-              {this.displayAuthenticationSection(minimised)}
+              {graphExplorerMode === Mode.TryIt && this.displayAuthenticationSection(minimised)}
 
               {displayContent && <>
                 <div style={{ marginBottom: 8 }}>
