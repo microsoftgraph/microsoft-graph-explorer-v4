b6d89b4f10d49bbfc64f57c8be28a1bb4ac95c2e
Charles Wahome <thewahome.cw@gmail.com>
2020-05-08 23:34:41 +0300

    create build files for 4.1.1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09207e68141989280db47ff10042ddd8.js"
+  "/precache-manifest.974b60cb18942dc95d39e7b12b7ec000.js"
 );
 
 workbox.clientsClaim();
