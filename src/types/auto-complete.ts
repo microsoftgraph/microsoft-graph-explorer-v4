9c87c9f7ab1f1a00e3c23872337121e8f216c419
Charles Wahome <thewahome.cw@gmail.com>
2020-07-08 17:28:15 +0300

    makes the autocorrect input larger

--- src/types/auto-complete.ts
+++ src/types/auto-complete.ts
@@ -1,25 +1,26 @@
 import { IQuery } from './query-runner';
 
 export interface IAutoCompleteProps {
-    suggestions: string[];
-    contentChanged: Function;
-    sampleQuery: IQuery;
-    fetchingSuggestions: boolean;
-    autoCompleteOptions: {
-      url: string;
-      parameters: any[];
-    };
-    actions?: {
-      fetchAutoCompleteOptions: Function;
-    };
-  }
+  suggestions: string[];
+  contentChanged: Function;
+  runQuery: Function;
+  sampleQuery: IQuery;
+  fetchingSuggestions: boolean;
+  autoCompleteOptions: {
+    url: string;
+    parameters: any[];
+  };
+  actions?: {
+    fetchAutoCompleteOptions: Function;
+  };
+}
 
 export interface IAutoCompleteState {
-    activeSuggestion: number;
-    filteredSuggestions: string[];
-    suggestions: string[];
-    showSuggestions: boolean;
-    userInput: string;
-    compare: string;
+  activeSuggestion: number;
+  filteredSuggestions: string[];
+  suggestions: string[];
+  showSuggestions: boolean;
+  userInput: string;
+  compare: string;
 
 }
