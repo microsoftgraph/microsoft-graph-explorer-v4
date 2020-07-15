ea69da9c1534aa81f9a059d3f61aa4b96677b569
jobala <japhethobalak@gmail.com>
2020-07-15 13:16:44 +0300

    Add spacing between buttons.

--- src/app/views/settings/Settings.tsx
+++ src/app/views/settings/Settings.tsx
@@ -133,7 +133,7 @@ function Settings(props: ISettingsProps) {
     return (
       <div>
         <Label>{getSelectionDetails()}</Label>
-        <PrimaryButton disabled={selectedPermissions.length === 0} onClick={() => handleConsent()}>
+        <PrimaryButton disabled={selectedPermissions.length === 0} onClick={() => handleConsent()} style={{ marginRight: 10 }}>
           <FormattedMessage id='Consent' />
         </PrimaryButton>
         <DefaultButton onClick={() => togglePermissionsPanel()}>
