d9413a7cd74be34c6d901812e28e3e5a578de58f
Charles Wahome <thewahome.cw@gmail.com>
2020-08-07 20:30:50 +0300

    create 4.1.6 build files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e58105c0374e915f17b786d5169db88.js"
+  "/precache-manifest.60c5a133b3b92a016d4198102e8c4acb.js"
 );
 
 workbox.clientsClaim();
@@ -29,6 +29,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-
+  
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
