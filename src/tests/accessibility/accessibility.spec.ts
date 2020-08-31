9f105e81fff867f5bd63c9fd232850e65d96935c
Elinor <ekaguongo@gmail.com>
2020-08-31 20:13:30 +0300

    add comments

--- src/tests/accessibility/accessibility.spec.ts
+++ src/tests/accessibility/accessibility.spec.ts
@@ -9,6 +9,7 @@ const TEST_TIMEOUT_MS = 300000;
 describe('Graph Explorer', () => {
   let driver: ThenableWebDriver;
 
+  // set browser environment to use headless Chrome
   beforeAll(async () => {
     chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
 
@@ -20,20 +21,22 @@ describe('Graph Explorer', () => {
     .build();
   }, TEST_TIMEOUT_MS);
 
+  // end browser environment
   afterAll(async() => {
     return driver && driver.quit();
   }, TEST_TIMEOUT_MS);
 
+  // load the page where app is hosted
   beforeEach(async() => {
     await driver.manage().setTimeouts( { implicit: 0, pageLoad:
       60000, script: TEST_TIMEOUT_MS });
     await driver.get('http://localhost:3000/');
   }, TEST_TIMEOUT_MS);
 
+  // scan the page and return an analysis
   it('checks for accessibility violations', async() => {
     // @ts-ignore
     const accessibilityScanResults = await AxeBuilder(driver)
-      .include('#root')
       .analyze();
     expect(accessibilityScanResults.violations).toStrictEqual([]);
   });
