849509d1a5f0c0167317b3ba1cc294f015c8ffbc
Millicent Achieng <achieng.milli@gmail.com>
2020-10-04 06:35:59 +0300

    Enable telemetry

--- src/telemetry/telemetry.ts
+++ src/telemetry/telemetry.ts
@@ -14,7 +14,7 @@ class Telemetry implements ITelemetry {
     this.config = {
       instrumentationKey: this.getInstrumentationKey(),
       disableExceptionTracking: true,
-      disableTelemetry: true,
+      disableTelemetry: false,
       extensions: [this.reactPlugin]
     };
 
@@ -26,7 +26,7 @@ class Telemetry implements ITelemetry {
   public initialize() {
     this.appInsights.loadAppInsights();
     this.appInsights.addTelemetryInitializer(this.filterFunction);
-    // this.appInsights.trackPageView();
+    this.appInsights.trackPageView();
   }
 
   public trackEvent(eventName: string, payload: any) {
