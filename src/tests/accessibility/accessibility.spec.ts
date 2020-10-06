5e558be850189d41912bce5d3c719c1400b9eec6
Elinor <ekaguongo@gmail.com>
2020-10-06 11:27:08 +0300

    change path

--- src/tests/accessibility/accessibility.spec.ts
+++ src/tests/accessibility/accessibility.spec.ts
@@ -6,35 +6,38 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000;
 const TEST_TIMEOUT_MS = 300000;
 
 describe('Graph Explorer', () => {
-   let driver: ThenableWebDriver;
+  let driver: ThenableWebDriver;
 
   // set browser environment to use headless Chrome
   beforeAll(async () => {
-    chrome.setDefaultService(new chrome.ServiceBuilder('C:\\SeleniumWebDrivers\\ChromeDriver\\chromedriver.exe')
-    .build());
+    chrome.setDefaultService(
+      new chrome.ServiceBuilder(
+        '\\usr\\lib\\chromium-browser\\chromedriver'
+      ).build()
+    );
 
-    driver = new webdriver
-    .Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(new Options().headless())
-    .withCapabilities(webdriver.Capabilities.chrome())
-    .build();
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new Options().headless())
+      .withCapabilities(webdriver.Capabilities.chrome())
+      .build();
   }, TEST_TIMEOUT_MS);
 
   // end browser environment
-  afterAll(async() => {
+  afterAll(async () => {
     return driver && driver.quit();
   }, TEST_TIMEOUT_MS);
 
   // load the page where app is hosted
-  beforeEach(async() => {
-    await driver.manage().setTimeouts( { implicit: 0, pageLoad:
-      60000, script: TEST_TIMEOUT_MS });
+  beforeEach(async () => {
+    await driver
+      .manage()
+      .setTimeouts({ implicit: 0, pageLoad: 60000, script: TEST_TIMEOUT_MS });
     await driver.get('http://localhost:3000/');
   }, TEST_TIMEOUT_MS);
 
   // scan the page and return an analysis
-  it('checks for accessibility violations', async() => {
+  it('checks for accessibility violations', async () => {
     // @ts-ignore
     const accessibilityScanResults = await AxeBuilder(driver)
       .disableRules(['landmark-one-main', 'region']) // disabled as main landmark already exists on live site
