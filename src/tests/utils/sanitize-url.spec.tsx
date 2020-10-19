588e13d8aefcf1516ea2520fd2740dfbcf6f4545
Charles Wahome <thewahome.cw@gmail.com>
2020-10-19 13:09:57 +0300

    add property for expected result

--- src/tests/utils/sanitize-url.spec.tsx
+++ src/tests/utils/sanitize-url.spec.tsx
@@ -5,14 +5,17 @@ describe('Sanitize Query Url should', () => {
     {
       check: 'without task id',
       url: 'https://graph.microsoft.com/v1.0/planner/tasks/oIx3zN98jEmVOM-4mUJzSGUANeje',
+      sanitized: 'https://graph.microsoft.com/v1.0/planner/tasks/<id>',
     },
     {
       check: 'without email',
       url: 'https://graph.microsoft.com/v1.0/users/MiriamG@M365x214355.onmicrosoft.com',
+      sanitized: 'https://graph.microsoft.com/v1.0/users/<email>',
     },
     {
       check: 'without message id',
-      url: 'https://graph.microsoft.com/v1.0/me/messages/AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEMAAAiIsqMbYjsT5e-T7KzowPTAAMCzwJpAAA='
+      url: 'https://graph.microsoft.com/v1.0/me/messages/AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEMAAAiIsqMbYjsT5e-T7KzowPTAAMCzwJpAAA=',
+      sanitized: 'https://graph.microsoft.com/v1.0/me/messages/<id>'
     }
   ];
 
@@ -21,7 +24,7 @@ describe('Sanitize Query Url should', () => {
 
     it(`return url ${element.check}`, () => {
       const normalizedUrl = sanitizeQueryUrl(element.url);
-      expect(normalizedUrl).not.toEqual(element.url);
+      expect(normalizedUrl).toEqual(element.sanitized);
     });
 
   }
