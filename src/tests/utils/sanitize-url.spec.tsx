51cbc7d350c666dabff4d7cfdff56c9ba5ab63f4
Charles Wahome <thewahome.cw@gmail.com>
2020-10-19 23:33:54 +0300

    add tests for the queries

--- src/tests/utils/sanitize-url.spec.tsx
+++ src/tests/utils/sanitize-url.spec.tsx
@@ -1,32 +1,40 @@
-import { sanitizeQueryUrl } from "../../app/utils/query-url-sanitization";
+import { sanitizeQueryUrl } from '../../app/utils/query-url-sanitization';
 
 describe('Sanitize Query Url should', () => {
   const list = [
     {
       check: 'without task id',
       url: 'https://graph.microsoft.com/v1.0/planner/tasks/oIx3zN98jEmVOM-4mUJzSGUANeje',
-      sanitized: 'https://graph.microsoft.com/v1.0/planner/tasks/<id>',
+      sanitized: 'https://graph.microsoft.com/v1.0/planner/tasks/{tasks-id}',
     },
     {
       check: 'without email',
       url: 'https://graph.microsoft.com/v1.0/users/MiriamG@M365x214355.onmicrosoft.com',
-      sanitized: 'https://graph.microsoft.com/v1.0/users/<email>',
+      sanitized: 'https://graph.microsoft.com/v1.0/users/{users-id}',
     },
     {
       check: 'without message id',
+      // tslint:disable-next-line: max-line-length
       url: 'https://graph.microsoft.com/v1.0/me/messages/AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEMAAAiIsqMbYjsT5e-T7KzowPTAAMCzwJpAAA=',
-      sanitized: 'https://graph.microsoft.com/v1.0/me/messages/<id>'
+      sanitized: 'https://graph.microsoft.com/v1.0/me/messages/{messages-id}'
+    },
+    {
+      check: 'without extension id',
+      url: 'https://graph.microsoft.com/v1.0/me/extensions/com.contoso.roamingSettings',
+      sanitized: 'https://graph.microsoft.com/v1.0/me/extensions/{extensions-id}'
+    },
+    {
+      check: 'without plan id',
+      url: 'https://graph.microsoft.com/v1.0/planner/plans/CONGZUWfGUu4msTgNP66e2UAAySi',
+      sanitized: 'https://graph.microsoft.com/v1.0/planner/plans/CONGZUWfGUu4msTgNP66e2UAAySi'
     }
   ];
 
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-
+  list.forEach(element => {
     it(`return url ${element.check}`, () => {
       const normalizedUrl = sanitizeQueryUrl(element.url);
       expect(normalizedUrl).toEqual(element.sanitized);
     });
-
-  }
+  });
 
 });
