b2932e5dcd1c4fe66bc949d2cef12e41a1fc84e6
Sébastien Levert <slevert@outlook.com>
2020-11-09 23:32:25 -0500

    Adding translate method

--- src/app/views/query-runner/request/auth/Auth.tsx
+++ src/app/views/query-runner/request/auth/Auth.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { telemetry } from '../../../../../telemetry';
+import { translateMessage } from '../../../../utils/translate-messages';
 import { classNames } from '../../../classnames';
 import { genericCopy } from '../../../common/copy';
 import { authStyles } from './Auth.styles';
@@ -30,7 +31,7 @@ export function Auth(props: any) {
         <div className={classes.accessTokenContainer}>
           <Label className={classes.accessTokenLabel}><FormattedMessage id='Access Token' /></Label>
           <IconButton onClick={handleCopy} iconProps={copyIcon} title='Copy' ariaLabel='Copy' />
-          <IconButton iconProps={tokenDetailsIcon} title='Get token details (Powered by jwt.ms)' ariaLabel='Get token details (Powered by jwt.ms)' href={`https://jwt.ms#access_token=${accessToken}`} target="_blank" />
+          <IconButton iconProps={tokenDetailsIcon} title={translateMessage('Get token details (Powered by jwt.ms)')} ariaLabel={translateMessage('Get token details (Powered by jwt.ms)')} href={`https://jwt.ms#access_token=${accessToken}`} target="_blank" />
         </div>
         <Label className={classes.accessToken} >{accessToken}</Label>
       </div>
