33c089ff89c9a71b802b7685abdb9936faaac95b
Charles Wahome <thewahome.cw@gmail.com>
2020-11-30 12:58:37 +0300

    abstract group generation function

---
+++ src/app/utils/generate-groups.ts
@@ -0,0 +1,32 @@
+export function generateGroupsFromList(list: any[], property: string) {
+    const map = new Map();
+    const groups: any[] = [];
+
+    let isCollapsed = false;
+    let previousCount = 0;
+    let count = 0;
+
+    if (list.some(e => !e[property])) {
+        return groups;
+    }
+
+    for (const listItem of list) {
+        if (!map.has(listItem[property])) {
+            map.set(listItem[property], true);
+            count = list.filter(item => item[property] === listItem[property]).length;
+            if (groups.length > 0) {
+                isCollapsed = true;
+            }
+            groups.push({
+                name: listItem[property],
+                key: listItem[property],
+                startIndex: previousCount,
+                isCollapsed,
+                count,
+            });
+            previousCount += count;
+        }
+    }
+
+    return groups;
+}
