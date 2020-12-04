105dfe40bd941487ddee73dc693f602abfcf9107
Millicent Achieng <miachien@microsoft.com>
2020-12-04 03:48:29 +0300

    Capture telemetry data for failed adaptive cards fetch

--- src/app/services/actions/adaptive-cards-action-creator.ts
+++ src/app/services/actions/adaptive-cards-action-creator.ts
@@ -1,7 +1,11 @@
+import { SeverityLevel } from '@microsoft/applicationinsights-web';
 import * as AdaptiveCardsTemplateAPI from 'adaptivecards-templating';
+import { telemetry } from '../../../telemetry';
+import { NETWORK_ERROR } from '../../../telemetry/error-types';
 import { IAction } from '../../../types/action';
 import { IQuery } from '../../../types/query-runner';
 import { lookupTemplate } from '../../utils/adaptive-cards-lookup';
+import { sanitizeQueryUrl } from '../../utils/query-url-sanitization';
 import {
   FETCH_ADAPTIVE_CARD_ERROR,
   FETCH_ADAPTIVE_CARD_PENDING,
@@ -72,6 +76,16 @@ export function getAdaptiveCard(
       })
       .catch((error) => {
         // something wrong happened
+        const sanitizedUrl = sanitizeQueryUrl(sampleQuery.sampleUrl);
+        telemetry.trackException(
+          new Error(NETWORK_ERROR),
+          SeverityLevel.Error,
+          {
+            ComponentName: 'Get adaptive card action',
+            QuerySignature: `${sampleQuery.selectedVerb} ${sanitizedUrl}`,
+            Message: error
+          }
+        );
         return dispatch(getAdaptiveCardError(error));
       });
   };
