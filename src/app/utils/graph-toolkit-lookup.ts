44895e15d42ae4204f1221c4860c701378d1207d
Millicent Achieng <miachien@microsoft.com>
2020-12-04 03:45:46 +0300

    Add telemetry for failed toolkit fetch

--- src/app/utils/graph-toolkit-lookup.ts
+++ src/app/utils/graph-toolkit-lookup.ts
@@ -1,6 +1,28 @@
+import { SeverityLevel } from '@microsoft/applicationinsights-web';
 import templates from '../../graph-toolkit-examples';
+import { telemetry } from '../../telemetry';
+import { LINK_ERROR } from '../../telemetry/error-types';
 import { IQuery } from '../../types/query-runner';
 import { parseSampleUrl } from './sample-url-generation';
+
+
+async function validateToolkitUrl(url: string, componentName: string): Promise<void> {
+  await fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+      })
+      .catch(error => {
+        telemetry.trackException(
+          new Error(LINK_ERROR),
+          SeverityLevel.Error,
+          {
+            ComponentName: componentName,
+            Message: error
+          });
+      });
+ }
 
 export function lookupToolkitUrl(sampleQuery: IQuery) {
   if (sampleQuery) {
@@ -10,12 +32,15 @@ export function lookupToolkitUrl(sampleQuery: IQuery) {
       if (templates.hasOwnProperty(templateMapKey)) {
         const isMatch = new RegExp(templateMapKey + '$', 'i').test(query);
         if (isMatch) {
-          const url: string = (templates as any)[templateMapKey];
-          let { search: componentUrl } = parseSampleUrl(url);
+          const toolkitUrl: string = (templates as any)[templateMapKey];
+          let { search: componentUrl } = parseSampleUrl(toolkitUrl);
           componentUrl = componentUrl.replace('?id=', '');
+          const exampleUrl = `https://mgt.dev/?path=/story/${componentUrl}`;
+          validateToolkitUrl(toolkitUrl, 'Graph toolkit link');
+          validateToolkitUrl(exampleUrl, 'Graph toolkit example link');
           return {
-            exampleUrl: `https://mgt.dev/?path=/story/${componentUrl}`,
-            toolkitUrl: url
+            exampleUrl: exampleUrl,
+            toolkitUrl: toolkitUrl
           };
         }
       }
