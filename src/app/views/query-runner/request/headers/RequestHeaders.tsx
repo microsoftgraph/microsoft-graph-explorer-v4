e630c98afda7e864f401a1eced863df5375d174a
Elinor <ekaguongo@gmail.com>
2021-01-18 18:32:00 +0300

    replace class component

--- src/app/views/query-runner/request/headers/RequestHeaders.tsx
+++ src/app/views/query-runner/request/headers/RequestHeaders.tsx
@@ -1,8 +1,9 @@
 import { Announced, PrimaryButton, TextField } from 'office-ui-fabric-react';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import { ISampleQuery } from '../../../../../types/query-runner';
 
 import { IRequestHeadersProps } from '../../../../../types/request';
 import * as queryInputActionCreators from '../../../../services/actions/query-input-action-creators';
@@ -10,51 +11,44 @@ import { translateMessage } from '../../../../utils/translate-messages';
 import { headerStyles } from './Headers.styles';
 import HeadersList from './HeadersList';
 
-class RequestHeaders extends Component<IRequestHeadersProps, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      headerName: '',
-      headerValue: '',
-      announcedMessage: ''
-    };
-  }
+const RequestHeaders = (props: any) => {
+  const sampleQuery = useSelector((state: any) => state.sampleQuery);
+  const [headerName, setHeaderName] = useState('');
+  const [headerValue, setHeaderValue] = useState('');
+  const [announcedMessage, setAnnouncedMessage] = useState('');
 
-  private handleOnHeaderNameChange = (name?: string) => {
+  const { intl: { messages } } = props;
+  const sampleQueryHeaders = sampleQuery.sampleHeaders;
+  const container: any = headerStyles().container;
+
+  const dispatch = useDispatch();
+
+  const handleOnHeaderNameChange = (name?: string) => {
     if (name) {
-      this.setState({
-        headerName: name,
-      });
+      setHeaderName(name);
     }
   };
 
-  private handleOnHeaderValueChange = (value?: string) => {
+  const handleOnHeaderValueChange = (value?: string) => {
     if (value) {
-      this.setState({ headerValue: value });
+      setHeaderValue(value);
     }
   };
 
-  private handleOnHeaderDelete = (header: any) => {
-    const { actions, sampleQuery } = this.props;
+  const handleOnHeaderDelete = (header: any) => {
     let headers = [...sampleQuery.sampleHeaders];
     headers = headers.filter(head => head.name !== header.name);
 
-    const query = sampleQuery;
+    const query = { ...sampleQuery };
     query.sampleHeaders = headers;
 
-    if (actions) {
-      actions.setSampleQuery(query);
-    }
-
-    this.setState(this.state);
-    this.setState({ announcedMessage: translateMessage('Request Header deleted') });
+    dispatch(queryInputActionCreators.setSampleQuery(query));
+    setAnnouncedMessage(translateMessage('Request Header deleted'));
   };
 
-  private handleOnHeaderAdd = () => {
-    if (this.state.headerName !== '') {
-      const { headerName, headerValue } = this.state;
-      const { actions } = this.props;
-      let { sampleHeaders } = this.props.sampleQuery;
+  const handleOnHeaderAdd = () => {
+    if (headerName !== '') {
+      let { sampleHeaders } = sampleQuery;
       const header = { name: headerName, value: headerValue };
 
       if (!sampleHeaders) {
@@ -66,78 +60,52 @@ class RequestHeaders extends Component<IRequestHeadersProps, any> {
 
       const newHeaders = [header, ...sampleHeaders];
 
-      this.setState({
-        headerName: '',
-        headerValue: '',
-        announcedMessage: translateMessage('Request Header added')
-      });
+      setHeaderName('');
+      setHeaderValue('');
+      setAnnouncedMessage(translateMessage('Request Header added'));
 
-      if (actions) {
-        const query = this.props.sampleQuery;
-        query.sampleHeaders = newHeaders;
-        actions.setSampleQuery(query);
-      }
+      const query = { ...sampleQuery };
+      query.sampleHeaders = newHeaders;
+      dispatch(queryInputActionCreators.setSampleQuery(query));
     }
   };
 
-  public render() {
-    // @ts-ignore
-    const { sampleQuery, intl: { messages } } = this.props;
-    const headers = sampleQuery.sampleHeaders;
-    const container: any = headerStyles().container;
-
-    return (
-      <div style={container}>
-        <Announced message={this.state.announcedMessage} />
-        <div className='row'>
-          <div className='col-sm-5'>
-            <TextField className='header-input'
-              placeholder={messages.Key}
-              value={this.state.headerName}
-              onChange={(event, name) => this.handleOnHeaderNameChange(name)}
-            />
-          </div>
-          <div className='col-sm-5'>
-            <TextField
-              className='header-input'
-              placeholder={messages.Value}
-              value={this.state.headerValue}
-              onChange={(event, value) => this.handleOnHeaderValueChange(value)}
-            />
-          </div>
-          <div className='col-sm-2 col-md-2'>
-            <PrimaryButton
-              style={{ width: '100%' }}
-              onClick={() => this.handleOnHeaderAdd()}>
-              <FormattedMessage id='Add' />
-            </PrimaryButton>
-          </div>
+  return (
+    <div style={container}>
+      <Announced message={announcedMessage} />
+      <div className='row'>
+        <div className='col-sm-5'>
+          <TextField className='header-input'
+            placeholder={messages.Key}
+            value={headerName}
+            onChange={(event, name) => handleOnHeaderNameChange(name)}
+          />
         </div>
-        <hr />
-        <HeadersList
-          messages={messages}
-          handleOnHeaderDelete={(event: any, header: any) => this.handleOnHeaderDelete(header)}
-          headers={headers}
-        />
+        <div className='col-sm-5'>
+          <TextField
+            className='header-input'
+            placeholder={messages.Value}
+            value={headerValue}
+            onChange={(event, value) => handleOnHeaderValueChange(value)}
+          />
+        </div>
+        <div className='col-sm-2 col-md-2'>
+          <PrimaryButton
+            style={{ width: '100%' }}
+            onClick={() => handleOnHeaderAdd()}>
+            <FormattedMessage id='Add' />
+          </PrimaryButton>
+        </div>
       </div>
-    );
-  }
-}
-
-function mapDispatchToProps(dispatch: Dispatch): object {
-  return {
-    actions: bindActionCreators(
-      { ...queryInputActionCreators },
-      dispatch),
-  };
-}
-
-function mapStateToProps(state: any) {
-  return {
-    sampleQuery: state.sampleQuery,
-  };
-}
-
+      <hr />
+      <HeadersList
+        messages={messages}
+        handleOnHeaderDelete={(event: any, header: any) => handleOnHeaderDelete(header)}
+        headers={sampleQueryHeaders}
+      />
+    </div>
+  );
+};
 // @ts-ignore
 const WithIntl = injectIntl(RequestHeaders);
-export default connect(mapStateToProps, mapDispatchToProps)(WithIntl);
+export default WithIntl;
