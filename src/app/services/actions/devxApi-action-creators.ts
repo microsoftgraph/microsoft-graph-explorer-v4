35fc72eff11e542894bff5809f65af5b0bed8492
Maggie Kimani <maggiekim42@gmail.com>
2021-02-03 17:44:29 +0300

    Change response type to object

--- src/app/services/actions/devxApi-action-creators.ts
+++ src/app/services/actions/devxApi-action-creators.ts
@@ -1,6 +1,6 @@
 import { SET_DEVX_API_URL_SUCCESS } from '../redux-constants';
 
-export function setDevxApiUrl(response: string): any {
+export function setDevxApiUrl(response: object): any {
   return {
     type: SET_DEVX_API_URL_SUCCESS,
     response,
