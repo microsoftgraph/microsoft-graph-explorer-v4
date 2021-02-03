41c13961effe22483075e2127bdc56e3d4ed1e86
Maggie Kimani <maggiekim42@gmail.com>
2021-02-03 17:45:56 +0300

    Create an initialState object and pass it to state

--- src/app/services/reducers/devxApi-reducers.ts
+++ src/app/services/reducers/devxApi-reducers.ts
@@ -2,7 +2,11 @@ import { IAction } from '../../../types/action';
 import { DEVX_API_URL } from '../graph-constants';
 import { SET_DEVX_API_URL_SUCCESS } from '../redux-constants';
 
-export function devxApi(state: string = DEVX_API_URL, action: IAction): any {
+const initialState = {
+  baseUrl: DEVX_API_URL,
+  parameters: ''
+};
+export function devxApi(state = initialState, action: IAction): any {
   switch (action.type) {
 
     case SET_DEVX_API_URL_SUCCESS:
