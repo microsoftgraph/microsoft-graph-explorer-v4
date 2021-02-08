e9e22f63a9d236ea09781f204b39c8e34ed6bd07
Maggie Kimani <maggiekim42@gmail.com>
2021-02-08 10:30:17 +0300

    Import and reference IAction from the types folder

--- src/app/services/actions/devxApi-action-creators.ts
+++ src/app/services/actions/devxApi-action-creators.ts
@@ -1,6 +1,7 @@
+import { IAction } from '../../../types/action';
 import { SET_DEVX_API_URL_SUCCESS } from '../redux-constants';
 
-export function setDevxApiUrl(response: object): any {
+export function setDevxApiUrl(response: object): IAction {
   return {
     type: SET_DEVX_API_URL_SUCCESS,
     response,
