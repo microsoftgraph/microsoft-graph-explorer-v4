9b94ccfdac8a2ebf438e868d4e004dd7777696a3
Maggie Kimani <maggiekim42@gmail.com>
2021-02-08 19:47:30 +0300

    Revert "Feature: Enable loading of samples and Permissions descriptions files from Github"

--- src/app/services/actions/devxApi-action-creators.ts
+++ src/app/services/actions/devxApi-action-creators.ts
@@ -1,7 +1,6 @@
-import { IAction } from '../../../types/action';
 import { SET_DEVX_API_URL_SUCCESS } from '../redux-constants';
 
-export function setDevxApiUrl(response: object): IAction {
+export function setDevxApiUrl(response: string): any {
   return {
     type: SET_DEVX_API_URL_SUCCESS,
     response,
--- src/types/devx-api.ts
+++
@@ -1,4 +0,0 @@
-export interface IDevxAPI {
-    baseUrl: string;
-    parameters: string;
-}
