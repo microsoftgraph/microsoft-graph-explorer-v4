ea9ee3ab35736a97bf6ae4bc96f2043dba60d0ac
Charles Wahome <thewahome.cw@gmail.com>
2021-04-01 18:49:44 +0300

    add root application state

---
+++ src/types/root.ts
@@ -0,0 +1,41 @@
+import { IAdaptiveCardResponse } from './adaptivecard';
+import { IAutocompleteResponse } from './auto-complete';
+import { IDimensions } from './dimensions';
+import { Mode } from './enums';
+import { IHistoryItem } from './history';
+import { IScopes } from './permissions';
+import { IUser } from './profile';
+import { IGraphResponse } from './query-response';
+import { IQuery, ISampleQuery } from './query-runner';
+import { ISidebarProps } from './sidebar';
+import { ISnippet } from './snippets';
+import { IStatus } from './status';
+
+export interface IRootState {
+  theme: string;
+  adaptiveCard: IAdaptiveCardResponse;
+  graphExplorerMode: Mode;
+  profile: IUser | undefined | null;
+  queryRunnerStatus: IStatus;
+  sampleQuery: IQuery;
+  termsOfUse: boolean;
+  sidebarProperties: ISidebarProps;
+  authToken: string;
+  samples: ISampleQuery[];
+  consentedScopes: string[];
+  scopes: IScopes;
+  history: IHistoryItem[];
+  graphResponse: IGraphResponse;
+  permissionsPanelOpen: boolean;
+  isLoadingData: boolean;
+  snippets: ISnippet;
+  responseAreaExpanded: boolean;
+  dimensions: IDimensions;
+  autoComplete: IAutocompleteResponse;
+}
+
+export interface IApiFetch {
+  pending: boolean;
+  data: any[] | object | null | any;
+  error: any | null;
+}
