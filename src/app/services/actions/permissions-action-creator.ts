f3732f8f44f02287481b68f6bd309d435407b0eb
Charles Wahome <thewahome.cw@gmail.com>
2021-06-17 14:55:58 +0300

    Fix: repopulate modify permissions when panel closed (#999)
    * repopulate open modify permissions when permissions panel is closed
    
    * move all fetch scopes logic to action creator

--- src/app/services/actions/permissions-action-creator.ts
+++ src/app/services/actions/permissions-action-creator.ts
@@ -3,8 +3,8 @@ import { MessageBarType } from 'office-ui-fabric-react';
 import { geLocale } from '../../../appLocale';
 import { authenticationWrapper } from '../../../modules/authentication';
 import { IAction } from '../../../types/action';
-import { IQuery } from '../../../types/query-runner';
 import { IRequestOptions } from '../../../types/request';
+import { IRootState } from '../../../types/root';
 import { sanitizeQueryUrl } from '../../utils/query-url-sanitization';
 import { parseSampleUrl } from '../../utils/sample-url-generation';
 import { translateMessage } from '../../utils/translate-messages';
@@ -39,14 +39,14 @@ export function fetchScopesError(response: object): IAction {
   };
 }
 
-export function fetchScopes(query?: IQuery): Function {
+export function fetchScopes(): Function {
   return async (dispatch: Function, getState: Function) => {
     let hasUrl = false; // whether permissions are for a specific url
     try {
-      const { devxApi } = getState();
+      const { devxApi, permissionsPanelOpen, sampleQuery: query }: IRootState = getState();
       let permissionsUrl = `${devxApi.baseUrl}/permissions`;
 
-      if (query) {
+      if (!permissionsPanelOpen) {
         const signature = sanitizeQueryUrl(query.sampleUrl);
         const { requestUrl, sampleUrl } = parseSampleUrl(signature);
 
