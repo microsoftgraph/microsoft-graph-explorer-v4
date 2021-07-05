0d8ef6e5f4a0e823cfbc50c3862600c80eba81ab
Charles Wahome <thewahome.cw@gmail.com>
2021-07-05 19:12:29 +0300

    replace # character with escape values

--- src/app/services/actions/query-action-creator-util.ts
+++ src/app/services/actions/query-action-creator-util.ts
@@ -21,7 +21,7 @@ export function queryResponse(response: object): IAction {
 
 export async function anonymousRequest(dispatch: Function, query: IQuery) {
   const authToken = '{token:https://graph.microsoft.com/}';
-  const escapedUrl = encodeURIComponent(query.sampleUrl);
+  const escapedUrl = encodeURIComponent(encodeHashCharacters(query));
   const graphUrl = `${GRAPH_API_SANDBOX_URL}/svc?url=${escapedUrl}`;
   const sampleHeaders: any = {};
   if (query.sampleHeaders && query.sampleHeaders.length > 0) {
@@ -112,8 +112,9 @@ const makeRequest = (httpVerb: string, scopes: string[]): Function => {
       authProvider,
       msalAuthOptions
     );
+
     const client = GraphClient.getInstance()
-      .api(query.sampleUrl)
+      .api(encodeHashCharacters(query))
       .middlewareOptions([middlewareOptions])
       .headers(sampleHeaders)
       .responseType(ResponseType.RAW);
@@ -145,3 +146,7 @@ const makeRequest = (httpVerb: string, scopes: string[]): Function => {
     return Promise.resolve(response);
   };
 };
+
+function encodeHashCharacters(query: IQuery): string {
+  return query.sampleUrl.replace(/#/g, '%23');
+}
