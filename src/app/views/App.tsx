9c85cc539c3abab1f415dccc2e19e938902b0ee8
Grace Tsao <tsaosgrace@gmail.com>
2021-07-07 10:37:47 -0700

    Rebased with interns/feature/switch-permissions

--- src/app/views/App.tsx
+++ src/app/views/App.tsx
@@ -350,8 +350,10 @@ class App extends Component<IAppProps, IAppState> {
     if (permissionModeType && this.state.hideDialog) {
       this.showDialog();
     }
+    // eslint-disable-next-line react/jsx-no-target-blank
     const teamsapp = <a href={"https://www.bing.com/?form=000010"} target="_blank">{translateMessage('Sample Explorer Teams app')}</a>;
     //TODO: put in the url when we have this set up ADO #38728
+    // eslint-disable-next-line react/jsx-no-target-blank
     const rsc = <a href={RSC_URL} target="_blank">{translateMessage('resource specific consent')}</a>;
     return (
       // @ts-ignore
