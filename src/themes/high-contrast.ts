d734ae02a4a85d3a7269394300e4a020ca7bd9a0
allison <47487758+acchiang@users.noreply.github.com>
2021-07-08 01:41:54 -0700

    Fix: improve themes MessageBar colors (#1015)

--- src/themes/high-contrast.ts
+++ src/themes/high-contrast.ts
@@ -22,6 +22,21 @@ export const highContrast = {
     neutralDark: '#f4f4f4',
     black: '#f8f8f8',
     white: '#000000',
-    blueMid: '#266ea6',
-  }
+    blueMid: '#6cb8f6',
+  },
+  semanticColors: {
+    messageText: '#f4f4f4',
+    infoBackground: '#1b1a19',
+    infoIcon: '#c8c6c4',
+    errorBackground: '#442726',
+    errorIcon: '#f1707b',
+    blockingBackground: '#442726',
+    blockingIcon: '#f1707b',
+    severeWarningBackground: '#4f2a0f',
+    severeWarningIcon: '#fce100',
+    successBackground: '#393d1b',
+    successIcon: '#92c353',
+    warningBackground: '#433519',
+    warningIcon: '#c8c6c4',
+  },
 };
