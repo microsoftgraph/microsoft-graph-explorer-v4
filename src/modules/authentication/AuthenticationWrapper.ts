ac6cdf71933107774c3a9865c9c23e22713f487b
Charles Wahome <thewahome.cw@gmail.com>
2021-07-09 15:18:21 +0300

    logout from account in current cloud

--- src/modules/authentication/AuthenticationWrapper.ts
+++ src/modules/authentication/AuthenticationWrapper.ts
@@ -45,7 +45,9 @@ export class AuthenticationWrapper implements IAuthenticationWrapper {
 
   public logOut() {
     this.deleteHomeAccountId();
-    msalApplication.logout();
+    msalApplication.logout({
+      authority: this.getAuthority()
+    });
   }
 
   public async logOutPopUp() {
