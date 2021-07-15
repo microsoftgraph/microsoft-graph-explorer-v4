054f0c5aeac5e0397f3d185d4d504d5beab44ffe
Charles Wahome <thewahome.cw@gmail.com>
2021-07-15 14:02:05 +0300

    Task: change sandbox url (#992)

---
+++ src/app/services/reducers/proxy-url-reducer.ts
@@ -0,0 +1,12 @@
+import { IAction } from '../../../types/action';
+import { GRAPH_API_SANDBOX_URL } from '../graph-constants';
+import { SET_GRAPH_PROXY_URL } from '../redux-constants';
+
+export function proxyUrl(state = GRAPH_API_SANDBOX_URL, action: IAction): any {
+  switch (action.type) {
+    case SET_GRAPH_PROXY_URL:
+      return action.response;
+    default:
+      return state;
+  }
+}
