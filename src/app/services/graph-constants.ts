76c2f02f115eee3ed9f9c75b9e4a5b0c46178287
Omer Abubaker <omer@DESKTOP-CQESA2S.localdomain>
2021-07-18 17:24:27 -0400

    Added back original DEVX API URL link

--- src/app/services/graph-constants.ts
+++ src/app/services/graph-constants.ts
@@ -3,7 +3,7 @@ export const USER_INFO_URL = `${GRAPH_URL}/v1.0/me`;
 export const USER_PICTURE_URL = `${GRAPH_URL}/beta/me/photo/$value`;
 export const AUTH_URL = 'https://login.microsoftonline.com';
 export const DEFAULT_USER_SCOPES = 'openid profile User.Read';
-export const DEVX_API_URL = 'https://gi21devxapi-devtest.azurewebsites.net/';
+export const DEVX_API_URL = 'https://graphexplorerapi.azurewebsites.net';
 export const GRAPH_API_SANDBOX_URL = 'https://proxy.apisandbox.msdn.microsoft.com';
 export const HOME_ACCOUNT_KEY = 'fbf1ecbe-27ab-42d7-96d4-3e6b03682ee4';
 export enum PERMS_SCOPE {
