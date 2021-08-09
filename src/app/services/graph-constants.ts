9d697e3e96ea8fd2897671e590c18084524aa017
Grace Tsao <tsaosgrace@gmail.com>
2021-08-09 14:49:52 -0700

    Updated id

--- src/app/services/graph-constants.ts
+++ src/app/services/graph-constants.ts
@@ -26,6 +26,6 @@ export const RSC_PERMISSIONS_ENDINGS = [".Group", ".Chat"];
 export const RSC_URL = "https://docs.microsoft.com/en-us/microsoftteams/platform/graph-api/rsc/resource-specific-consent";
 export const RSC_HIDE_POPUP = "do not show RSC popup again"
 export const APP_IMAGE = "https://docs.microsoft.com/en-us/microsoftteams/platform/assets/icons/graph-icon-1.png";
-export const TEAMS_APP_ID = "46c88300-12bd-44cb-b3ba-734ed25fe1de";
+export const TEAMS_APP_ID = "c9c5f709-36f4-4ea4-b9f0-50b8c8792e54";
 export const TEAMS_APP_URL = "https://www.bing.com/?form=000010";
 export const ADAPTIVE_CARD_URL = 'https://templates.adaptivecards.io/graph.microsoft.com';
