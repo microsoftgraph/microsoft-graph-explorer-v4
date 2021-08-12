618002790f4ba96c057e67afbec3d6f46fd961c6
Onokaev <evansonoka05.j@gmail.com>
2021-08-12 10:28:51 +0300

    initial commit on auth error hints

--- src/app/views/authentication/Authentication.tsx
+++ src/app/views/authentication/Authentication.tsx
@@ -1,6 +1,13 @@
-
 import { SeverityLevel } from '@microsoft/applicationinsights-web';
-import { Icon, Label, MessageBar, MessageBarType, Spinner, SpinnerSize, styled } from 'office-ui-fabric-react';
+import {
+  Icon,
+  Label,
+  MessageBar,
+  MessageBarType,
+  Spinner,
+  SpinnerSize,
+  styled,
+} from 'office-ui-fabric-react';
 import React, { useState } from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,7 +16,10 @@ import { authenticationWrapper } from '../../../modules/authentication';
 import { componentNames, errorTypes, telemetry } from '../../../telemetry';
 import { Mode } from '../../../types/enums';
 import { IRootState } from '../../../types/root';
-import { getAuthTokenSuccess, getConsentedScopesSuccess } from '../../services/actions/auth-action-creators';
+import {
+  getAuthTokenSuccess,
+  getConsentedScopesSuccess,
+} from '../../services/actions/auth-action-creators';
 import { setQueryResponseStatus } from '../../services/actions/query-status-action-creator';
 import { translateMessage } from '../../utils/translate-messages';
 import { classNames } from '../classnames';
@@ -19,7 +29,9 @@ import { authenticationStyles } from './Authentication.styles';
 const Authentication = (props: any) => {
   const dispatch = useDispatch();
   const [loginInProgress, setLoginInProgress] = useState(false);
-  const { sidebarProperties, authToken, graphExplorerMode } = useSelector((state: IRootState) => state);
+  const { sidebarProperties, authToken, graphExplorerMode } = useSelector(
+    (state: IRootState) => state
+  );
   const mobileScreen = !!sidebarProperties.mobileScreen;
   const showSidebar = !!sidebarProperties.showSidebar;
   const tokenPresent = !!authToken.token;
@@ -38,52 +50,69 @@ const Authentication = (props: any) => {
       const authResponse = await authenticationWrapper.logIn();
       if (authResponse) {
         setLoginInProgress(false);
-        dispatch(getAuthTokenSuccess(!!authResponse.accessToken))
-        dispatch(getConsentedScopesSuccess(authResponse.scopes))
+        dispatch(getAuthTokenSuccess(!!authResponse.accessToken));
+        dispatch(getConsentedScopesSuccess(authResponse.scopes));
       }
     } catch (error) {
       const { errorCode } = error;
-      dispatch(setQueryResponseStatus({
-        ok: false,
-        statusText: messages['Authentication failed'],
-        status: errorCode === 'popup_window_error'
-          ? translateMessage('popup blocked, allow pop-up windows in your browser')
-          : errorCode ? errorCode.replace('_', ' ') : '',
-        messageType: MessageBarType.error
-      }));
+      dispatch(
+        setQueryResponseStatus({
+          ok: false,
+          statusText: messages['Authentication failed'],
+          status: checkErrorHint(errorCode),
+          messageType: MessageBarType.error,
+        })
+      );
       setLoginInProgress(false);
       telemetry.trackException(
         new Error(errorTypes.OPERATIONAL_ERROR),
         SeverityLevel.Error,
         {
           ComponentName: componentNames.AUTHENTICATION_ACTION,
-          Message: `Authentication failed: ${errorCode ? errorCode.replace('_', ' ') : ''}`,
-        });
+          Message: `Authentication failed: ${
+            errorCode ? errorCode.replace('_', ' ') : ''
+          }`,
+        }
+      );
     }
   };
 
+  const checkErrorHint = (errorCode: string): string => {
+    const errorMessage = translateMessage(errorCode);
+    return errorCode.replace('_', ' ') + '    Hint: ' + errorMessage;
+  };
+
   const showProgressSpinner = (): React.ReactNode => {
-    return <div className={classes.spinnerContainer}>
-      <Spinner className={classes.spinner} size={SpinnerSize.medium} />
-      {!minimised && <Label>
-        <FormattedMessage id={`Signing you ${loginInProgress ? 'in' : 'out'}...`} />
-      </Label>}
-    </div>;
-  }
+    return (
+      <div className={classes.spinnerContainer}>
+        <Spinner className={classes.spinner} size={SpinnerSize.medium} />
+        {!minimised && (
+          <Label>
+            <FormattedMessage
+              id={`Signing you ${loginInProgress ? 'in' : 'out'}...`}
+            />
+          </Label>
+        )}
+      </div>
+    );
+  };
 
   const showUnAuthenticatedText = (): React.ReactNode => {
-    return <>
-      <Label className={classes.authenticationLabel}>
-        <Icon iconName='Permissions' className={classes.keyIcon} />
-        <FormattedMessage id='Authentication' />
-      </Label>
+    return (
+      <>
+        <Label className={classes.authenticationLabel}>
+          <Icon iconName='Permissions' className={classes.keyIcon} />
+          <FormattedMessage id='Authentication' />
+        </Label>
 
-      <br />
-      <MessageBar messageBarType={MessageBarType.warning} isMultiline={true}>
-        <FormattedMessage id='Using demo tenant' /> <FormattedMessage id='To access your own data:' />
-      </MessageBar>
-    </>;
-  }
+        <br />
+        <MessageBar messageBarType={MessageBarType.warning} isMultiline={true}>
+          <FormattedMessage id='Using demo tenant' />{' '}
+          <FormattedMessage id='To access your own data:' />
+        </MessageBar>
+      </>
+    );
+  };
 
   if (logoutInProgress) {
     return showProgressSpinner();
@@ -91,16 +120,29 @@ const Authentication = (props: any) => {
 
   return (
     <>
-      {loginInProgress ? showProgressSpinner()
-        :
-        mobileScreen ? showSignInButtonOrProfile(tokenPresent, mobileScreen, signIn, minimised) :
-          <>
-            {!tokenPresent && graphExplorerMode === Mode.Complete && !minimised && showUnAuthenticatedText()}
-            <br />{showSignInButtonOrProfile(tokenPresent, mobileScreen, signIn, minimised)}<br />
-          </>}
+      {loginInProgress ? (
+        showProgressSpinner()
+      ) : mobileScreen ? (
+        showSignInButtonOrProfile(tokenPresent, mobileScreen, signIn, minimised)
+      ) : (
+        <>
+          {!tokenPresent &&
+            graphExplorerMode === Mode.Complete &&
+            !minimised &&
+            showUnAuthenticatedText()}
+          <br />
+          {showSignInButtonOrProfile(
+            tokenPresent,
+            mobileScreen,
+            signIn,
+            minimised
+          )}
+          <br />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
 // @ts-ignore
 const IntlAuthentication = injectIntl(Authentication);
