2ac229d177452b805dd3fc465b5bc4b48963ed78
Charles Wahome <thewahome.cw@gmail.com>
2021-08-12 18:05:24 +0300

    remove references to office-ui-fabric-react

--- src/app/views/query-response/QueryResponse.tsx
+++ src/app/views/query-response/QueryResponse.tsx
@@ -1,9 +1,8 @@
 import {
-  Announced,
+  Announced, Dialog, DialogFooter, DialogType,
   DefaultButton, FontSizes, getId, Icon, IconButton,
   Modal, Pivot, PivotItem, PrimaryButton, TooltipHost
 } from '@fluentui/react';
-import { Dialog, DialogFooter, DialogType } from 'office-ui-fabric-react/lib/Dialog';
 import { Resizable } from 're-resizable';
 import React, { useEffect, useState } from 'react';
 import { injectIntl } from 'react-intl';
