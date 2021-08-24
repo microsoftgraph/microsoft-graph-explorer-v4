e6eb5b3799516f74960b44fc67a85b5ca8f4aa41
Charles Wahome <thewahome.cw@gmail.com>
2021-08-24 10:35:16 +0300

    reuse call to api functionality

--- src/app/services/actions/profile-action-creators.ts
+++ src/app/services/actions/profile-action-creators.ts
@@ -22,52 +22,62 @@ export function profileRequestError(response: object): any {
   };
 }
 
+const query: IQuery = {
+  selectedVerb: 'GET',
+  sampleHeaders: [{
+    name: 'Cache-Control',
+    value: 'no-cache'
+  }],
+  selectedVersion: '',
+  sampleUrl: ''
+}
+
 export function getProfileInfo(): Function {
   return async (dispatch: Function) => {
-    const respHeaders: any = {};
-    const query: IQuery = {
-      selectedVerb: 'GET',
-      sampleHeaders: [],
-      selectedVersion: '',
-      sampleUrl: ''
-    }
-
-    query.sampleHeaders.push({
-      name: 'Cache-Control',
-      value: 'no-cache'
-    });
-
-    const scopes = DEFAULT_USER_SCOPES.split(' ');
-
     dispatch(queryRunningStatus(true));
-
     try {
-      const profile: IUser = await getProfileInformation(query, scopes, respHeaders);
-      profile.profileType = await getProfileType(query, scopes, respHeaders);
-      profile.profileImageUrl = await getProfileImage(query, scopes, respHeaders);
+      const profile: IUser = await getProfileInformation();
+      profile.profileType = await getProfileType();
+      profile.profileImageUrl = await getProfileImage();
       dispatch(profileRequestSuccess(profile));
     } catch (error) {
       dispatch(profileRequestError({ error }));
     }
   };
 }
-async function getProfileType(query: IQuery, scopes: string[], respHeaders: any): Promise<ACCOUNT_TYPE> {
+
+async function getProfileInformation(): Promise<IUser> {
+  const profile: IUser = {
+    displayName: '',
+    emailAddress: '',
+    profileImageUrl: '',
+  };
+  try {
+    query.sampleUrl = USER_INFO_URL;
+    const { userInfo } = await getProfileResponse();
+    profile.displayName = userInfo.displayName;
+    profile.emailAddress = userInfo.mail || userInfo.userPrincipalName;
+    return profile;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getProfileType(): Promise<ACCOUNT_TYPE> {
   try {
     query.sampleUrl = BETA_USER_INFO_URL;
-    const response = await makeRequest(query.selectedVerb, scopes)(query);
-    const userInfo = await parseResponse(response, respHeaders);
+    const { userInfo } = await getProfileResponse();
     return userInfo?.account?.[0]?.source?.type?.[0];
   } catch (error) {
     return ACCOUNT_TYPE.MSA;
   }
 }
 
-async function getProfileImage(query: IQuery, scopes: string[], respHeaders: any): Promise<string> {
+async function getProfileImage(): Promise<string> {
   let profileImageUrl = '';
   try {
     query.sampleUrl = USER_PICTURE_URL;
-    const response = await makeRequest(query.selectedVerb, scopes)(query);
-    const userPicture = await parseResponse(response, respHeaders);
+    const { response, userInfo: userPicture } = await getProfileResponse();
     if (userPicture) {
       const buffer = await response.arrayBuffer();
       const blob = new Blob([buffer], { type: 'image/jpeg' });
@@ -79,20 +89,14 @@ async function getProfileImage(query: IQuery, scopes: string[], respHeaders: any
   return profileImageUrl;
 }
 
-async function getProfileInformation(query: IQuery, scopes: string[], respHeaders: any): Promise<IUser> {
-  const profile: IUser = {
-    displayName: '',
-    emailAddress: '',
-    profileImageUrl: '',
+async function getProfileResponse() {
+  const scopes = DEFAULT_USER_SCOPES.split(' ');
+  const respHeaders: any = {};
+
+  const response = await makeRequest(query.selectedVerb, scopes)(query);
+  const userInfo = await parseResponse(response, respHeaders);
+  return {
+    userInfo,
+    response
   };
-  try {
-    query.sampleUrl = USER_INFO_URL;
-    const response = await makeRequest(query.selectedVerb, scopes)(query);
-    const userInfo = await parseResponse(response, respHeaders);
-    profile.displayName = userInfo.displayName;
-    profile.emailAddress = userInfo.mail || userInfo.userPrincipalName;
-  } catch (error) {
-    throw error;
-  }
-  return profile;
 }
