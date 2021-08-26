9e93661fea16119d1a9626f6dd59550a776bd5ff
Onokaev <evansonoka05.j@gmail.com>
2021-08-26 11:57:57 +0300

    code refactors in error hinting

--- src/app/views/authentication/AuthenticationErrorsHints.ts
+++ src/app/views/authentication/AuthenticationErrorsHints.ts
@@ -1,32 +1,32 @@
 import { translateMessage } from "../../utils/translate-messages";
 
-const AuthErrorList : string[] = ['user_cancelled','null_or_empty_id_token',
+const authErrorList : string[] = ['user_cancelled','null_or_empty_id_token',
 "authorization_code_missing_from_server_response",
 'no_tokens_found', 'invalid_request', "user_login_error","nonce_mismatch_error",
 'login_progress_error', 'interaction_in_progress',
 'interaction_required', 'invalid_grant', 'endpoints_resolution_error']
 
-const ScopeErrorList : string[] = ['interaction_required', 'consent_required', 'login_required', 'access_denied', 'user_cancelled' ]
+const scopeErrorList : string[] = ['interaction_required', 'consent_required', 'login_required', 'access_denied', 'user_cancelled' ]
 
 export function getSignInAuthErrorHint(error: string): string{
     const authErrorHintAvailable = signInAuthError(error);
-    return getHint(authErrorHintAvailable, error);
+    return authErrorHintAvailable ? getHint(error) : '';
 }
 
 export function signInAuthError(error: string): boolean {
-    return AuthErrorList.includes(error.trim());
+    return authErrorList.includes(error.trim());
 }
 
 export function getConsentAuthErrorHint(error: string): string{
     const authErrorHintAvailable = scopeAuthError(error);
-    const consentErrorHint = error + '_consent';
-    return getHint(authErrorHintAvailable, consentErrorHint);
+    const consentError = error + '_consent';
+    return authErrorHintAvailable ? getHint(consentError) : '';
 }
 
 export function scopeAuthError(error: string): boolean {
-    return ScopeErrorList.includes(error);
+    return scopeErrorList.includes(error);
 }
 
-function getHint(errorHintAvailable: boolean, error: string): string {
-    return ((errorHintAvailable === true) ? ( translateMessage('Tip') + ' -  ' + translateMessage(error) ) : '');
+function getHint( error: string): string {
+    return translateMessage('Tip') + ' -  ' + translateMessage(error);
 }
