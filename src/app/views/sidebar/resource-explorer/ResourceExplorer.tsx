bfe407adb2c017d6a8622feed351e743d3478ec8
Charles Wahome <thewahome.cw@gmail.com>
2021-09-14 15:48:16 +0300

    display first level of items

--- src/app/views/sidebar/resource-explorer/ResourceExplorer.tsx
+++ src/app/views/sidebar/resource-explorer/ResourceExplorer.tsx
@@ -1,29 +1,71 @@
-import { SearchBox } from '@fluentui/react';
+import { DetailsRow, GroupedList, IGroup, SearchBox, SelectionMode, styled } from '@fluentui/react';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { IResource } from '../../../../types/resources';
 import { IRootState } from '../../../../types/root';
 import { translateMessage } from '../../../utils/translate-messages';
+import { classNames } from '../../classnames';
+import { sidebarStyles } from '../Sidebar.styles';
 
-const ResourceExplorer = () => {
+
+const ResourceExplorer = (props: any) => {
   const { resources } = useSelector(
     (state: IRootState) => state
   );
+  const classes = classNames(props);
+
   const { data } = resources;
   const [resourceItems, setResourceItems] = useState<IResource[]>(data.children);
   const searchValueChanged = (event: any, value?: string) => {
-    const items = [...resourceItems];
-    let filtered: any[] = items;
+    let filtered: any[] = [...resourceItems];
     if (value) {
       const keyword = value.toLowerCase();
-      filtered = items.filter((sample: IResource) => {
+      filtered = resourceItems.filter((sample: IResource) => {
         const name = sample.segment.toLowerCase();
         return name.toLowerCase().includes(keyword);
       });
     }
     setResourceItems(filtered);
   }
+
+  const createListItems = (source: IResource[]) => {
+    return source;
+  }
+
+  const items = createListItems(resourceItems);
+  const columns = [
+    {
+      key: 'segment',
+      name: 'segment',
+      fieldName: 'segment',
+      minWidth: 300,
+    },
+    {
+      key: 'children',
+      name: 'children',
+      fieldName: 'children',
+      minWidth: 300,
+    }
+  ]
+
+  const onRenderCell = (nestingDepth?: number, item?: any, itemIndex?: number, group?: IGroup): JSX.Element => {
+    const content = {
+      segment: item.segment,
+      children: (item.children) ? item.children.length : 0,
+      label: (item.label) ? item.label.length : 0
+    }
+    return (
+      <DetailsRow
+        columns={columns}
+        className={classes.queryRow}
+        groupNestingDepth={nestingDepth}
+        item={content}
+        itemIndex={itemIndex!}
+        group={group}
+      />
+    );
+  };
 
   return (
     <section>
@@ -33,10 +75,15 @@ const ResourceExplorer = () => {
         styles={{ field: { paddingLeft: 10 } }}
       />
       <hr />
-      {JSON.stringify(resourceItems, null, 4)}
-
+      <GroupedList
+        items={items}
+        className={classes.queryList}
+        onRenderCell={onRenderCell}
+        selectionMode={SelectionMode.multiple}
+      />
     </section>
   )
 }
-
-export default ResourceExplorer;
+// @ts-ignore
+const styledResourceExplorer = styled(ResourceExplorer, sidebarStyles);
+export default styledResourceExplorer;
