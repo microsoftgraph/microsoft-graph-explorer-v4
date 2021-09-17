c3f11e9abd7ef49e08eeed4ac9d1240aab9d108c
Charles Wahome <thewahome.cw@gmail.com>
2021-09-17 10:25:51 +0300

    enable search and filtering

--- src/app/views/sidebar/resource-explorer/ResourceExplorer.tsx
+++ src/app/views/sidebar/resource-explorer/ResourceExplorer.tsx
@@ -1,5 +1,6 @@
-import { INavLinkGroup, Nav, SearchBox, styled } from '@fluentui/react';
+import { ChoiceGroup, IChoiceGroupOption, INavLink, INavLinkGroup, Label, Nav, SearchBox, styled } from '@fluentui/react';
 import React, { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { useSelector } from 'react-redux';
 
 import { IResource } from '../../../../types/resources';
@@ -9,28 +10,26 @@ import { translateMessage } from '../../../utils/translate-messages';
 import { classNames } from '../../classnames';
 import { sidebarStyles } from '../Sidebar.styles';
 
-
 const ResourceExplorer = (props: any) => {
   const { resources } = useSelector(
     (state: IRootState) => state
   );
   const classes = classNames(props);
-
   const { data } = resources;
-  const filteredPayload = filterResourcesByLabel(filterResourcesByLabel(data, ['Prod']), ['v1.0']);
+  const versions: IChoiceGroupOption[] = [
+    { key: 'v1.0', text: 'v1.0', iconProps: { iconName: 'CloudWeather' } },
+    { key: 'beta', text: 'beta', iconProps: { iconName: 'PartlyCloudyNight' } }
+  ];
 
-  const [resourceItems, setResourceItems] = useState<IResource[]>(filteredPayload.children);
-  const searchValueChanged = (event: any, value?: string) => {
-    let filtered: any[] = [...resourceItems];
-    if (value) {
-      const keyword = value.toLowerCase();
-      filtered = resourceItems.filter((sample: IResource) => {
-        const name = sample.segment.toLowerCase();
-        return name.toLowerCase().includes(keyword);
-      });
-    }
-    setResourceItems(filtered);
+  const [version, setVersion] = useState(versions[0].key);
+  const [searchText, setSearchText] = useState<string>('');
+
+  const filterDataByVersion = (info: IResource, selectedVersion: string) => {
+    return filterResourcesByLabel(filterResourcesByLabel(info, ['Prod']), [selectedVersion]);
   }
+
+  const filteredPayload = filterDataByVersion(data, version);
+  const [resourceItems, setResourceItems] = useState<IResource[]>(filteredPayload.children);
 
   const createList = (source: IResource[]) => {
     function getIcon({ segment, children }: IResource) {
@@ -69,24 +68,82 @@ const ResourceExplorer = (props: any) => {
         links: converted.links
       }
     ];
+  }
 
+  const performSearch = (needle: string, haystack: any[]) => {
+    const keyword = needle.toLowerCase();
+    return haystack.filter((sample: IResource) => {
+      const name = sample.segment.toLowerCase();
+      return name.toLowerCase().includes(keyword);
+    });
+  }
 
+  const clickLink = (ev?: React.MouseEvent<HTMLElement>, item?: INavLink) => {
+    ev!.preventDefault();
+    if (item && item.name) {
+      alert(item.name + ' link clicked');
+    }
+  }
+
+  const changeVersion = (ev: React.FormEvent<HTMLElement | HTMLInputElement> | undefined,
+    option: IChoiceGroupOption | undefined): void => {
+    const selectedVersion = option!.key;
+    setVersion(selectedVersion);
+    const list = filterDataByVersion(data, selectedVersion);
+    setResourceItems((searchText) ? performSearch(searchText, list.children) : list.children);
+  }
+
+  const changeSearchValue = (event: any, value?: string) => {
+    let filtered: any[] = [...data.children];
+    setSearchText(value || '');
+    if (value) {
+      filtered = performSearch(value, filtered);
+    }
+    setResourceItems(filterDataByVersion({
+      children: filtered,
+      label: data.label,
+      segment: data.segment
+    }, version).children);
   }
 
   const items: INavLinkGroup[] = createList(resourceItems);
+  const navStyles: any = (properties: any) => ({
+    chevronIcon: [
+      properties.isExpanded && {
+        transform: `rotate(0deg)`
+      },
+      !properties.isExpanded && {
+        transform: `rotate(-90deg)`
+      }
+    ]
+  });
 
   return (
     <section>
       <SearchBox
         placeholder={translateMessage('Search resources')}
-        onChange={searchValueChanged}
+        onChange={changeSearchValue}
         styles={{ field: { paddingLeft: 10 } }}
       />
       <hr />
-      <Nav groups={items} className={classes.queryList} />
+      <ChoiceGroup
+        label={translateMessage('Select version')}
+        defaultSelectedKey={version}
+        options={versions}
+        onChange={changeVersion}
+      />;
+      <Label>
+        <FormattedMessage id='Resources available' />
+      </Label>
+      <Nav
+        groups={items}
+        onLinkClick={clickLink}
+        styles={navStyles}
+        className={classes.queryList} />
     </section>
   )
 }
+
 // @ts-ignore
 const styledResourceExplorer = styled(ResourceExplorer, sidebarStyles);
 export default styledResourceExplorer;
