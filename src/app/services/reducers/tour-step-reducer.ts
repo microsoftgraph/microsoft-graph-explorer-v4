bc691c43f20ffed1adbcfe21db00f8eb99cda17e
Onokaev <evansonoka05.j@gmail.com>
2021-10-06 17:28:57 +0300

    fixed linting

--- src/app/services/reducers/tour-step-reducer.ts
+++ src/app/services/reducers/tour-step-reducer.ts
@@ -2,10 +2,10 @@ import { IAction } from '../../../types/action';
 import { SET_NEXT_TOUR_STEP_SUCCESS } from '../redux-constants';
 
 export function setNextTourStep(state = [], action: IAction) {
-    switch (action.type) {
-        case SET_NEXT_TOUR_STEP_SUCCESS:
-            return action.response;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_NEXT_TOUR_STEP_SUCCESS:
+      return action.response;
+    default:
+      return state;
+  }
 }
