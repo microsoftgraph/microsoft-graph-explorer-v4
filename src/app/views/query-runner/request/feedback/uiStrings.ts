02486bb403110627a375b59490f18175f50c4945
Elinor <ekaguongo@gmail.com>
2021-11-10 18:45:42 +0300

    add localization

--- src/app/views/query-runner/request/feedback/uiStrings.ts
+++ src/app/views/query-runner/request/feedback/uiStrings.ts
@@ -6,7 +6,7 @@ export const uiStringMap: any = {
   Prompt_YesLabel: translateMessage('Sure'),
   Prompt_NoLabel: translateMessage('Sure'),
   Graph_Explorer_Rating_Question: translateMessage('Graph Rating Question'),
-  Rating_Values_1: '1 - Not at all likely',
+  Rating_Values_1: `1 - ${translateMessage('Not at all likely')}`,
   Rating_Values_2: '2',
   Rating_Values_3: '3',
   Rating_Values_4: '4',
@@ -15,6 +15,6 @@ export const uiStringMap: any = {
   Rating_Values_7: '7',
   Rating_Values_8: '8',
   Rating_Values_9: '9',
-  Rating_Values_10: '10 - Extremely likely',
+  Rating_Values_10: `10 - ${translateMessage('Extremely likely')}`,
   Question_Question: translateMessage('Please tell us more. Why did you choose that answer')
 }
