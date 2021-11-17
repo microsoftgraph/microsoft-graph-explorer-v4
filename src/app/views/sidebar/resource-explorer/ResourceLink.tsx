d10fdd37f21599a6163d928362a12684427bbbcd
Charles Wahome <thewahome.cw@gmail.com>
2021-11-17 14:51:45 +0300

    remove right click; display methods inside context

--- src/app/views/sidebar/resource-explorer/ResourceLink.tsx
+++ src/app/views/sidebar/resource-explorer/ResourceLink.tsx
@@ -1,4 +1,7 @@
-import { ContextualMenuItemType, Icon, INavLink } from '@fluentui/react';
+import {
+  ContextualMenuItemType, Icon, IconButton,
+  IContextualMenuItem, INavLink
+} from '@fluentui/react';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -7,9 +10,7 @@ import { runQuery } from '../../../services/actions/query-action-creators';
 import { setSampleQuery } from '../../../services/actions/query-input-action-creators';
 import { GRAPH_URL } from '../../../services/graph-constants';
 import { translateMessage } from '../../../utils/translate-messages';
-import { MethodIndicator } from './methods';
-import { getAvailableMethods, getUrlFromLink } from './resource-explorer.utils';
-import ContextMenu from '../../common/ContextMenu';
+import { getAvailableMethods, getUrlFromLink, removeCounter } from './resource-explorer.utils';
 
 interface IResourceLink {
   link: any;
@@ -21,20 +22,6 @@ interface IResourceLink {
 const ResourceLink = (props: IResourceLink) => {
   const dispatch = useDispatch();
   const { link: resourceLink, version } = props;
-
-  const selectContextItem = (e: any, item: any, link: INavLink) => {
-    switch (item.key) {
-      case 'isolate':
-        props.isolateTree(link);
-        break;
-      case 'show-query-parameters':
-        props.openPanel('show-query-parameters', link);
-        break;
-      default:
-        setQuery(link);
-        break;
-    }
-  };
 
   const setQuery = (link: INavLink) => {
     const sampleUrl = `${GRAPH_URL}/${version}${getUrlFromLink(link)}`;
@@ -51,54 +38,83 @@ const ResourceLink = (props: IResourceLink) => {
     }
   }
 
-  const menuItems = [
-    {
-      key: 'actions',
-      itemType: ContextualMenuItemType.Header,
-      text: resourceLink.name
+  const items = getMenuItems();
+
+  return <span style={{ display: 'flex' }}>
+    {!!resourceLink.iconresourceLink && <Icon style={{ margin: '0 4px' }}
+      {...resourceLink.iconresourceLink} />}
+    {resourceLink.name}
+    {items.length > 0 && <IconButton
+      ariaLabel={translateMessage('More actions')}
+      role='button'
+      style={{ float: 'right', position: 'absolute', right: 0 }}
+      styles={{
+        label: { marginBottom: -20 },
+        menuIcon: { fontSize: 20 }
+      }}
+      menuIconProps={{ iconName: 'MoreVertical' }}
+      menuProps={{
+        shouldFocusOnMount: true,
+        alignTargetEdge: true,
+        items
+      }}
+    />}
+  </span>;
+
+  function getMenuItems() {
+    const menuItems: IContextualMenuItem[] = [];
+
+    if (resourceLink!.links!.length > 0) {
+      menuItems.push(
+        {
+          key: 'isolate',
+          text: translateMessage('Isolate'),
+          itemType: ContextualMenuItemType.Normal,
+          onClick: () => props.isolateTree(resourceLink)
+        });
     }
-  ];
 
-  if (resourceLink!.links!.length > 0) {
-    menuItems.push(
-      {
-        key: 'isolate',
-        text: translateMessage('Isolate'),
-        itemType: ContextualMenuItemType.Normal
+    if (resourceLink.type === 'path') {
+      menuItems.push(
+        {
+          key: 'run-query',
+          text: translateMessage('Run Query'),
+          itemType: ContextualMenuItemType.Normal,
+          onClick: () => setQuery(resourceLink)
+        },
+        {
+          key: 'show-query-parameters',
+          text: translateMessage('Access query parameters'),
+          itemType: ContextualMenuItemType.Normal,
+          onClick: () => props.openPanel('show-query-parameters', resourceLink)
+        });
+    }
+
+    const availableMethods = getAvailableMethods(resourceLink.labels, version);
+    if (availableMethods.length > 0) {
+      const subMenuItems: IContextualMenuItem[] = [];
+      availableMethods.forEach(element => {
+        subMenuItems.push({ key: element, text: element, disabled: true })
       });
-  }
-
-  if (resourceLink.type === 'path') {
-    menuItems.push(
-      {
-        key: 'run-query',
-        text: translateMessage('Run Query'),
-        itemType: ContextualMenuItemType.Normal
-      },
-      {
-        key: 'show-query-parameters',
-        text: translateMessage('Query parameters'),
-        itemType: ContextualMenuItemType.Normal
+      menuItems.unshift({
+        key: 'supported-methods',
+        itemType: ContextualMenuItemType.Normal,
+        text: translateMessage('Show methods'),
+        subMenuProps: {
+          items: subMenuItems
+        }
       });
+    }
+
+    if (menuItems.length > 0) {
+      menuItems.unshift({
+        key: 'actions',
+        itemType: ContextualMenuItemType.Header,
+        text: removeCounter(resourceLink.name)
+      });
+    }
+    return menuItems;
   }
-
-  const availableMethods = getAvailableMethods(resourceLink.labels, version);
-
-  return <ContextMenu
-    style={{
-      flexGrow: 1,
-      textAlign: 'left',
-      boxSizing: 'border-box'
-    }}
-    key={resourceLink.key}
-    items={menuItems}
-    onItemClick={(e: any, item: any) => selectContextItem(e, item, resourceLink)}>
-    <span style={{ display: 'flex' }}>
-      {!!resourceLink.iconresourceLink && <Icon style={{ margin: '0 4px' }} {...resourceLink.iconresourceLink} />}
-      {resourceLink.name}
-      {availableMethods.length > 0 && <MethodIndicator methods={availableMethods} key={resourceLink.key} />}
-    </span>
-  </ContextMenu>;
 }
 
 export default ResourceLink;
