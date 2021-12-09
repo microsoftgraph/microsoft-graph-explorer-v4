6e92f2cfe6079f865c72ffc6152a7fbdc7433e92
Evans Aboge <evansonoka05.j@gmail.com>
2021-12-09 14:51:14 +0300

    Merge dev into the release branch

--- src/app/services/reducers/index.ts
+++ src/app/services/reducers/index.ts
@@ -1,40 +1,46 @@
 import { combineReducers } from 'redux';
+
 import { adaptiveCard } from './adaptive-cards-reducer';
 import { authToken, consentedScopes } from './auth-reducers';
 import { autoComplete } from './autocomplete-reducer';
 import { devxApi } from './devxApi-reducers';
 import { dimensions } from './dimensions-reducers';
-import { permissionsPanelOpen } from './permissions-panel-reducer';
 import { graphExplorerMode } from './graph-explorer-mode-reducer';
+import { policies } from './ocps-reducers';
+import { permissionsPanelOpen } from './permissions-panel-reducer';
 import { scopes } from './permissions-reducer';
 import { profile } from './profile-reducer';
+import { proxyUrl } from './proxy-url-reducer';
 import { sampleQuery } from './query-input-reducers';
 import { isLoadingData } from './query-loading-reducers';
 import { graphResponse } from './query-runner-reducers';
 import { queryRunnerStatus } from './query-runner-status-reducers';
 import { history } from './request-history-reducers';
+import { resources } from './resources-reducer';
 import { responseAreaExpanded } from './response-expanded-reducer';
 import { samples } from './samples-reducers';
 import { snippets } from './snippet-reducer';
 import { termsOfUse } from './terms-of-use-reducer';
 import { theme } from './theme-reducer';
 import { sidebarProperties } from './toggle-sidebar-reducer';
-import { proxyUrl } from './proxy-url-reducer';
-import { policies } from './ocps-reducers';
 
 export default combineReducers({
   adaptiveCard,
   authToken,
   autoComplete,
   consentedScopes,
+  devxApi,
+  dimensions,
   graphExplorerMode,
   graphResponse,
-  devxApi,
   history,
   isLoadingData,
+  permissionsPanelOpen,
   profile,
   proxyUrl,
+  policies,
   queryRunnerStatus,
+  resources,
   responseAreaExpanded,
   sampleQuery,
   samples,
@@ -42,8 +48,5 @@ export default combineReducers({
   sidebarProperties,
   snippets,
   termsOfUse,
-  theme,
-  dimensions,
-  permissionsPanelOpen,
-  policies
+  theme
 });
