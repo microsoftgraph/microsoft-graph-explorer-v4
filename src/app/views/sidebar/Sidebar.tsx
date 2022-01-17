d4ac813809121e38a57c1c869834dd7f7a83531d
Evans Aboge <evansonoka05.j@gmail.com>
2022-01-17 15:40:40 +0300

    restore resources tab for review

--- src/app/views/sidebar/Sidebar.tsx
+++ src/app/views/sidebar/Sidebar.tsx
@@ -5,13 +5,16 @@ import { telemetry } from '../../../telemetry';
 import { translateMessage } from '../../utils/translate-messages';
 import History from './history/History';
 import SampleQueries from './sample-queries/SampleQueries';
-
+import { ResourceExplorer } from './resource-explorer';
 export const Sidebar = () => {
   return (
     <div>
       <Pivot onLinkClick={onPivotItemClick} overflowBehavior='menu'>
         <PivotItem headerText={translateMessage('Sample Queries')} itemIcon='Rocket' itemKey='sample-queries'>
           <SampleQueries />
+        </PivotItem>
+        <PivotItem headerText={translateMessage('Resources')} itemIcon='ExploreData' itemKey='resources'>
+          <ResourceExplorer />
         </PivotItem>
         <PivotItem headerText={translateMessage('History')} itemIcon='History' itemKey='history'>
           <History />
