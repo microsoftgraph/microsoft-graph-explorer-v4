9055147c65bff157a7dacb7ab06b59bd4fb9525d
Elinor <ekaguongo@gmail.com>
2022-01-27 11:00:47 +0300

    Fix: Add aria-label for items in the sample queries tab (#1378)
    * Add ariaLabel
    
    * change to lowercase

--- src/app/views/sidebar/sample-queries/SampleQueries.tsx
+++ src/app/views/sidebar/sample-queries/SampleQueries.tsx
@@ -238,6 +238,7 @@ export class SampleQueries extends Component<ISampleQueriesProps, any> {
               (selectionDisabled ? classes.rowDisabled : '')
             }
             data-selection-disabled={selectionDisabled}
+            getRowAriaLabel={() => props.item.method.toLowerCase() + props.item.humanName}
           />
         </div>
       );
