f89ba84a824bcd9cf2a6e940747ac919faf37d06
Evans Aboge <evansonoka05.j@gmail.com>
2022-02-08 16:11:43 +0300

    fix linting error

--- src/app/utils/searchbox.styles.ts
+++ src/app/utils/searchbox.styles.ts
@@ -1,14 +1,14 @@
 export const searchBoxStyles: any = () => ({
-    field: [
-        {
-            paddingLeft: 10
-        },
-        {
-            selectors: {
-                ':focus': {
-                    outline: 'none !important'
-                }
-            }
+  field: [
+    {
+      paddingLeft: 10
+    },
+    {
+      selectors: {
+        ':focus': {
+          outline: 'none !important'
         }
-    ]
+      }
+    }
+  ]
 })
