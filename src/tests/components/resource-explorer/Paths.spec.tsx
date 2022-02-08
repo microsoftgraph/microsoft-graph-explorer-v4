d6b0f158febbc9e5684cb7bf98bebfbac233f8dc
Evans Aboge <evansonoka05.j@gmail.com>
2022-02-08 10:55:16 +0300

    fix linting error

--- src/tests/components/resource-explorer/Paths.spec.tsx
+++ src/tests/components/resource-explorer/Paths.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import Paths from '../../../app/views/sidebar/resource-explorer/panels/Paths';
 import { IResourceLink, ResourceLinkType } from '../../../types/resources';
 
