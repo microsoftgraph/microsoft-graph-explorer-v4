cbc09067e933678b48669ee1f83a765a7179ff4a
Elinor <ekaguongo@gmail.com>
2022-03-01 16:52:12 +0300

    change to percentage values

--- src/app/services/reducers/dimensions-reducers.ts
+++ src/app/services/reducers/dimensions-reducers.ts
@@ -5,19 +5,19 @@ import { RESIZE_SUCCESS } from '../redux-constants';
 const initialState: IDimensions = {
   request: {
     width: '100%',
-    height: '36vh'
+    height: '35%'
   },
   response: {
     width: '100%',
-    height: '62vh'
+    height: '60%'
   },
   sidebar: {
     width: '26%',
-    height: '98vh'
+    height: '100%'
   },
   content: {
     width: '72%',
-    height: '98vh'
+    height: '100%'
   }
 };
 
