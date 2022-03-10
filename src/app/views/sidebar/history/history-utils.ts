e7cabd48cf61d8a7e29700532c7b0e60525f90cb
Charles Wahome <thewahome.cw@gmail.com>
2022-03-10 22:50:00 +0300

    validate get item

--- src/app/views/sidebar/history/history-utils.ts
+++ src/app/views/sidebar/history/history-utils.ts
@@ -17,7 +17,7 @@ export async function readHistoryData(): Promise<IHistoryItem[]> {
     if (historyItemHasExpired(creationTime)) {
       historyStorage.removeItem(creationTime);
     } else {
-      const historyItem: IHistoryItem = await historyStorage.getItem(creationTime);
+      const historyItem: IHistoryItem = await historyStorage.getItem(creationTime)!;
       historyData = [...historyData, historyItem];
     }
   }
