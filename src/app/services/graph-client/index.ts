563376f16b57c660bc8974b726696314d60c6ed5
Elinor <ekaguongo@gmail.com>
2022-03-16 20:13:48 +0300

    update authentication provider

--- src/app/services/graph-client/index.ts
+++ src/app/services/graph-client/index.ts
@@ -1,4 +1,20 @@
-import { GraphAuthenticationProvider } from './GraphAuthenticationProvider';
+import { InteractionType } from '@azure/msal-browser';
+import {
+  AuthCodeMSALBrowserAuthenticationProvider,
+  AuthCodeMSALBrowserAuthenticationProviderOptions
+} from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
+import { authenticationWrapper } from '../../../modules/authentication';
+import { msalApplication } from '../../../modules/authentication/msal-app';
+import { DEFAULT_USER_SCOPES } from '../graph-constants';
 
 export { GraphClient } from './graph-client';
-export const authProvider = new GraphAuthenticationProvider();
+
+const scopes = DEFAULT_USER_SCOPES.split(' ');
+
+const msalAuthOptions:AuthCodeMSALBrowserAuthenticationProviderOptions = {
+  account: authenticationWrapper.getAccount()!,
+  interactionType: InteractionType.Popup ,
+  scopes
+}
+
+export const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(msalApplication, msalAuthOptions);
