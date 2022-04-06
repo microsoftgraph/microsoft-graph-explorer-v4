732d683ec2a5f65f479143bf84f0763844a912e5
Elinor <ekaguongo@gmail.com>
2022-04-06 17:11:31 +0300

    use previous custom authentication provider

--- src/app/services/graph-client/index.ts
+++ src/app/services/graph-client/index.ts
@@ -1,20 +1,4 @@
-import { InteractionType } from '@azure/msal-browser';
-import {
-  AuthCodeMSALBrowserAuthenticationProvider,
-  AuthCodeMSALBrowserAuthenticationProviderOptions
-} from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
-import { authenticationWrapper } from '../../../modules/authentication';
-import { msalApplication } from '../../../modules/authentication/msal-app';
-import { DEFAULT_USER_SCOPES } from '../graph-constants';
+import { GraphAuthenticationProvider } from './GraphAuthenticationProvider';
 
 export { GraphClient } from './graph-client';
-
-const scopes = DEFAULT_USER_SCOPES.split(' ');
-
-const msalAuthOptions:AuthCodeMSALBrowserAuthenticationProviderOptions = {
-  account: authenticationWrapper.getAccount()!,
-  interactionType: InteractionType.Popup ,
-  scopes
-}
-
-export const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(msalApplication, msalAuthOptions);
+export const authProvider = new GraphAuthenticationProvider();
