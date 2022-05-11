4303a1cee3d7391cf517983f0091e54e2c9a862b
Millicent Achieng <achieng.milli@gmail.com>
2022-05-11 10:46:58 +0300

    Remove Objective C from list of code snippets languages

--- src/telemetry/component-names.ts
+++ src/telemetry/component-names.ts
@@ -54,20 +54,18 @@ export const MICROSOFT_APIS_TERMS_OF_USE_LINK = 'Microsoft APIs terms of use lin
 export const MICROSOFT_PRIVACY_STATEMENT_LINK = 'Microsoft privacy statement link';
 export const MICROSOFT_GRAPH_API_REFERENCE_DOCS_LINK = 'Microsoft graph API reference docs link';
 export const CODE_SNIPPET_LANGUAGES = {
-  CSharp: 'C Sharp sdk link',
-  JavaScript: 'Javascript sdk link',
-  Java: 'Java snippet sdk link',
-  Go: 'Go sdk link',
-  'Objective-C': 'Objective-C sdk link',
-  Powershell: 'Powershell sdk link'
+  CSharp: 'C# SDK link',
+  JavaScript: 'JavaScript SDK link',
+  Java: 'Java snippet SDK link',
+  Go: 'Go SDK link',
+  Powershell: 'PowerShell SDK link'
 }
 export const CODE_SNIPPET_DOCS_LINKS = {
   CSharp: 'C# snippet docs link',
   JavaScript: 'JavaScript snippet docs link',
   Java: 'Java snippet docs link',
   Go: 'Go snippet docs link',
-  'Objective-C': 'Objective-C snippet docs link',
-  Powershell: 'Powershell snippet docs link'
+  Powershell: 'PowerShell snippet docs link'
 }
 
 // Actions
