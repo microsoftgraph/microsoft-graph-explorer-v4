db2aad9a33fac9f08286ac24ba85ec26e82a532e
Charles Wahome <thewahome.cw@gmail.com>
2022-05-18 17:01:19 +0300

    fix linting in jest configurations

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -4,12 +4,12 @@
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process () {
+  process() {
     return {
       code: 'module.exports = {};'
     }
   },
-  getCacheKey () {
+  getCacheKey() {
     // The output is always the same.
     return 'cssTransform';
   }
--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -6,7 +6,7 @@ const path = require('path');
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process (src, filename) {
+  process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
 
     if (filename.match(/\.svg$/)) {
