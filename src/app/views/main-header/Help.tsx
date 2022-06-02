9380aa1413ca7c9f29f2b6e774f00205258f8d4c
Elinor <ekaguongo@gmail.com>
2022-06-02 17:55:36 +0300

    add graph explorer documentation

--- src/app/views/main-header/Help.tsx
+++ src/app/views/main-header/Help.tsx
@@ -1,5 +1,6 @@
 import {
   ContextualMenuItemType,
+  DirectionalHint,
   getId,
   getTheme,
   IconButton,
@@ -42,18 +43,28 @@ export const Help = () => {
       { key: 'divider_1', itemType: ContextualMenuItemType.Divider },
       {
         key: 'ge-documentation',
-        text: translateMessage('Documentation'),
+        text: translateMessage('Get started with Graph Explorer'),
+        href: 'https://docs.microsoft.com/en-us/graph/graph-explorer/graph-explorer-overview?view=graph-rest-1.0',
+        target: '_blank',
+        iconProps: {
+          iconName: 'TextDocument'
+        },
+        onClick: () => trackDocumentationLinkClickEvent()
+      },
+      {
+        key: 'graph-documentation',
+        text: translateMessage('Graph Documentation'),
         href: ' https://docs.microsoft.com/en-us/graph/api/overview?view=graph-rest-1.0',
         target: '_blank',
         iconProps: {
           iconName: 'Documentation'
         },
-        onClick: () => trackDocumentationLinkClickEvent()
+        onClick: () => trackGraphDocumentationLinkClickEvent()
       },
       {
         key: 'github',
         text: 'GitHub',
-        href: 'https://github.com/microsoftgraph/microsoft-graph-explorer-v4',
+        href: 'https://github.com/microsoftgraph/microsoft-graph-explorer-v4#readme',
         target: '_blank',
         iconProps: {
           iconName: 'GitHubLogo',
@@ -82,6 +93,12 @@ export const Help = () => {
     });
   };
 
+  const trackGraphDocumentationLinkClickEvent = () => {
+    telemetry.trackEvent(eventTypes.LINK_CLICK_EVENT, {
+      ComponentName: componentNames.GRAPH_DOCUMENTATION_LINK
+    });
+  }
+
   const trackGithubLinkClickEvent = () => {
     telemetry.trackEvent(eventTypes.LINK_CLICK_EVENT, {
       ComponentName: componentNames.GITHUB_LINK
@@ -91,12 +108,13 @@ export const Help = () => {
   const calloutStyles: React.CSSProperties = {
     overflowY: 'hidden'
   }
-  const { iconButton: helpButtonStyles, tooltipStyles } = mainHeaderStyles(currentTheme);
+  const { iconButton: helpButtonStyles, tooltipStyles, helpContainerStyles } = mainHeaderStyles(currentTheme);
 
   const menuProperties = {
     shouldFocusOnMount: true,
     alignTargetEdge: true,
     items,
+    directionalHint: DirectionalHint.bottomLeftEdge,
     calloutProps: {
       style: calloutStyles
     },
@@ -104,9 +122,12 @@ export const Help = () => {
   };
 
   return (
-    <div>
+    <div style={helpContainerStyles}>
       <TooltipHost
-        content={translateMessage('Help')}
+        content={
+          <div style={{padding:'3px'}}>
+            {translateMessage('Help')}
+          </div>}
         id={getId()}
         calloutProps={{ gapSpace: 0 }}
         styles={ tooltipStyles }
