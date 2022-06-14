e6e213735ca6bc4646a60266b6fc8eed9b723c86
Evans Aboge <evansonoka05.j@gmail.com>
2022-06-14 13:30:38 +0300

    remove console statement

--- scripts/test.js
+++ scripts/test.js
@@ -19,7 +19,6 @@ require('../config/env');
 const jest = require('jest');
 const execSync = require('child_process').execSync;
 let argv = process.argv.slice(2);
-console.log('Setting this up')
 
 function isInGitRepository() {
   try {
