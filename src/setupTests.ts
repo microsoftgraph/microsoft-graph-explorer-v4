b1f678d0fe974c48d4bb5c47f13ce213702fe27d
Evans Aboge <evansonoka05.j@gmail.com>
2022-06-15 10:06:27 +0300

    fix code smell

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,4 @@
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
 import { GlobalWithFetchMock } from 'jest-fetch-mock';
-
-// configure({ adapter: new Adapter() });
 
 const customGlobal: GlobalWithFetchMock = global as unknown as GlobalWithFetchMock;
 customGlobal.fetch = require('jest-fetch-mock'); // tslint:disable-line
