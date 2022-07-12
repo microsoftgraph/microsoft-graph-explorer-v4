5eb2074cc416ca6a53ea97e99955a9044848931d
Elinor <ekaguongo@gmail.com>
2022-07-12 20:49:35 +0300

    Fix: Update link classes (#1902)

--- src/app/views/app-sections/TermsOfUseMessage.tsx
+++ src/app/views/app-sections/TermsOfUseMessage.tsx
@@ -7,15 +7,13 @@ import { geLocale } from '../../../appLocale';
 import { componentNames, telemetry } from '../../../telemetry';
 import { IRootState } from '../../../types/root';
 import { clearTermsOfUse } from '../../services/actions/terms-of-use-action-creator';
-import { classNames } from '../classnames';
 import { appStyles } from '../App.styles';
 
-const styledTermsOfUseMessage = (props: any) => {
+const styledTermsOfUseMessage = () => {
 
   const { termsOfUse } =
     useSelector((state: IRootState) => state);
 
-  const classes = classNames(props);
   const dispatch = useDispatch();
   if (termsOfUse) {
     return <MessageBar messageBarType={MessageBarType.info}
@@ -27,14 +25,14 @@ const styledTermsOfUseMessage = (props: any) => {
       <a
         onClick={(e) =>
           telemetry.trackLinkClickEvent(e.currentTarget.href, componentNames.MICROSOFT_APIS_TERMS_OF_USE_LINK)}
-        className={classes.links} href={'https://docs.microsoft.com/' + geLocale +
+        className={'ms-Link'} href={'https://docs.microsoft.com/' + geLocale +
           '/legal/microsoft-apis/terms-of-use?context=graph/context'} target='_blank' rel='noopener noreferrer'>
         <FormattedMessage id='Terms of use' /></a>.
       <FormattedMessage id='View the' />
       <a
         onClick={(e) =>
           telemetry.trackLinkClickEvent(e.currentTarget.href, componentNames.MICROSOFT_PRIVACY_STATEMENT_LINK)}
-        className={classes.links} href={'https://privacy.microsoft.com/' + geLocale + '/privacystatement'}
+        className={'ms-Link'} href={'https://privacy.microsoft.com/' + geLocale + '/privacystatement'}
         target='_blank' rel='noopener noreferrer'>
         <FormattedMessage id='Microsoft Privacy Statement' /></a>.
     </MessageBar>;
