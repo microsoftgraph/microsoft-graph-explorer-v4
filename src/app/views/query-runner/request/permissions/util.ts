3e8558de5dea52453429e1a9b4b06732d7cd79a5
Evans Aboge <evansonoka05.j@gmail.com>
2022-08-11 11:52:30 +0300

    validate permissions before accessing array

--- src/app/views/query-runner/request/permissions/util.ts
+++ src/app/views/query-runner/request/permissions/util.ts
@@ -2,10 +2,12 @@ import { IPermission } from '../../../../../types/permissions';
 
 export function setConsentedStatus(tokenPresent: any, permissions: IPermission[], consentedScopes: string[]) {
   if (tokenPresent) {
-    permissions.forEach((permission: IPermission) => {
-      if (consentedScopes.indexOf(permission.value) !== -1) {
-        permission.consented = true;
-      }
-    });
+    if (permissions && permissions.length > 0) {
+      permissions.forEach((permission: IPermission) => {
+        if (consentedScopes.indexOf(permission.value) !== -1) {
+          permission.consented = true;
+        }
+      });
+    }
   }
 }
