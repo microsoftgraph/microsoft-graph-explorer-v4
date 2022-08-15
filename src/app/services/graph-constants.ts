32e75307a9471410eb63dc93a5def3f515dff7d7
MG <t-mgitonga@microsoft.com>
2022-08-15 10:23:54 +0300

    Fix: Update BETA_USER_INFO_URL constant in graph-constants.ts

--- src/app/services/graph-constants.ts
+++ src/app/services/graph-constants.ts
@@ -1,7 +1,7 @@
 export const GRAPH_URL = 'https://graph.microsoft.com';
 export const GRAPH_API_VERSIONS = ['v1.0', 'beta'];
 export const USER_INFO_URL = `${GRAPH_URL}/v1.0/me`;
-export const BETA_USER_INFO_URL = `${GRAPH_URL}/beta/me/profile`;
+export const BETA_USER_INFO_URL = `${GRAPH_URL}/beta/me`;
 export const USER_PICTURE_URL = `${GRAPH_URL}/beta/me/photo/$value`;
 export const AUTH_URL = 'https://login.microsoftonline.com';
 export const DEFAULT_USER_SCOPES = 'openid profile User.Read';
