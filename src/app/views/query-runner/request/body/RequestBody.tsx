d95c10a256adc6be4468d904825945eb2d4b75c3
MG <t-mgitonga@microsoft.com>
2022-08-18 11:29:55 +0300

    Add height on request body

--- src/app/views/query-runner/request/body/RequestBody.tsx
+++ src/app/views/query-runner/request/body/RequestBody.tsx
@@ -4,14 +4,16 @@ import { useSelector } from 'react-redux';
 
 import { IRootState } from '../../../../../types/root';
 import { Monaco } from '../../../common';
+import { convertVhToPx } from '../../../common/dimensions/dimensions-adjustment';
 
 const RequestBody = ({ handleOnEditorChange }: any) => {
-  const { sampleQuery } = useSelector((state: IRootState) => state);
+  const { dimensions: { request: { height } }, sampleQuery } = useSelector((state: IRootState) => state);
 
   return (
     <FocusZone>
       <Monaco
         body={sampleQuery.sampleBody}
+        height={convertVhToPx(height, 60)}
         onChange={(value) => handleOnEditorChange(value)} />
     </FocusZone>
 
