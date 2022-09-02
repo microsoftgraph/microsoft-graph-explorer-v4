3b5dfbb700f637117185f7c46729395af4692b40
Charles Wahome <thewahome.cw@gmail.com>
2022-09-02 10:36:42 +0300

    add react to scope

--- src/app/views/query-response/snippets/Snippets.tsx
+++ src/app/views/query-response/snippets/Snippets.tsx
@@ -1,5 +1,6 @@
 import { FontSizes, Pivot, PivotItem } from '@fluentui/react';
 import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 import { componentNames, telemetry } from '../../../../telemetry';
 import { IRootState } from '../../../../types/root';
