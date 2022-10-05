02e42896d5a78f964df7a5f9e5b685f37d752e64
Charles Wahome <thewahome.cw@gmail.com>
2022-10-05 15:58:48 +0300

    move playwright configuration file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
-require('dotenv');
+require('dotenv').config();
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+
 const baseURL = process.env.PLAYWRIGHT_TESTS_BASE_URL!;
 
 const config: PlaywrightTestConfig = {
