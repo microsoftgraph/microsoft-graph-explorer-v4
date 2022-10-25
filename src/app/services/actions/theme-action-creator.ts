4861590367abf44a5d72deaa82ed09a1b7abbcab
Charles Wahome <thewahome.cw@gmail.com>
2022-10-25 13:33:55 +0300

    use localStorageMiddleware

--- src/app/services/actions/theme-action-creator.ts
+++ src/app/services/actions/theme-action-creator.ts
@@ -1,7 +1,5 @@
 import { Dispatch } from 'redux';
 import { AppThunk } from '../../../store';
-
-import { saveTheme } from '../../../themes/theme-utils';
 import { AppAction } from '../../../types/action';
 import { CHANGE_THEME_SUCCESS } from '../redux-constants';
 
@@ -14,7 +12,6 @@ export function changeThemeSuccess(response: string): AppAction {
 
 export function changeTheme(theme: string): AppThunk {
   return (dispatch: Dispatch): any => {
-    saveTheme(theme);
     return dispatch(changeThemeSuccess(theme));
   };
 }
