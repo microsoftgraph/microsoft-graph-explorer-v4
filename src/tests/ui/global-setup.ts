e9feed44924cee7b61af2450c98d883f3e9aeac4
Charles Wahome <thewahome.cw@gmail.com>
2022-10-26 17:02:53 +0300

    cast baseURL to string

--- src/tests/ui/global-setup.ts
+++ src/tests/ui/global-setup.ts
@@ -1,10 +1,10 @@
 import { chromium, FullConfig } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
-  const { baseURL } = config.projects[0].use;
+  const baseURL = config.projects[0].use.baseURL as string;
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(baseURL!);
+  await page.goto(baseURL);
 }
 
 export default globalSetup;
