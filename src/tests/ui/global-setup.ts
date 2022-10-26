40638896c7595032b9dd74423b08899fba035bd0
Charles Wahome <thewahome.cw@gmail.com>
2022-10-26 16:56:19 +0300

    return assertion for baseUrl

--- src/tests/ui/global-setup.ts
+++ src/tests/ui/global-setup.ts
@@ -4,7 +4,7 @@ async function globalSetup(config: FullConfig) {
   const { baseURL } = config.projects[0].use;
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(baseURL);
+  await page.goto(baseURL!);
 }
 
 export default globalSetup;
