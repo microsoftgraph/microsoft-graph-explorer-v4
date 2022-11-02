c91c7935c15f7886b0ed452cc2874856d20c1b0f
Elinor <ekaguongo@gmail.com>
2022-11-02 12:35:19 +0300

    add accessibility test

---
+++ src/tests/ui/accessibility.spec.ts
@@ -0,0 +1,26 @@
+import AxeBuilder from '@axe-core/playwright';
+import { test, expect } from '@playwright/test';
+
+test.describe('Accessibility', () => {
+  test.use({ viewport: { width: 1024, height: 768 } });
+
+  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
+
+    await page.goto('/');
+    await page.waitForTimeout(500);
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules([
+        'landmark-one-main',
+        'region',
+        'document-title',
+        'html-has-lang',
+        'page-has-heading-one',
+        'landmark-unique',
+        'aria-allowed-attr',
+        'aria-required-children'
+      ])
+      .analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+})
