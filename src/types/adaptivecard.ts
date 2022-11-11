a19984f4e81e541eca40f1ad6c5b42efbec6c4e7
Charles Wahome <thewahome.cw@gmail.com>
2022-11-11 10:27:55 +0300

    Chore: remove class components (#2226)

--- src/types/adaptivecard.ts
+++ src/types/adaptivecard.ts
@@ -1,22 +1,5 @@
-import { IQuery } from './query-runner';
 import * as AdaptiveCardsTemplateAPI from 'adaptivecards-templating';
 
-export interface IAdaptiveCardProps {
-  body: any;
-  card: {
-    pending: boolean;
-    data?: IAdaptiveCardContent
-  };
-  intl: {
-    message: object;
-  };
-  hostConfig?: any;
-  sampleQuery?: IQuery;
-  actions?: {
-    getAdaptiveCard: Function;
-  };
-  queryStatus?: any;
-}
 
 export interface IAdaptiveCardContent {
   card?: AdaptiveCardsTemplateAPI.Template;
