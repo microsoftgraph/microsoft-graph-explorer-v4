b11fa204729267cf19944f535a48b802ee223fa8
Charles Wahome <thewahome.cw@gmail.com>
2022-11-23 15:59:34 +0800

    Enhancement: sample queries cache (#2239)

--- src/app/middleware/localStorageMiddleware.ts
+++ src/app/middleware/localStorageMiddleware.ts
@@ -1,10 +1,22 @@
+import { samplesCache } from '../../modules/cache/samples.cache';
 import { saveTheme } from '../../themes/theme-utils';
 import { AppAction } from '../../types/action';
-import { CHANGE_THEME_SUCCESS } from '../services/redux-constants';
+import {
+  CHANGE_THEME_SUCCESS, SAMPLES_FETCH_SUCCESS
+} from '../services/redux-constants';
 
 const localStorageMiddleware = () => (next: any) => (action: AppAction) => {
-  if (action.type === CHANGE_THEME_SUCCESS) {
-    saveTheme(action.response);
+  switch (action.type) {
+    case CHANGE_THEME_SUCCESS:
+      saveTheme(action.response);
+      break;
+
+    case SAMPLES_FETCH_SUCCESS:
+      samplesCache.saveSamples(action.response);
+      break;
+
+    default:
+      break;
   }
   return next(action);
 };
