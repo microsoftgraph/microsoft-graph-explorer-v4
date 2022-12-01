592b2f1ebc3ca61ba9b15b47466b74e3cca83195
Elinor <ekaguongo@gmail.com>
2022-12-01 16:13:37 +0300

    replace deprecated methods

--- src/app/views/App.tsx
+++ src/app/views/App.tsx
@@ -86,7 +86,7 @@ class App extends Component<IAppProps, IAppState> {
 
   public componentDidMount = async () => {
     this.displayToggleButton(this.mediaQueryList);
-    this.mediaQueryList.addListener(this.displayToggleButton);
+    this.mediaQueryList.addEventListener('click',this.displayToggleButton);
 
     const urlParams = new URLSearchParams(window.location.search);
     const sessionId = urlParams.get('sid');
@@ -186,7 +186,7 @@ class App extends Component<IAppProps, IAppState> {
 
   public componentWillUnmount(): void {
     window.removeEventListener('message', this.receiveMessage);
-    this.mediaQueryList.removeListener(this.displayToggleButton);
+    this.mediaQueryList.removeEventListener('click', this.displayToggleButton);
   }
 
   private handleThemeChangeMsg = (msg: IThemeChangedMessage) => {
