dd8cb8796ca9cac13224f3f645cf514950ad88ce
Gavin Barron <gavin.barron@gmail.com>
2022-12-08 15:23:40 -0800

    Update src/modules/validation/abnf.spec.ts

--- src/modules/validation/abnf.spec.ts
+++ src/modules/validation/abnf.spec.ts
@@ -21,7 +21,8 @@ const validUrls = [
 const invalidUrls = [
   'https://graph.microsoft.com/me+you',
   'https://graph.microsoft.com/v1.0/me/messages?$$select=id',
-  'https://graph.microsoft.com/v1.0/me/drive/root:/Encoded%'
+  'https://graph.microsoft.com/v1.0/me/drive/root:/Encoded%',
+  'https://graph.microsoft.com/v1.0/me/drive/root:/Encoded%2'
 ];
 
 /*
