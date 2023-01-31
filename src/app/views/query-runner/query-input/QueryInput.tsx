888c68d6ad51eb5dba0b7793dd6691c9c7916d50
Onokaev <evansonoka05.j@gmail.com>
2023-01-31 11:45:32 +0300

    prevent running queries for URLs with no protocol

--- src/app/views/query-runner/query-input/QueryInput.tsx
+++ src/app/views/query-runner/query-input/QueryInput.tsx
@@ -61,7 +61,7 @@ const QueryInput = (props: IQueryInputProps) => {
   }
 
   const runQuery = () => {
-    if (!sampleQuery.sampleUrl) {
+    if (!sampleQuery.sampleUrl || sampleQuery.sampleUrl.indexOf('https://') === -1) {
       return;
     }
     handleOnRunQuery(sampleQuery);
