48bc6580931cb95d509faf5e2af9451b1aad679b
MaryannGitonga <naygitonga4@gmail.com>
2023-02-01 13:04:00 +0300

    Fix failing test

--- src/app/views/sidebar/resource-explorer/resource-explorer.utils.spec.ts
+++ src/app/views/sidebar/resource-explorer/resource-explorer.utils.spec.ts
@@ -4,7 +4,7 @@ import {
   createResourcesList, getAvailableMethods, getCurrentTree, getResourcePaths, getUrlFromLink, removeCounter
 } from './resource-explorer.utils';
 
-const resource: IResource = {
+const res = {
   'segment': '/',
   'labels': [
     {
@@ -20,38 +20,222 @@ const resource: IResource = {
       ]
     }
   ],
-  'children': [
-    {
-      'segment': 'accessReviewDecisions',
-      'labels': [
-        {
-          'name': 'beta',
-          'methods': [
-            'Get',
-            'Post'
-          ]
-        }
-      ],
-      'children': [
-        {
-          'segment': '{accessReviewDecision-id}',
-          'labels': [
-            {
-              'name': 'beta',
-              'methods': [
-                'Get',
-                'Patch',
-                'Delete'
-              ]
-            }
-          ],
-          'children': []
-        }
-      ]
-    }
-  ]
-};
+  'children': [{
+    'segment': 'appCatalogs',
+    'labels': [
+      {
+        'name': 'v1.0',
+        'methods': [
+          'Get',
+          'Patch'
+        ]
+      },
+      {
+        'name': 'beta',
+        'methods': [
+          'Get',
+          'Patch'
+        ]
+      }
+    ],
+    'children': [
+      {
+        'segment': 'teamsApps',
+        'labels': [
+          {
+            'name': 'v1.0',
+            'methods': [
+              'Get',
+              'Post'
+            ]
+          },
+          {
+            'name': 'beta',
+            'methods': [
+              'Get',
+              'Post'
+            ]
+          }
+        ],
+        'children': [
+          {
+            'segment': '{teamsApp-id}',
+            'labels': [
+              {
+                'name': 'v1.0',
+                'methods': [
+                  'Get',
+                  'Patch',
+                  'Delete'
+                ]
+              },
+              {
+                'name': 'beta',
+                'methods': [
+                  'Get',
+                  'Patch',
+                  'Delete'
+                ]
+              }
+            ],
+            'children': [
+              {
+                'segment': 'appDefinitions',
+                'labels': [
+                  {
+                    'name': 'v1.0',
+                    'methods': [
+                      'Get',
+                      'Post'
+                    ]
+                  },
+                  {
+                    'name': 'beta',
+                    'methods': [
+                      'Get',
+                      'Post'
+                    ]
+                  }
+                ],
+                'children': [
+                  {
+                    'segment': '{teamsAppDefinition-id}',
+                    'labels': [
+                      {
+                        'name': 'v1.0',
+                        'methods': [
+                          'Get',
+                          'Patch',
+                          'Delete'
+                        ]
+                      },
+                      {
+                        'name': 'beta',
+                        'methods': [
+                          'Get',
+                          'Patch',
+                          'Delete'
+                        ]
+                      }
+                    ],
+                    'children': [
+                      {
+                        'segment': 'bot',
+                        'labels': [
+                          {
+                            'name': 'v1.0',
+                            'methods': [
+                              'Get',
+                              'Patch',
+                              'Delete'
+                            ]
+                          },
+                          {
+                            'name': 'beta',
+                            'methods': [
+                              'Get',
+                              'Patch',
+                              'Delete'
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        'segment': 'colorIcon',
+                        'labels': [
+                          {
+                            'name': 'beta',
+                            'methods': [
+                              'Get',
+                              'Patch',
+                              'Delete'
+                            ]
+                          }
+                        ],
+                        'children': [
+                          {
+                            'segment': 'hostedContent',
+                            'labels': [
+                              {
+                                'name': 'beta',
+                                'methods': [
+                                  'Get',
+                                  'Patch',
+                                  'Delete'
+                                ]
+                              }
+                            ],
+                            'children': [
+                              {
+                                'segment': '$value',
+                                'labels': [
+                                  {
+                                    'name': 'beta',
+                                    'methods': [
+                                      'Get',
+                                      'Put'
+                                    ]
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        'segment': 'outlineIcon',
+                        'labels': [
+                          {
+                            'name': 'beta',
+                            'methods': [
+                              'Get',
+                              'Patch',
+                              'Delete'
+                            ]
+                          }
+                        ],
+                        'children': [
+                          {
+                            'segment': 'hostedContent',
+                            'labels': [
+                              {
+                                'name': 'beta',
+                                'methods': [
+                                  'Get',
+                                  'Patch',
+                                  'Delete'
+                                ]
+                              }
+                            ],
+                            'children': [
+                              {
+                                'segment': '$value',
+                                'labels': [
+                                  {
+                                    'name': 'beta',
+                                    'methods': [
+                                      'Get',
+                                      'Put'
+                                    ]
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }]};
 
+const resource = JSON.parse(JSON.stringify(res)) as IResource
 describe('Resource payload should', () => {
   it('have children', async () => {
     const resources: any = { ...resource };
@@ -98,7 +282,7 @@ describe('Resource payload should', () => {
   it('return a string without counters', async () => {
     const version = 'v1.0';
     const paths = ['/', 'appCatalogs', 'teamsApps'];
-    const level = 2;
+    const level = 3 ;
     const currentTree = getCurrentTree({
       paths,
       level,
