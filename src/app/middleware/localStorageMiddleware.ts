74f3c063d8dc9f0cd6a6934656f1db3845b4db52
Charles Wahome <thewahome.cw@gmail.com>
2023-04-04 17:55:53 +0300

    remove middleware resource saving

--- src/app/middleware/localStorageMiddleware.ts
+++ src/app/middleware/localStorageMiddleware.ts
@@ -1,9 +1,8 @@
-import { resourcesCache } from '../../modules/cache/resources.cache';
 import { samplesCache } from '../../modules/cache/samples.cache';
 import { saveTheme } from '../../themes/theme-utils';
 import { AppAction } from '../../types/action';
 import {
-  CHANGE_THEME_SUCCESS, FETCH_RESOURCES_SUCCESS, SAMPLES_FETCH_SUCCESS
+  CHANGE_THEME_SUCCESS, SAMPLES_FETCH_SUCCESS
 } from '../services/redux-constants';
 
 const localStorageMiddleware = () => (next: any) => (action: AppAction) => {
@@ -14,10 +13,6 @@ const localStorageMiddleware = () => (next: any) => (action: AppAction) => {
 
     case SAMPLES_FETCH_SUCCESS:
       samplesCache.saveSamples(action.response);
-      break;
-
-    case FETCH_RESOURCES_SUCCESS:
-      resourcesCache.saveResources(action.response);
       break;
 
     default:
