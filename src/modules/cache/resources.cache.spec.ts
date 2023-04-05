75d2f5b56d534b8f72666118e44cc217fd443e9d
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2023-04-05 15:36:17 +0300

    Task: Add browser storage for resources (#2373)

---
+++ src/modules/cache/resources.cache.spec.ts
@@ -0,0 +1,92 @@
+import { IResource } from '../../types/resources';
+import { resourcesCache } from './resources.cache';
+jest.mock('localforage', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  config: () => { },
+  createInstance: () => ({
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    getItem: () => { },
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    setItem: () => { }
+  })
+}));
+
+const emptyResource: IResource = {
+  segment: '',
+  labels: [],
+  children: []
+}
+
+const resources: IResource = {
+  segment: '/',
+  labels: [
+    {
+      name: 'v1.0',
+      methods: [
+        'Get'
+      ]
+    },
+    {
+      name: 'beta',
+      methods: [
+        'Get'
+      ]
+    }
+  ],
+  children: [
+    {
+      segment: 'accessReviewDecisions',
+      labels: [
+        {
+          name: 'beta',
+          methods: [
+            'Get',
+            'Post'
+          ]
+        }
+      ],
+      children: [
+        {
+          segment: '{accessReviewDecision-id}',
+          labels: [
+            {
+              name: 'beta',
+              methods: [
+                'Get',
+                'Patch',
+                'Delete'
+              ]
+            }
+          ],
+          children: []
+        }
+      ]
+    }
+  ]
+};
+
+beforeEach(async () => {
+  // Save resource in the cache
+  await resourcesCache.saveResources(resources);
+});
+
+afterEach(async () => {
+  // Clear the cache
+  await resourcesCache.saveResources(emptyResource);
+});
+
+describe('Resources Cache should', () => {
+
+  it('update the cache after 3 days', async () => {
+    // Moving the clock forward by 3 days
+    const currentTime = new Date().getTime();
+    jest.spyOn(Date, 'now').mockImplementation(() => currentTime + 3 * 24 * 60 * 60 * 1000);
+
+    // Fetch the resource and check that it's updated
+    const updatedResource = await resourcesCache.readResources();
+
+    expect(updatedResource).toEqual(null);
+  });
+});
+
+
