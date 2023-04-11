490dadd051cb2c3652ce4497b7997e99bd5a6e78
Charles Wahome <thewahome.cw@gmail.com>
2023-04-11 12:17:17 +0300

    change path name to collection

--- src/app/views/common/registry/popups.tsx
+++ src/app/views/common/registry/popups.tsx
@@ -3,7 +3,7 @@ import { lazy } from 'react';
 export const popups = new Map<string, any>([
   ['share-query', lazy(() => import('../../query-runner/query-input/share-query/ShareQuery'))],
   ['theme-chooser', lazy(() => import('../../main-header/settings/ThemeChooser'))],
-  ['preview-collection', lazy(() => import('../../sidebar/resource-explorer/panels/PreviewCollection'))]
+  ['preview-collection', lazy(() => import('../../sidebar/resource-explorer/collection/PreviewCollection'))]
 ]);
 
 export type PopupItem = 'share-query' | 'theme-chooser' | 'preview-collection';
