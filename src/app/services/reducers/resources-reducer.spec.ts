79fc7095da5dca40a98bea1cb531bf693de2b564
Charles Wahome <thewahome.cw@gmail.com>
2023-05-02 16:39:28 +0300

    move tests to the correct file

--- src/app/services/reducers/resources-reducer.spec.ts
+++ src/app/services/reducers/resources-reducer.spec.ts
@@ -1,13 +1,10 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { addResourcePaths, removeResourcePaths } from '../../../app/services/actions/resource-explorer-action-creators';
 import { resources } from '../../../app/services/reducers/resources-reducer';
 import {
   FETCH_RESOURCES_ERROR, FETCH_RESOURCES_PENDING,
-  FETCH_RESOURCES_SUCCESS,
-  RESOURCEPATHS_ADD_SUCCESS,
-  RESOURCEPATHS_DELETE_SUCCESS
+  FETCH_RESOURCES_SUCCESS
 } from '../../../app/services/redux-constants';
 import { IResource, IResourceLink, IResources, ResourceLinkType } from '../../../types/resources';
 
@@ -176,56 +173,6 @@ describe('Resources Reducer', () => {
     const queryAction = { type: FETCH_RESOURCES_PENDING, response: null };
     const state = resources(initialState, queryAction);
     expect(state).toEqual(newState);
-  });
-
-  it('should handle RESOURCEPATHS_ADD_SUCCESS', () => {
-    const expectedActions = [{ response: paths, type: RESOURCEPATHS_ADD_SUCCESS }];
-    const store = mockStore({ resources: {} });
-    store.dispatch(addResourcePaths(paths));
-    expect(store.getActions()).toEqual(expectedActions);
-  });
-
-  it('should handle RESOURCEPATHS_DELETE_SUCCESS', () => {
-    const expectedActions = [{ response: paths, type: RESOURCEPATHS_DELETE_SUCCESS }];
-    const store = mockStore({
-      resources: {
-        paths
-      }
-    });
-    store.dispatch(removeResourcePaths(paths));
-    expect(store.getActions()).toEqual(expectedActions);
-  });
-
-  it('should handle RESOURCEPATHS_ADD_SUCCESS and return new state with the paths', () => {
-    const newState = { ...initialState };
-    newState.paths = resourceLinks;
-    const action_ = {
-      type: RESOURCEPATHS_ADD_SUCCESS,
-      response: paths
-    }
-    const state_ = resources(newState, action_);
-    expect(state_.paths).toEqual(resourceLinks);
-  });
-
-  it('should handle RESOURCEPATHS_DELETE_SUCCESS and return new state with no resource paths', () => {
-    const newState = { ...initialState };
-    newState.paths = resourceLinks;
-    const action_ = {
-      type: RESOURCEPATHS_DELETE_SUCCESS,
-      response: paths
-    }
-    const state_ = resources(newState, action_);
-    expect(state_.paths).toEqual([]);
-  });
-
-  it('should return unchanged state if no relevant action is passed', () => {
-    const newState = { ...initialState };
-    const action_ = {
-      type: 'Dummy',
-      response: { dummy: 'Dummy' }
-    }
-    const state_ = resources(newState, action_);
-    expect(state_).toEqual(newState);
   });
 
 });
