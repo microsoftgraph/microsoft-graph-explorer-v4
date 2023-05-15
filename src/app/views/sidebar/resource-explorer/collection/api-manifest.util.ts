da834f9da4ccfe745fc8633d253a790fa6c6a0ea
Charles Wahome <thewahome.cw@gmail.com>
2023-05-15 11:24:44 +0300

    generate api manifest

---
+++ src/app/views/sidebar/resource-explorer/collection/api-manifest.util.ts
@@ -0,0 +1,38 @@
+import { FolderQueryIcon } from '@fluentui/react-icons-mdl2';
+import { APIManifest, ManifestRequest } from '../../../../../types/api-manifest';
+import { IResourceLink, Method } from '../../../../../types/resources';
+import { GRAPH_BETA_DESCRIPTION_URL, GRAPH_URL, GRAPH_V1_DESCRIPTION_URL } from '../../../../services/graph-constants';
+
+export function generateAPIManifest(paths: IResourceLink[]): APIManifest {
+  return {
+    publisher: {
+      name: 'Microsoft Graph',
+      contactEmail: ''
+    },
+    apiDependencies: [
+      {
+        apiDescripionUrl: paths[0].version === 'beta' ? GRAPH_BETA_DESCRIPTION_URL : GRAPH_V1_DESCRIPTION_URL,
+        auth: {
+          clientId: '',
+          permissions: {
+            delegated: [],
+            application: []
+          }
+        },
+        requests: getRequestsFromPaths(paths)
+      }
+    ]
+  };
+}
+
+function getRequestsFromPaths(paths: IResourceLink[]): ManifestRequest[] {
+  const requests: ManifestRequest[] = [];
+  paths.forEach(path => {
+    const { version, method, url } = path;
+    requests.push({
+      method: method as Method,
+      uriTemplate: `${GRAPH_URL}/${version}${url}`
+    });
+  });
+  return requests;
+}
