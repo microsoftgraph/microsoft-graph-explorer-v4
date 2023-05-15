d1833319be7bf1fb355221f99327ad144c32e49f
Charles Wahome <thewahome.cw@gmail.com>
2023-05-15 11:24:29 +0300

    define api manifest

---
+++ src/types/api-manifest.ts
@@ -0,0 +1,32 @@
+import { Method } from './resources';
+
+export interface APIManifest {
+  publisher: Publisher;
+  apiDependencies: ApiDependency[];
+}
+
+interface ApiDependency {
+  apiDescripionUrl: string;
+  auth: Auth;
+  requests: ManifestRequest[];
+}
+
+export interface ManifestRequest {
+  method: Method;
+  uriTemplate: string;
+}
+
+interface Auth {
+  clientId: string;
+  permissions: Permissions;
+}
+
+interface Permissions {
+  delegated: string[];
+  application: string[];
+}
+
+interface Publisher {
+  name: string;
+  contactEmail: string;
+}
