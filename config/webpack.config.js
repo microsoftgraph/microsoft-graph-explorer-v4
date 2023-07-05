2c99f7d2befe1d3aab1d75f4180bd96a14393c4c
ElinorW <ekaguongo@gmail.com>
2023-07-05 20:06:09 +0300

    update loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -396,7 +396,7 @@ module.exports = function (webpackEnv) {
             },
             {
               test: /\.ttf$/,
-              use: ['file-loader']
+              use: 'asset/resource'
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
