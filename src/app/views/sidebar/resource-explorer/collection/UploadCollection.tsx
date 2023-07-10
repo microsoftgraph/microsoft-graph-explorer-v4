5dcf64dd39dd67de5dffa50e6522f0279e62de5d
EvansA <evansonoka05.j@gmail.com>
2023-07-10 11:51:01 +0300

    Task: Show upload collections button (#2583)

--- src/app/views/sidebar/resource-explorer/collection/UploadCollection.tsx
+++ src/app/views/sidebar/resource-explorer/collection/UploadCollection.tsx
@@ -1,19 +1,24 @@
 import { CSSProperties, useState } from 'react';
 import { generateResourcePathsFromPostmanCollection } from './postman.util';
 import { ActionButton, DefaultButton, Dialog, DialogFooter, DialogType, IIconProps,
-  Label, MessageBarType, PrimaryButton, getId } from '@fluentui/react';
+  Label, MessageBarType, PrimaryButton, getId, getTheme } from '@fluentui/react';
 import { useDispatch } from 'react-redux';
 import { addResourcePaths, removeResourcePaths } from '../../../../services/actions/collections-action-creators';
 import { translateMessage } from '../../../../utils/translate-messages';
 import { setQueryResponseStatus } from '../../../../services/actions/query-status-action-creator';
 import { ResourcePath } from '../../../../../types/resources';
 import { useAppSelector } from '../../../../../store';
+import { collectionStyles } from './Collection.styles';
+import { isGeneratedCollectionInCollection } from './upload-collection.util';
+import { componentNames, eventTypes, telemetry } from '../../../../../telemetry';
 
 export const UploadPostmanCollection = () => {
   const dispatch = useDispatch();
   const [isDialogHidden, setIsDialogHidden] = useState(true);
   const [uploadedCollections, setUploadedCollections] = useState<ResourcePath[]>([]);
   const { collections } = useAppSelector((state) => state);
+  const theme = getTheme();
+  const { uploadButtonStyles, uploadLabelStyles } = collectionStyles(theme);
 
   const uploadIcon: IIconProps = {
     iconName: 'Upload'
@@ -22,6 +27,7 @@ export const UploadPostmanCollection = () => {
   const style_: CSSProperties = {
     display: 'none'
   }
+
   const selectFile = () => {
     const fileInput = document.getElementById('file-input');
     fileInput?.click();
@@ -37,33 +43,53 @@ export const UploadPostmanCollection = () => {
           const jsonData = JSON.parse(fileContent as string);
           const generatedCollection = generateResourcePathsFromPostmanCollection(jsonData);
           if(collections && collections.length > 0 && collections.find(k => k.isDefault)!.paths.length > 0){
-            setUploadedCollections(generatedCollection);
-            toggleIsDialogHidden();
+            const currentCollection = collections.find(k => k.isDefault)!.paths;
+            if(isGeneratedCollectionInCollection(currentCollection, generatedCollection)){
+              trackUploadAction('Collection exists');
+              dispatchCollectionSelectionStatus('Collection items exist', 'Collection items exist');
+            }
+            else{
+              setUploadedCollections(generatedCollection);
+              toggleIsDialogHidden();
+            }
           }
           else{
+            trackUploadAction('Collection added')
             dispatch(addResourcePaths(generatedCollection));
           }
         }
         catch(error){
-          dispatch(
-            setQueryResponseStatus({
-              status: translateMessage('Invalid file format'),
-              statusMessage: translateMessage('Invalid file format'),
-              ok: true,
-              messageType: MessageBarType.error
-            })
-          )
+          trackUploadAction('Invalid file format');
+          dispatchCollectionSelectionStatus('Invalid file format', 'Invalid file format');
         }
       };
       reader.readAsText(file);
     }
   }
 
+  const dispatchCollectionSelectionStatus = (status: string, statusMessage: string) => {
+    dispatch(
+      setQueryResponseStatus({
+        status: translateMessage(status),
+        statusMessage: translateMessage(statusMessage),
+        ok: false,
+        messageType: MessageBarType.error
+      })
+    )
+  }
+
+  const trackUploadAction = (status: string) => {
+    telemetry.trackEvent(eventTypes.BUTTON_CLICK_EVENT, {
+      componentName: componentNames.UPLOAD_COLLECTIONS_BUTTON,
+      status
+    });
+  }
+
   const deleteResourcesDialogProps = {
     type: DialogType.normal,
     title: translateMessage('Upload collection'),
     closeButtonAriaLabel: 'Close',
-    subText: translateMessage('Would you like to merge with the current collection?')
+    subText: translateMessage('You have an existing')
   };
 
   const toggleIsDialogHidden = () => {
@@ -71,11 +97,13 @@ export const UploadPostmanCollection = () => {
   }
 
   const mergeWithExistingCollection = () => {
+    trackUploadAction('Collection merged');
     dispatch(addResourcePaths(uploadedCollections));
     setIsDialogHidden(!isDialogHidden);
   }
 
   const overwriteCollection = () => {
+    trackUploadAction('Collection replaced');
     const resourcePaths = getPathsFromCollection();
     dispatch(removeResourcePaths(resourcePaths));
     setIsDialogHidden(!isDialogHidden);
@@ -91,17 +119,17 @@ export const UploadPostmanCollection = () => {
     return resourcePaths
   }
 
-
   return (
-    <div style={{position: 'relative', bottom: '4px'}} hidden={true}>
-      <input type="file" id="file-input" style={style_} onChange={handleFileSelect}/>
+    <div style={{position: 'relative', bottom: '4px'}} >
       <ActionButton iconProps={uploadIcon}
         title={translateMessage('Upload collection')}
         ariaLabel={translateMessage('Upload collection')}
         disabled={false}
         onClick={() => selectFile()}
+        styles={uploadButtonStyles}
       >
-        <Label>
+        <input type="file" id="file-input" style={style_} onInput={handleFileSelect} value={''}/>
+        <Label styles = {uploadLabelStyles}>
           {translateMessage('Upload collection')}
         </Label>
       </ActionButton>
@@ -116,8 +144,8 @@ export const UploadPostmanCollection = () => {
         }}
       >
         <DialogFooter>
-          <PrimaryButton onClick={mergeWithExistingCollection} text={translateMessage('YES')} />
-          <DefaultButton onClick={overwriteCollection} text={translateMessage('NO')} />
+          <PrimaryButton onClick={mergeWithExistingCollection} text={translateMessage('Merge with existing')} />
+          <DefaultButton onClick={overwriteCollection} text={translateMessage('Replace existing')} />
         </DialogFooter>
       </Dialog>
     </div>
