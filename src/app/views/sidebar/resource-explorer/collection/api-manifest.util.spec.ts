99645513c7246deceec88b717dfa80995fc331b2
Charles Wahome <thewahome.cw@gmail.com>
2023-11-01 18:05:12 +0300

    add manifest dependency example to tests

--- src/app/views/sidebar/resource-explorer/collection/api-manifest.util.spec.ts
+++ src/app/views/sidebar/resource-explorer/collection/api-manifest.util.spec.ts
@@ -12,6 +12,6 @@ describe('API Manifest should', () => {
     const item: any = filtered.links[0];
     const paths = getResourcePaths(item, version);
     const manifest = generateAPIManifest({ paths, permissions: undefined, scopeType: 'Application_DelegatedWork' });
-    expect(manifest.apiDependencies.graph.requests.length).toBe(paths.length);
+    expect(manifest.apiDependencies[`graph-${version}`].requests.length).toBe(paths.length);
   });
 });
