e15f8b15eef68782c8ab3e4395cd7377f4730ea7
Charles Wahome <thewahome.cw@gmail.com>
2023-11-06 10:45:39 +0300

    Group manifest URLs based on version and scope pairs

--- src/app/views/sidebar/resource-explorer/collection/api-manifest.util.spec.ts
+++ src/app/views/sidebar/resource-explorer/collection/api-manifest.util.spec.ts
@@ -8,10 +8,11 @@ const resource = JSON.parse(JSON.stringify(content)) as IResource;
 describe('API Manifest should', () => {
   it.only('have requests generated', async () => {
     const version = 'v1.0';
+    const scopeType = 'DelegatedWork';
     const filtered = createResourcesList(resource.children!, version)[0];
     const item: any = filtered.links[0];
     const paths = getResourcePaths(item, version);
-    const manifest = generateAPIManifest({ paths, permissions: undefined, scopeType: 'Application_DelegatedWork' });
-    expect(manifest.apiDependencies[`graph-${version}`].requests.length).toBe(paths.length);
+    const manifest = generateAPIManifest({ paths, permissions: undefined });
+    expect(manifest.apiDependencies[`graph-${version}-${scopeType}`].requests.length).toBe(paths.length);
   });
 });
