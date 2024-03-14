149a9956d0283a95d770ed9e46bbdd93f08f0e6e
thewahome <thewahome.cw@gmail.com>
2024-03-14 16:19:26 +0300

    add a close button and make it multiline

--- src/app/views/query-runner/BannerNotification.tsx
+++ src/app/views/query-runner/BannerNotification.tsx
@@ -1,13 +1,27 @@
 import { MessageBar, MessageBarType } from '@fluentui/react';
+import { useState } from 'react';
 
 import { translateMessage } from '../../utils/translate-messages';
 import MessageDisplay from '../common/message-display/MessageDisplay';
 
 const BannerNotification = () => {
   const bannerMessage = translateMessage('Banner message');
+  const [visible, setMessageBarVisibility] = useState(true);
+
+  const dismissMessageBar = () => {
+    setMessageBarVisibility(false);
+  };
+
+  if (!visible) {
+    return null;
+  }
 
   return (
-    <MessageBar messageBarType={MessageBarType.warning}>
+    <MessageBar
+      isMultiline={true}
+      onDismiss={dismissMessageBar}
+      dismissButtonAriaLabel={translateMessage('Close')}
+      messageBarType={MessageBarType.warning}>
       <MessageDisplay message={bannerMessage} />
     </MessageBar>
   );
