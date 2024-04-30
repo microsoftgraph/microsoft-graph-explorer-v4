38c3849ef352d03735cb92c7a7afba8f38da5fa2
Charles Wahome <thewahome.cw@gmail.com>
2024-04-30 16:00:39 +0300

    Fix: malformed json error (#3107)
    * create json validator function
    
    * make message more readable
    
    * show response alongside warning

--- src/app/services/actions/query-action-creator-util.ts
+++ src/app/services/actions/query-action-creator-util.ts
@@ -221,6 +221,14 @@ export async function generateResponseDownloadUrl(
   }
 }
 
+async function tryParseJson(textValue: string) {
+  try {
+    return JSON.parse(textValue);
+  } catch (error) {
+    return textValue;
+  }
+}
+
 export function parseResponse(
   response: any,
   respHeaders: any = {}
@@ -233,7 +241,7 @@ export function parseResponse(
     const contentType = getContentType(response.headers);
     switch (contentType) {
       case ContentType.Json:
-        return response.json();
+        return response.text().then(tryParseJson);
       case ContentType.XML:
       case ContentType.HTML:
       case ContentType.TextCsv:
