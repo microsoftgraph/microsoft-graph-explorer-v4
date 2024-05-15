5d4e8745ecd84cbf2c548e27539bb0a2d1405dac
Charles Wahome <thewahome.cw@gmail.com>
2024-05-15 13:28:05 +0300

    Chore: app migration with feature flags (#3144)

--- src/modules/authentication/msal-app.ts
+++ src/modules/authentication/msal-app.ts
@@ -1,4 +1,5 @@
-import { Configuration, PublicClientApplication } from '@azure/msal-browser';
+import { Configuration, LogLevel, PublicClientApplication } from '@azure/msal-browser';
+import { eventTypes, telemetry } from '../../telemetry';
 
 function getClientIdFromWindow() {
   return (window as any).ClientId;
@@ -19,10 +20,36 @@ export const configuration: Configuration = {
     cacheLocation: 'localStorage',
     storeAuthStateInCookie: true,
     claimsBasedCachingEnabled: true
+  },
+  system: {
+    loggerOptions: {
+      logLevel: LogLevel.Verbose,
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        telemetry.trackEvent(eventTypes.AUTH_REQUEST_EVENT, { message, level });
+        switch (level) {
+          case LogLevel.Error:
+            console.error('[MSAL]', message);
+            return;
+          case LogLevel.Info:
+            console.info('[MSAL]', message);
+            return;
+          case LogLevel.Verbose:
+            console.debug('[MSAL]', message);
+            return;
+          case LogLevel.Warning:
+            console.warn('[MSAL]', message);
+            return;
+        }
+      },
+      piiLoggingEnabled: false
+    }
   }
 };
 
 
 const msalApplication = new PublicClientApplication(configuration);
 msalApplication.initialize();
-export{ msalApplication };
+export { msalApplication };
