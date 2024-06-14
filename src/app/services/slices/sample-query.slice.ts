e83589eaeef5369be058aeae79d189797e0bd403
Musale Martin <martinmusale@microsoft.com>
2024-06-14 14:29:31 +0300

    Fix linting issues

--- src/app/services/slices/sample-query.slice.ts
+++ src/app/services/slices/sample-query.slice.ts
@@ -2,21 +2,21 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IQuery } from '../../../types/query-runner';
 
 const initialState: IQuery = {
-    selectedVerb: 'GET',
-    sampleHeaders: [],
-    sampleUrl: 'https://graph.microsoft.com/v1.0/me',
-    sampleBody: undefined,
-    selectedVersion: 'v1.0'
+  selectedVerb: 'GET',
+  sampleHeaders: [],
+  sampleUrl: 'https://graph.microsoft.com/v1.0/me',
+  sampleBody: undefined,
+  selectedVersion: 'v1.0'
 }
 const sampleQuery = createSlice({
-    name: 'sampleQuery',
-    initialState,
-    reducers: {
-        setSampleQuery: (state, action: PayloadAction<IQuery>)=>{
-            state = action.payload
-            return state
-        }
+  name: 'sampleQuery',
+  initialState,
+  reducers: {
+    setSampleQuery: (state, action: PayloadAction<IQuery>)=>{
+      state = action.payload
+      return state
     }
+  }
 })
 
 export const {setSampleQuery} = sampleQuery.actions
