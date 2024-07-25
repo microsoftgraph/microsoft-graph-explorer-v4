47fe5f1f91f6eed2c4392c953c9b2bd8d8018ac9
Charles Wahome <thewahome.cw@gmail.com>
2024-07-25 20:08:44 +0300

    Task: response area expanded slice (#3267)

--- src/app/services/actions/response-expanded-action-creator.ts
+++
@@ -1,9 +0,0 @@
-import { AppAction } from '../../../types/action';
-import { RESPONSE_EXPANDED } from '../redux-constants';
-
-export function expandResponseArea(expanded: boolean): AppAction {
-  return {
-    type: RESPONSE_EXPANDED,
-    response: expanded
-  };
-}
