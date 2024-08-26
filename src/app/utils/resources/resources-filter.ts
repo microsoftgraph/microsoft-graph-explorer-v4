a7a181f138d62663bf3a9e25f3c51fc71656128d
Musale Martin <martinmusale@microsoft.com>
2024-08-26 20:08:19 +0300

    Fetch all children in a segment
    Signed-off-by: Musale Martin <martinmusale@microsoft.com>

--- src/app/utils/resources/resources-filter.ts
+++ src/app/utils/resources/resources-filter.ts
@@ -35,7 +35,19 @@ function getMatchingResourceForUrl(url: string, resources: IResource[]): IResour
   return node;
 }
 
+const getResourceFromURL = (url: string, resource: IResource): IResource | null =>{
+  url.split('/').filter(u=>u!=='').forEach((segment:string)=>{
+    const foundResource = resource?.children?.find(res=>return res.segment === segment);
+    if(foundResource){
+      console.log(`Found ${segment}`)
+      resource = foundResource
+    }
+  })
+  return resource;
+}
+
 export {
   searchResources,
-  getMatchingResourceForUrl
+  getMatchingResourceForUrl,
+  getResourceFromURL
 }
