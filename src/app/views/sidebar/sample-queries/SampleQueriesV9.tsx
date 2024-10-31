007a207271a6072353b9586932c4b9067b75b0b5
Musale Martin <martinmusale@microsoft.com>
2024-10-31 19:19:10 +0300

    Get the sample queries

--- src/app/views/sidebar/sample-queries/SampleQueriesV9.tsx
+++ src/app/views/sidebar/sample-queries/SampleQueriesV9.tsx
@@ -1,15 +1,17 @@
 import {
     Button, InputOnChangeData, makeStyles, MessageBar, MessageBarActions, MessageBarBody,
-    SearchBox, SearchBoxChangeEvent, Link } from '@fluentui/react-components';
+    SearchBox, SearchBoxChangeEvent, Link, AriaLiveAnnouncer, Text } from '@fluentui/react-components';
 import { DismissRegular } from '@fluentui/react-icons';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
-import { useAppSelector } from '../../../../store';
+import { useAppSelector, useAppDispatch } from '../../../../store';
 import { ISampleQuery } from '../../../../types/query-runner';
 import { translateMessage } from '../../../utils/translate-messages';
 import { performSearch } from './sample-query-utils';
 import { componentNames, telemetry } from '../../../../telemetry';
 import { geLocale } from '../../../../appLocale';
+import { NoResultsFound } from '../sidebar-utils/SearchResult';
+import { fetchSamples } from '../../../services/slices/samples.slice';
 
 const useStyles = makeStyles({
     searchBox: {
@@ -19,19 +21,32 @@ const useStyles = makeStyles({
 })
 
 export const SampleQueriesV9 = ()=>{
+    const dispatch = useAppDispatch();
     const sampleQueriesStyles = useStyles();
     const samples = useAppSelector(s => s.samples);
     const {error, pending, queries} = samples
+    const token = useAppSelector(s => s.auth.authToken.token);
+
 
     const shouldGenerateGroups = useRef(true);
-    const [sampleQueries, setSampleQueries] = useState<ISampleQuery[] | string>(samples.queries);
+    const [sampleQueries, setSampleQueries] = useState<ISampleQuery[]>(queries);
+    console.log(sampleQueries)
 
     const handleSearchValueChange = (_: SearchBoxChangeEvent, data: InputOnChangeData)=>{
         const value = data.value;
         shouldGenerateGroups.current = true;
-        const filteredQueries = value ? performSearch(queries, value) : value;
+        const filteredQueries = value ? performSearch(queries, value) : [];
         setSampleQueries(filteredQueries);
     }
+
+    useEffect(() => {
+        if (queries.length === 0) {
+          dispatch(fetchSamples());
+        } else {
+          setSampleQueries(queries)
+        }
+      }, [queries])
+
 
     return <>
         <SearchBox
@@ -41,6 +56,8 @@ export const SampleQueriesV9 = ()=>{
         <hr />
         {error && <CachedSetMessageBar />}
         <MoreQueriesMessageBar/>
+        <AriaLiveAnnouncer><Text>{`${sampleQueries.length} search results available.`}</Text></AriaLiveAnnouncer>
+        {sampleQueries.length === 0 ? NoResultsFound('No samples found'): <Samples />}
     </>
 }
 
@@ -55,7 +72,7 @@ const CachedSetMessageBar = ()=>{
 
 const MoreQueriesMessageBar = ()=>{
     return (
-        <MessageBar intent="info">
+        <MessageBar intent={'info'}>
             <MessageBarBody>{translateMessage('see more queries')}{' '}
             <Link
                 target='_blank'
@@ -66,5 +83,11 @@ const MoreQueriesMessageBar = ()=>{
                 inline={true}>{translateMessage('Microsoft Graph API Reference docs')}</Link>
             </MessageBarBody>
       </MessageBar>
+    )
+}
+
+const Samples = () =>{
+    return (
+    <></>
     )
 }
