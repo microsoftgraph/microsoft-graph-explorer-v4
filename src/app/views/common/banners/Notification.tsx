e248e4a7aaadd7fbe3f713f81509727673c9825c
Musale Martin <martinmusale@microsoft.com>
2024-11-05 14:31:44 +0300

    Track the tutorial link with telemetry

--- src/app/views/common/banners/Notification.tsx
+++ src/app/views/common/banners/Notification.tsx
@@ -9,6 +9,8 @@ import {
   } from '@fluentui/react-components';
 import { useNotificationStyles } from './Notification.styles';
 import { useState } from 'react';
+import { translateMessage } from '../../../utils/translate-messages';
+import { componentNames, telemetry } from '../../../../telemetry';
 
 interface NotificationProps {
     header: string;
@@ -17,12 +19,16 @@ interface NotificationProps {
     linkText: string;
 }
 
+const handleOnClickLink = (e: React.MouseEvent<HTMLAnchorElement>)=>{
+    telemetry.trackLinkClickEvent(
+        (e.currentTarget as HTMLAnchorElement).href, componentNames.GRAPH_EXPLORER_TUTORIAL_LINK)
+}
+
 export const Notification: React.FunctionComponent<NotificationProps> = (props: NotificationProps) => {
     const styles = useNotificationStyles();
     const [isVisible, setIsVisible] = useState(true);
 
     const handleDismiss = () => {
-        // TODO: track as dismissed in telemetry
         // TODO: don't show again. Persist choice to state
         setIsVisible(false);
     };
@@ -36,13 +42,16 @@ export const Notification: React.FunctionComponent<NotificationProps> = (props: 
             <MessageBarBody>
                 <MessageBarTitle>{props.header}</MessageBarTitle><br></br>
                 {props.content}{' '}
-                <Link href={props.link} target='_blank'>{props.linkText} <OpenRegular /></Link>
+                <Link
+                    onClick={handleOnClickLink}
+                    href={props.link}
+                    target='_blank'>{props.linkText} <OpenRegular /></Link>
             </MessageBarBody>
             <MessageBarActions
             containerAction={
                 <Button
                 onClick={handleDismiss}
-                aria-label="dismiss"
+                aria-label={translateMessage('Dismiss banner')}
                 appearance="transparent"
                 icon={<DismissRegular />}
                 />
--- src/telemetry/component-names.ts
+++ src/telemetry/component-names.ts
@@ -74,6 +74,7 @@ export const GRAPH_TOOLKIT_PLAYGROUND_LINK = 'Graph toolkit playground link';
 export const MICROSOFT_APIS_TERMS_OF_USE_LINK = 'Microsoft APIs terms of use link';
 export const MICROSOFT_PRIVACY_STATEMENT_LINK = 'Microsoft privacy statement link';
 export const MICROSOFT_GRAPH_API_REFERENCE_DOCS_LINK = 'Microsoft graph API reference docs link';
+export const GRAPH_EXPLORER_TUTORIAL_LINK = 'Graph Explorer Tutorial Link';
 export const CODE_SNIPPET_LANGUAGES = {
   CSharp: {
     sdk: 'C# SDK link', doc: 'C# snippet docs link'
