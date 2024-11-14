1b57f2bafc67f60947502826d09c894524c9cccf
Musale Martin <martinmusale@microsoft.com>
2024-11-14 17:38:09 +0300

    Add telemetry tracking for settings button and theme chooser interactions

--- src/app/views/main-header/settings/SettingsV9.tsx
+++ src/app/views/main-header/settings/SettingsV9.tsx
@@ -3,7 +3,10 @@ import {
 } from '@fluentui/react-components'
 import { Color20Regular, Settings20Regular, WindowDevTools20Regular } from '@fluentui/react-icons'
 
+import { componentNames, eventTypes, telemetry } from '../../../../telemetry'
+import { usePopups } from '../../../services/hooks'
 import { translateMessage } from '../../../utils/translate-messages'
+
 const useStyles = makeStyles({
   button: {
     height: '100%',
@@ -11,20 +14,42 @@ const useStyles = makeStyles({
     maxWidth: '48px'
   }
 })
+
+const trackSettingsButtonClickEvent = () => {
+  telemetry.trackEvent(eventTypes.BUTTON_CLICK_EVENT, {
+    ComponentName: componentNames.SETTINGS_BUTTON
+  });
+}
+
 const SettingsV9 = ()=>{
   const styles = useStyles();
+  const {show: showThemeChooser} = usePopups('theme-chooser', 'dialog')
+  const toggleThemeChooserDialogState = () => {
+    showThemeChooser({
+      settings: {
+        title: translateMessage('Change theme')
+      }
+    });
+    telemetry.trackEvent(eventTypes.BUTTON_CLICK_EVENT, {
+      ComponentName: componentNames.THEME_CHANGE_BUTTON
+    });
+  };
 
   return (
     <Menu>
       <Tooltip content={translateMessage('Settings')} relationship="description">
         <MenuTrigger disableButtonEnhancement>
-          <Button className={styles.button} appearance="subtle" icon={<Settings20Regular />} />
+          <Button
+            onClick={trackSettingsButtonClickEvent}
+            className={styles.button} appearance="subtle" icon={<Settings20Regular />} />
         </MenuTrigger>
       </Tooltip>
 
       <MenuPopover>
         <MenuList>
-          <MenuItem icon={<Color20Regular />}>{translateMessage('Change theme')}</MenuItem>
+          <MenuItem
+            icon={<Color20Regular />}
+            onClick={toggleThemeChooserDialogState}>{translateMessage('Change theme')}</MenuItem>
           <MenuItem icon={<WindowDevTools20Regular />}>{translateMessage('Office Dev Program')}</MenuItem>
         </MenuList>
       </MenuPopover>
