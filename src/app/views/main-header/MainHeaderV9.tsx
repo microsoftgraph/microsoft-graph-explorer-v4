b18c0e0a305ceb2883a91f2f8fef71b217b10d15
Musale Martin <martinmusale@microsoft.com>
2024-11-14 16:45:20 +0300

    Add a component that displays the header with icons

---
+++ src/app/views/main-header/MainHeaderV9.tsx
@@ -0,0 +1,50 @@
+import { makeStyles, Text, tokens } from '@fluentui/react-components';
+import { FeedbackButton } from './FeedbackButton';
+import { Help } from './Help';
+import { Settings } from './settings/Settings';
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 24px',
+    height: '50px',
+    background: tokens.colorNeutralBackground4,
+    marginBottom: '8px' // TODO: remove when sidebar and query areas are updated
+  },
+  headerIcons: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    maxHeight: '100%'
+  },
+  headerText: {
+    color: tokens.colorBrandForeground1,
+    lineHeight: '28px'
+  }
+});
+
+const MainHeaderV9 = ()=>{
+  const styles = useStyles()
+  return (
+    <div className={styles.root}>
+      <Text size={600} as="h1" className={styles.headerText}>Graph Explorer</Text>
+      <HeaderIcons />
+    </div>
+  )
+}
+
+const HeaderIcons = () => {
+
+  const styles = useStyles()
+  return (
+    <div className={styles.headerIcons}>
+      <Settings />
+      <Help />
+      <FeedbackButton />
+    </div>
+  )
+}
+
+export { MainHeaderV9 };
+
