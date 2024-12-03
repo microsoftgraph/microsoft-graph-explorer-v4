0becb35f2966edb055cac5cc17971dc89b4a18d5
Mnickii <miaronkirote@gmail.com>
2024-12-03 11:31:36 +0100

    detect system theme in theme-chooser and set as default

--- src/app/views/main-header/settings/ThemeChooserV9.tsx
+++ src/app/views/main-header/settings/ThemeChooserV9.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { makeStyles, RadioGroup, Radio} from '@fluentui/react-components';
 import { useAppDispatch, useAppSelector } from '../../../../store';
 import { componentNames, eventTypes, telemetry } from '../../../../telemetry';
@@ -28,20 +29,37 @@ const availableThemes = [
 const useIconOptionStyles = makeStyles({
   root: {
     display: 'flex',
-    alignItems: 'center',
-    gap: '5px'
+    alignItems: 'center'
   },
   icon: {
     fontSize: '30px'
+  },
+  radio: {
+    '&:checked ~ .fui-Radio__indicator::after': {
+      borderRadius: '50%'
+    }
   }
+
 });
 
 const useLabelStyles = makeStyles({
   root: {
     display: 'flex',
-    gap: '5px'
+    textAlign: 'center'
+  },
+  container: {
+    display: 'block',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
+
+const getSystemTheme = (): string => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  }
+  return 'light';
+};
 
 const ThemeChooserV9: React.FC<PopupsComponent<null>> = () => {
   const dispatch = useAppDispatch();
@@ -49,9 +67,18 @@ const ThemeChooserV9: React.FC<PopupsComponent<null>> = () => {
   const iconOptionStyles = useIconOptionStyles();
   const labelStyles = useLabelStyles();
 
+  useEffect(() => {
+    // Load the theme from local storage or use the system theme as the default
+    const savedTheme = localStorage.getItem('appTheme') ?? getSystemTheme();
+    dispatch(changeTheme(savedTheme));
+    loadGETheme(savedTheme); // Remove when cleaning up
+  }, [dispatch]);
+
 
   const handleChangeTheme = (selectedTheme: { key: string; displayName: string; icon: JSX.Element }) => {
     const newTheme: string = selectedTheme.key;
+    // Save the selected theme to local storage
+    localStorage.setItem('appTheme', newTheme);
     // Applies the theme to the Fluent UI components
     dispatch(changeTheme(newTheme));
     loadGETheme(newTheme); //Remove when cleaning up
@@ -62,18 +89,20 @@ const ThemeChooserV9: React.FC<PopupsComponent<null>> = () => {
   };
   return (
     <RadioGroup layout="horizontal" aria-labelledby='theme-chooser'
-      defaultValue={availableThemes.find(theme => theme.key === appTheme)?.displayName}
+      value={availableThemes.find(theme => theme.key === appTheme)?.displayName}
     >
       {availableThemes.map((theme) => (
         <div key={theme.key} className={iconOptionStyles.root}>
           <Radio
             value={theme.key}
+            checked={appTheme === theme.key}
+            className={iconOptionStyles.radio}
             label={{
               className: labelStyles.root,
               children: (
-                <>
+                <div className={labelStyles.container}>
                   <span className={iconOptionStyles.icon}>{theme.icon}</span> {translateMessage(theme.displayName)}
-                </>
+                </div>
               )
             }}
             onClick={() => handleChangeTheme(theme)}>
