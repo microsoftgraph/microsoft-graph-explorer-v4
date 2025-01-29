43852f93bff3582079731ee2f5dfa5ca84bae1d3
Mnickii <miaronkirote@gmail.com>
2025-01-29 09:03:45 +0100

    fix styling

--- src/app/views/common/popups/DrawerWrapper.tsx
+++ src/app/views/common/popups/DrawerWrapper.tsx
@@ -1,21 +1,34 @@
-// import { getTheme, IconButton, IOverlayProps, Spinner } from '@fluentui/react';
 import {
   Drawer,
   DrawerHeader,
   DrawerBody,
   DrawerFooter,
   Spinner,
-  Button
+  Button,
+  makeStyles,
+  DrawerHeaderTitle
 } from '@fluentui/react-components';
-import { ArrowLeft20Filled, TrayItemRemove20Regular  } from '@fluentui/react-icons';
+import { ArrowLeft20Filled } from '@fluentui/react-icons';
 import { Suspense } from 'react';
 import { translateMessage } from '../../../utils/translate-messages';
 import { WrapperProps } from './popups.types';
+
+const useDrawerStyles = makeStyles({
+  header: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  backButton: {
+    marginInlineEnd: '20px'
+  }
+});
 
 export function DrawerWrapper(props: WrapperProps) {
   const { isOpen, dismissPopup, Component, popupsProps, closePopup } = props;
   const { title, renderFooter } = popupsProps.settings;
   const headerText = title || '';
+
+  const drawerClasses = useDrawerStyles();
 
   const getDrawerSize = () => {
     switch (popupsProps.settings.width) {
@@ -40,15 +53,17 @@ export function DrawerWrapper(props: WrapperProps) {
   const showBackButton = title === 'Edit Scope' || title === 'Edit Collection' || title === 'Preview Permissions';
 
   const onRenderHeader = (): JSX.Element => (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
+
+    <div className={drawerClasses.header}>
       <Button
-        icon={< ArrowLeft20Filled />}
+        className={drawerClasses.backButton}
+        icon={<ArrowLeft20Filled />}
         aria-label={translateMessage('Back')}
         onClick={() => dismissPopup()}
       />
-      <span>
+      <DrawerHeaderTitle>
         {headerText}
-      </span>
+      </DrawerHeaderTitle>
     </div>
   );
 
@@ -62,11 +77,6 @@ export function DrawerWrapper(props: WrapperProps) {
       >
         <DrawerHeader>
           {showBackButton && onRenderHeader()}
-          <Button
-            icon={< TrayItemRemove20Regular />}
-            aria-label={translateMessage('Close')}
-            onClick={() => dismissPopup()}
-          />
         </DrawerHeader>
         <DrawerBody>
           <Suspense fallback={<Spinner />}>
