3d57dc3c630a01594b1fa757502b30482ac4ca09
Mnickii <miaronkirote@gmail.com>
2025-02-11 09:50:46 +0300

    remove geLocale

--- src/app/views/app-sections/TermsOfUseMessage.tsx
+++ src/app/views/app-sections/TermsOfUseMessage.tsx
@@ -1,6 +1,5 @@
 import { Link, MessageBar, MessageBarType, styled } from '@fluentui/react';
 
-import { geLocale } from '../../../appLocale';
 import { useAppDispatch, useAppSelector } from '../../../store';
 import { componentNames, telemetry } from '../../../telemetry';
 import { clearTermsOfUse } from '../../services/slices/terms-of-use.slice';
@@ -23,8 +22,9 @@ const StyledTermsOfUseMessage = () => {
         onClick={(e) =>
           telemetry.trackLinkClickEvent((e.currentTarget as HTMLAnchorElement).href,
             componentNames.MICROSOFT_APIS_TERMS_OF_USE_LINK)}
-        href={'https://learn.microsoft.com/' + geLocale +
-          '/legal/microsoft-apis/terms-of-use?context=graph/context'} target='_blank' rel='noopener noreferrer'
+        href={'https://learn.microsoft.com/legal/microsoft-apis/terms-of-use?context=graph/context'}
+        target='_blank'
+        rel='noopener noreferrer'
         underline>
         {translateMessage('Terms of use')}</Link>.
       {translateMessage('View the')}
@@ -32,7 +32,7 @@ const StyledTermsOfUseMessage = () => {
         onClick={(e) =>
           telemetry.trackLinkClickEvent((e.currentTarget as HTMLAnchorElement).href,
             componentNames.MICROSOFT_PRIVACY_STATEMENT_LINK)}
-        href={'https://privacy.microsoft.com/' + geLocale + '/privacystatement'}
+        href={'https://privacy.microsoft.com/privacystatement'}
         target='_blank' rel='noopener noreferrer'
         underline>
         {translateMessage('Microsoft Privacy Statement')}</Link>.
--- src/app/views/sidebar/sample-queries/SampleQueries.tsx
+++ src/app/views/sidebar/sample-queries/SampleQueries.tsx
@@ -6,7 +6,6 @@ import {
 } from '@fluentui/react';
 import { useEffect, useRef, useState } from 'react';
 
-import { geLocale } from '../../../../appLocale';
 import { useAppDispatch, useAppSelector } from '../../../../store';
 import { componentNames, telemetry } from '../../../../telemetry';
 import { IQuery, ISampleQueriesProps, ISampleQuery } from '../../../../types/query-runner';
@@ -362,7 +361,7 @@ const UnstyledSampleQueries = (sampleProps?: ISampleQueriesProps): JSX.Element =
           rel="noopener noreferrer"
           onClick={(e) => telemetry.trackLinkClickEvent((e.currentTarget as HTMLAnchorElement).href,
             componentNames.MICROSOFT_GRAPH_API_REFERENCE_DOCS_LINK)}
-          href={`https://learn.microsoft.com/${geLocale}/graph/api/overview?view=graph-rest-1.0`}
+          href={'https://learn.microsoft.com/graph/api/overview?view=graph-rest-1.0'}
           underline
         >
           {translateMessage('Microsoft Graph API Reference docs')}
