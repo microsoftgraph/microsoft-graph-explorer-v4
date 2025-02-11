f4e595fbc6874daa68963111729afe7c47b17305
Mnickii <miaronkirote@gmail.com>
2025-02-11 18:22:14 +0300

    migrate request headerList

--- src/app/views/common/lazy-loader/component-registry/index.tsx
+++ src/app/views/common/lazy-loader/component-registry/index.tsx
@@ -8,7 +8,7 @@ import LazyGraphToolkit from  '../../../query-response/graph-toolkit/GraphToolki
 import LazySnippets from  '../../../query-response/snippets/Snippets';
 import LazyCopyButton from  '../../copy-button/CopyButtonV9';
 import LazyAuth from  '../../../query-runner/request/auth/AuthV9';
-import LazyRequestHeaders from  '../../../query-runner/request/headers/RequestHeadersV9';
+import LazyRequestHeaders from  '../../../query-runner/request/headers/RequestHeaders';
 import LazyHistory from  '../../../sidebar/history/History';
 import LazyResourceExplorer from  '../../../sidebar/resource-explorer/ResourceExplorer';
 
