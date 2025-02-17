fc7165b0a886ba1afbcad379333f32d143a189bc
ElinorW <elkaguon@microsoft.com>
2025-02-17 19:57:23 +0300

    add toggle button on header

--- src/app/views/layout/Layout.tsx
+++ src/app/views/layout/Layout.tsx
@@ -5,6 +5,7 @@ import CollectionPermissionsProvider from '../../services/context/collection-per
 import { PopupsProvider } from '../../services/context/popups-context';
 import { ValidationProvider } from '../../services/context/validation-context/ValidationProvider';
 import { setSampleQuery } from '../../services/slices/sample-query.slice';
+import { toggleSidebar } from '../../services/slices/sidebar-properties.slice'; // Import sidebar action
 import { translateMessage } from '../../utils/translate-messages';
 import { StatusMessagesV9, TermsOfUseMessageV9 } from '../app-sections';
 import Notification from '../common/banners/Notification';
@@ -30,10 +31,8 @@ export const Layout = (props: LayoutProps) => {
   const resizeStyles = useLayoutResizeStyles();
   const dispatch = useAppDispatch();
   const sampleQuery = useAppSelector((state) => state.sampleQuery);
-  // NOTE: use this to show only the icons of the request and response area tabs
   const mode = useAppSelector((state) => state.graphExplorerMode);
-  const mobileScreen = useAppSelector((state) => state.sidebarProperties.mobileScreen);
-  const showSidebar = mode === Mode.Complete && !mobileScreen;
+  const { mobileScreen, showSidebar } = useAppSelector((state) => state.sidebarProperties);
 
   const {
     handleRef: sidebarHandleRef,
@@ -55,17 +54,24 @@ export const Layout = (props: LayoutProps) => {
     }
   }, [sampleBody]);
 
+  useEffect(() => {
+    if (mobileScreen) {
+      dispatch(toggleSidebar({ showSidebar: false, mobileScreen: true }));
+      setSidebarColumnSize(0);
+    }
+  }, [mobileScreen, dispatch]);
+
   const handleOnEditorChange = (value?: string) => {
     setSampleBody(value!);
   };
 
-  const handleToggleSelect = (toggled: boolean)=> {
+  const handleToggleSelect = (toggled: boolean) => {
     setSidebarColumnSize(toggled ? 456 : 5);
-  }
+  };
 
   const resetSidebarArea = () => {
-    setSidebarColumnSize(456)
-  }
+    setSidebarColumnSize(456);
+  };
 
   return (
     <>
@@ -73,17 +79,14 @@ export const Layout = (props: LayoutProps) => {
         <div className={layoutStyles.container}>
           <MainHeaderV9 />
           <div id='content-ref' className={mergeClasses(layoutStyles.content, resizeStyles)} ref={sidebarWrapperRef}>
-            {showSidebar &&
-            <div id='sidebar-ref' className={layoutStyles.sidebar} ref={sidebarElementRef}>
-              <SidebarV9 handleToggleSelect={handleToggleSelect} />
-              <LayoutResizeHandler
-                position='end'
-                ref={sidebarHandleRef}
-                onDoubleClick={resetSidebarArea}
-              />
-            </div>}
+            {showSidebar && (
+              <div id='sidebar-ref' className={layoutStyles.sidebar} ref={sidebarElementRef}>
+                <SidebarV9 handleToggleSelect={handleToggleSelect} />
+                <LayoutResizeHandler position='end' ref={sidebarHandleRef} onDoubleClick={resetSidebarArea} />
+              </div>
+            )}
             <div id='main-content' className={layoutStyles.mainContent}>
-              <div style={{margin: '0 10px'}}>
+              <div style={{ margin: '0 10px' }}>
                 <Notification
                   header={translateMessage('Banner notification 1 header')}
                   content={translateMessage('Banner notification 1 content')}
@@ -93,20 +96,14 @@ export const Layout = (props: LayoutProps) => {
               </div>
 
               <ValidationProvider>
-                <div style={{margin: '0 10px'}}>
+                <div style={{ margin: '0 10px' }}>
                   <QueryRunner onSelectVerb={props.handleSelectVerb} />
                 </div>
-                <div
-                  id='request-response-area'
-                  className={layoutStyles.requestResponseArea}
-                >
+                <div id='request-response-area' className={layoutStyles.requestResponseArea}>
                   <div id='request-area' className={layoutStyles.requestArea}>
-                    <Request
-                      handleOnEditorChange={handleOnEditorChange}
-                      sampleQuery={sampleQuery}
-                    />
+                    <Request handleOnEditorChange={handleOnEditorChange} sampleQuery={sampleQuery} />
                   </div>
-                  <div style={{margin: '0 10px'}}>
+                  <div style={{ margin: '0 10px' }}>
                     <StatusMessagesV9 />
                   </div>
                   <div id='response-area' className={layoutStyles.responseArea}>
