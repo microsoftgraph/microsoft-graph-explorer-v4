9fb7c5bb9ef8ac6469e35102a5aabd6d00965ca1
ElinorW <elkaguon@microsoft.com>
2025-02-18 13:00:57 +0300

    use classNames

--- src/app/views/query-response/adaptive-cards/AdaptiveCardV9.tsx
+++ src/app/views/query-response/adaptive-cards/AdaptiveCardV9.tsx
@@ -48,6 +48,11 @@ const adaptiveCardStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     gap: '4px'
+  },
+  editorContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '500px'
   }
 });
 
@@ -242,6 +247,7 @@ const RenderCard = (props: RenderCardJSONProps) => {
 
 const RenderJSONSchema = (props: RenderCardJSONProps) => {
   const { handleCopy, template } = props;
+  const styles = adaptiveCardStyles();
 
   return (
     <div>
@@ -275,7 +281,7 @@ const RenderJSONSchema = (props: RenderCardJSONProps) => {
           </Link>
         </MessageBarBody>
       </MessageBar>
-      <div style={{ display: 'flex', flexDirection: 'column',height: '500px' }}>
+      <div className={styles.editorContainer}>
         <CopyButtonV9 handleOnClick={handleCopy} isIconButton={true} />
         <MonacoV9
           body={template}
