c76510c708d0d55c4afe9dec6d59f2d2313771df
ElinorW <elkaguon@microsoft.com>
2025-04-09 16:47:04 +0300

    fix container styles

--- src/app/views/query-runner/request/Request.tsx
+++ src/app/views/query-runner/request/Request.tsx
@@ -38,9 +38,11 @@ interface IRequestProps {
 
 const useStyles = makeStyles({
   container: {
-    height: '-webkit-fill-available',
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden'
   },
   tabContainer: {
     display: 'flex',
